8884b46c18b0a8792d5d9e3f32c665f7c9ed650a
Jonas Schneider <mail@jonasschneider.com>
2016-04-28 22:32:38 -0700

    Bump mujoco-py

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ setup(name='gym',
       extras_require={
           'all': ['atari_py>=0.0.17', 'Pillow', 'pyglet', 'PyOpenGL',
                   'pachi-py>=0.0.19',
-                  'mujoco_py>=0.4.1', 'imageio'],
+                  'mujoco_py>=0.4.2', 'imageio'],
 
           # Environment-specific dependencies. Keep these in sync with
           # 'all'!
           'atari': ['atari_py>=0.0.17', 'Pillow', 'pyglet', 'PyOpenGL'],
           'board_game' : ['pachi-py>=0.0.19'],
           'classic_control': ['pyglet', 'PyOpenGL'],
-          'mujoco': ['mujoco_py>=0.4.1', 'imageio'],
+          'mujoco': ['mujoco_py>=0.4.2', 'imageio'],
       },
       package_data={'gym': ['envs/mujoco/assets/*.xml', 'envs/classic_control/assets/*.png']},
       tests_require=['nose2', 'mock'],
