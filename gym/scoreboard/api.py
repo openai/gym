7ec98ede9b87869a67d4a335afe4ffc45af1d581
Jonas Schneider <mail@jonasschneider.com>
2016-05-01 23:39:14 -0400

    Cast WeakRefDict to list first to ensure stability

--- gym/scoreboard/api.py
+++ gym/scoreboard/api.py
@@ -25,8 +25,8 @@ def upload(training_dir, algorithm_id=None, writeup=None, api_key=None):
         api_key (Optional[str]): Your OpenAI API key. Can also be provided as an environment variable (OPENAI_GYM_API_KEY).
     """
 
-    open_monitors = monitoring._monitors.values()
-    if open_monitors:
+    open_monitors = list(monitoring._monitors.values())
+    if len(open_monitors) > 0:
         envs = [m.env.spec.id if m.env.spec else '(unknown)' for m in open_monitors]
         raise error.Error("Still have an open monitor on {}. You must run 'env.monitor.close()' before uploading.".format(', '.join(envs)))
 
