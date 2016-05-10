5efb1c37a975c641df7ff8729a2f630e7e7c0610
Alex Ray <a@machinaut.com>
2016-05-09 22:05:56 -0700

    Blackjack environment from Sutton & Barto textbook (#60)
    * blackjack environment from sutton & barto textbook
    
    * blackjack: dumb agents can hit at 21
    
    * blackjack: add natural (Casino Rules) flag
    
    * blackjack: style and whitespace fixes
    
    * blackjack: simplify reward calculation

--- gym/envs/toy_text/__init__.py
+++ gym/envs/toy_text/__init__.py
@@ -1,3 +1,4 @@
+from gym.envs.toy_text.blackjack import BlackjackEnv
 from gym.envs.toy_text.roulette import RouletteEnv
 from gym.envs.toy_text.frozen_lake import FrozenLakeEnv
 from gym.envs.toy_text.nchain import NChainEnv
