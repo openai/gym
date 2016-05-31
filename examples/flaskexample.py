a0ca18abd14e3543b128a8fca5611d6e2b134837
Catherine Olsson <catherio@mit.edu>
2016-05-31 17:03:00 -0400

    Use JSON payloads and correct mimetype

--- examples/flaskexample.py
+++ examples/flaskexample.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import uuid
 import gym
 
@@ -8,12 +8,6 @@ class Envs(object):
         self.id_len = 8
 
     def create(self, env_id):
-
-        # env_id = request.form['env_id']
-        # TODO: gdb was able to use the request object directly
-        # within the class because the class method was
-        # called straight from the blueprint.route
-
         env = gym.make(env_id)
         instance_id = str(uuid.uuid4().hex)[:self.id_len]
         self.envs[instance_id] = env
@@ -22,25 +16,34 @@ class Envs(object):
     def check_exists(self, instance_id):
         return instance_id in self.envs
 
+    def reset(self, instance_id):
+        env = self.envs[instance_id]
+        obs = env.reset()
+        return env.observation_space.to_jsonable(obs)
+
 app = Flask(__name__)
 envs = Envs()
 
-@app.route('/v1/create/', methods=['POST'])
-def create_env():
+@app.route('/v1/envs/', methods=['POST'])
+def env_create():
     instance_id = envs.create(request.form['env_id'])
-    # TODO: ok to leave the potential KeyError exposed like this?
+    return jsonify(instance_id = instance_id)
 
-    return str(instance_id)
-    # TODO: make the returned values be JSON objects
+@app.route('/v1/envs/<instance_id>/check_exists/', methods=['POST'])
+def env_check_exists(instance_id):
+    exists = envs.check_exists(instance_id)
+    return jsonify(exists = exists)
 
-@app.route('/v1/check/', methods=['POST'])
-def check_exists():
-    return str(envs.check_exists(request.form['instance_id']))
+@app.route('/v1/envs/<instance_id>/reset/', methods=['POST'])
+def env_reset(instance_id):    
+    observation = envs.reset(instance_id)
+    return jsonify(observation = observation)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
  
 # Can test this with
-# curl -i -X POST -d env_id='CartPole-v0' http://127.0.0.1:5000/v1/create/ 
-# curl -i -X POST -d instance_id='<id>' http://127.0.0.1:5000/v1/check/
+# curl -i -X POST -d env_id='CartPole-v0' http://127.0.0.1:5000/v1/envs/
+# curl -i -X POST -d http://127.0.0.1:5000/v1/envs/<instance_id>/check/
+
 
