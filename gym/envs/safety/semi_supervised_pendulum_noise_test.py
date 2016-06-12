0b6dc93c9515a785794f8f4315e5b78b0eeba2c1
Rafael Cosman <rafaelcosman@gmail.com>
2016-06-12 07:13:08 -0700

    adds calls to reset()

--- gym/envs/safety/semi_supervised_pendulum_noise_test.py
+++ gym/envs/safety/semi_supervised_pendulum_noise_test.py
@@ -2,5 +2,6 @@ from gym.envs.safety.semi_supervised_pendulum_noise import SemiSupervisedPendulu
 import numpy as np
 
 env = SemiSupervisedPendulumNoiseEnv()
+env.reset()
 for i in xrange(100):
     print env.step(np.asarray([1]))
