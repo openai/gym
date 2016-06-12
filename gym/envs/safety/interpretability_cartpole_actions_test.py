e3945640450043fd62560e70f7897d9daf622d74
Rafael Cosman <rafaelcosman@gmail.com>
2016-06-12 09:58:14 -0700

    removes unofficial test files

--- gym/envs/safety/interpretability_cartpole_actions_test.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.safety.interpretability_cartpole_actions import InterpretabilityCartpoleActionsEnv
-
-env = InterpretabilityCartpoleActionsEnv()
-env.reset()
-for i in xrange(100):
-    print env.step([1, 0, 1, 0, 1, 0])
-    print env.step([0, 1, 0, 1, 0, 1])
--- gym/envs/safety/semi_supervised_pendulum_noise_test.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.safety.semi_supervised_pendulum_noise import SemiSupervisedPendulumNoiseEnv
-import numpy as np
-
-env = SemiSupervisedPendulumNoiseEnv()
-env.reset()
-for i in xrange(100):
-    print env.step(np.asarray([1]))
