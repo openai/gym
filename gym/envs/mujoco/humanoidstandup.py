9e00591bf2f9906bc08b30f5c19aa4c8b131267e
lmj <lmj@lmjdeMacBook-Air.local>
2016-06-16 15:56:57 +0800

    standup change to up for still standing

--- gym/envs/mujoco/humanoidstandup.py
+++ gym/envs/mujoco/humanoidstandup.py
@@ -22,7 +22,9 @@ class HumanoidStandupEnv(mujoco_env.MujocoEnv, utils.EzPickle):
                                data.cfrc_ext.flat])
 
     def _step(self, a):
+        qpos_bf = self.model.data.qpos
         self.do_simulation(a, self.frame_skip)
+        qpos = self.model.data.qpos
         pos_after = self.model.data.qpos[2][0]
         data = self.model.data
         uph_cost=(pos_after - 0 ) / self.model.opt.timestep
@@ -31,8 +33,8 @@ class HumanoidStandupEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         quad_impact_cost = .5e-6 * np.square(data.cfrc_ext).sum()
         quad_impact_cost = min(quad_impact_cost, 10)
         reward = uph_cost - quad_ctrl_cost - quad_impact_cost + 1
-
-        done = bool(False)
+        
+        done = bool(qpos_bf[2]>qpos[2]+0.25) 
         return self._get_obs(), reward, done, dict(reward_linup=uph_cost, reward_quadctrl=-quad_ctrl_cost, reward_impact=-quad_impact_cost)
 
     def reset_model(self):
