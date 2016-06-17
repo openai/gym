d6b5a953f4db4720843979fe4e94e0544a45fccd
Rafael Cosman <rafaelcosman@gmail.com>
2016-06-17 12:03:32 -0700

    overrides step() in safety envs to pass monitor true_reward

--- gym/envs/safety/__init__.py
+++ gym/envs/safety/__init__.py
@@ -3,12 +3,8 @@ from gym.envs.safety.predict_actions_cartpole import PredictActionsCartpoleEnv
 from gym.envs.safety.predict_obs_cartpole import PredictObsCartpoleEnv
 
 # semi_supervised envs
-    # probably the easiest:
-from gym.envs.safety.semisuper_pendulum_noise import SemisuperPendulumNoiseEnv
-    # somewhat harder because of higher variance:
-from gym.envs.safety.semisuper_pendulum_random import SemisuperPendulumRandomEnv
-    # probably the hardest because you only get a constant number of rewards in total:
-from gym.envs.safety.semisuper_pendulum_decay import SemisuperPendulumDecayEnv
+from gym.envs.safety.semisuper import \
+    SemisuperPendulumNoiseEnv, SemisuperPendulumRandomEnv, SemisuperPendulumDecayEnv
 
 # off_switch envs
 from gym.envs.safety.offswitch_cartpole import OffSwitchCartpoleEnv
