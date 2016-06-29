15afd9c54aab0fb3d69028045093df20c81c07e3
JKCooper2 <JKCooper2@users.noreply.github.com>
2016-06-29 18:58:25 +1000

    Guessing Game and Hotter Colder toy text environments (#217)

---
+++ gym/envs/toy_text/guessing_game.py
@@ -0,0 +1,87 @@
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+
+
+class GuessingGame(gym.Env):
+    """Number guessing game
+
+    The object of the game is to guess within 1% of the randomly chosen number
+    within 200 time steps
+
+    After each step the agent is provided with one of four possible observations
+    which indicate where the guess is in relation to the randomly chosen number
+
+    0 - No guess yet submitted (only after reset)
+    1 - Guess is lower than the target
+    2 - Guess is equal to the target
+    3 - Guess is higher than the target
+
+    The rewards are:
+    0 if the agent's guess is outside of 1% of the target
+    1 if the agent's guess is inside 1% of the target
+
+    The episode terminates after the agent guesses within 1% of the target or
+    200 steps have been taken
+
+    The agent will need to use a memory of previously submitted actions and observations
+    in order to efficiently explore the available actions
+
+    The purpose is to have agents optimise their exploration parameters (e.g. how far to
+    explore from previous actions) based on previous experience. Because the goal changes
+    each episode a state-value or action-value function isn't able to provide any additional
+    benefit apart from being able to tell whether to increase or decrease the next guess.
+
+    The perfect agent would likely learn the bounds of the action space (without referring
+    to them explicitly) and then follow binary tree style exploration towards to goal number
+    """
+    def __init__(self):
+        self.range = 1000  # Randomly selected number is within +/- this value
+        self.bounds = 10000
+
+        self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]))
+        self.observation_space = spaces.Discrete(4)
+
+        self.number = 0
+        self.guess_count = 0
+        self.guess_max = 200
+        self.observation = 0
+
+        self._seed()
+        self._reset()
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _step(self, action):
+        assert self.action_space.contains(action)
+
+        if action < self.number:
+            self.observation = 1
+
+        elif action == self.number:
+            self.observation = 2
+
+        elif action > self.number:
+            self.observation = 3
+
+        reward = 0
+        done = False
+
+        if (self.number - self.range * 0.01) < action < (self.number + self.range * 0.01):
+            reward = 1
+            done = True
+
+        self.guess_count += 1
+        if self.guess_count >= self.guess_max:
+            done = True
+
+        return self.observation, reward, done, {"number": self.number, "guesses": self.guess_count}
+
+    def _reset(self):
+        self.number = self.np_random.uniform(-self.range, self.range)
+        self.guess_count = 0
+        self.observation = 0
+        return self.observation
--- gym/scoreboard/__init__.py
+++ gym/scoreboard/__init__.py
@@ -602,6 +602,78 @@ add_task(
     contributor='machinaut',
 )
 
+add_task(
+    id='GuessingGame-v0',
+    group='toy_text',
+    experimental=True,
+    contributor='jkcooper2',
+    summary='Guess close to randomly selected number',
+    description='''
+    The goal of the game is to guess within 1% of the randomly
+    chosen number within 200 time steps
+
+    After each step the agent is provided with one of four possible
+    observations which indicate where the guess is in relation to
+    the randomly chosen number
+
+    0 - No guess yet submitted (only after reset)
+    1 - Guess is lower than the target
+    2 - Guess is equal to the target
+    3 - Guess is higher than the target
+
+    The rewards are:
+    0 if the agent's guess is outside of 1% of the target
+    1 if the agent's guess is inside 1% of the target
+
+    The episode terminates after the agent guesses within 1% of
+    the target or 200 steps have been taken
+
+    The agent will need to use a memory of previously submitted
+    actions and observations in order to efficiently explore
+    the available actions.
+    ''',
+    background='''
+    The purpose is to have agents able to optimise their exploration
+    parameters based on histories. Since the observation only provides
+    at most the direction of the next step agents will need to alter
+    they way they explore the environment (e.g. binary tree style search)
+    in order to achieve a good score
+    '''
+)
+
+add_task(
+    id='HotterColder-v0',
+    group='toy_text',
+    experimental=True,
+    contributor='jkcooper2',
+    summary='Guess close to a random selected number using hints',
+    description='''
+    The goal of the game is to effective use the reward provided
+    in order to understand the best action to take.
+
+    After each step the agent receives an observation of:
+    0 - No guess yet submitted (only after reset)
+    1 - Guess is lower than the target
+    2 - Guess is equal to the target
+    3 - Guess is higher than the target
+
+    The rewards is calculated as:
+    ((min(action, self.number) + self.bounds) / (max(action, self.number) + self.bounds)) ** 2
+    This is essentially the squared percentage of the way the
+    agent has guessed toward the target.
+
+    Ideally an agent will be able to recognise the 'scent' of a
+    higher reward and increase the rate in which is guesses in that
+    direction until the reward reaches its maximum.
+    ''',
+    background='''
+    It is possible to reach the maximum reward within 2 steps if
+    an agent is capable of learning the reward dynamics (one to
+    determine the direction of the target, the second to jump
+    directly to the target based on the reward).
+    '''
+)
+
 ram_desc = "In this environment, the observation is the RAM of the Atari machine, consisting of (only!) 128 bytes."
 image_desc = "In this environment, the observation is an RGB image of the screen, which is an array of shape (210, 160, 3)"
 
