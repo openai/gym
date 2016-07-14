acee56254ea69af38cd2af0e468bd5d28bfeae53
IkerZamora <ikerzamora94@gmail.com>
2016-07-14 12:46:50 +0200

    Add installation script

---
+++ gym/envs/gazebo/installation/setup.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source /opt/ros/indigo/setup.bash
+
+# Create catkin_ws
+ws="catkin_ws"
+if [ -d $ws ]; then
+  echo "Error: catkin_ws directory already exists" 1>&2
+  exit 1
+fi
+src=$ws"/src"
+mkdir -p $src
+cd $src
+catkin_init_workspace
+cd ..
+catkin_make_isolated
+cd src
+
+# Install dependencies
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install python-vcstool ros-indigo-sophus libusb-dev libftdi-dev libsdl-image1.2-dev ros-indigo-bfl libspnav-dev pyqt4-dev-tools git
+
+# Import and build dependencies
+vcs import < ../../gazebo.repos
+cd ..
+source devel_isolated/setup.bash
+catkin_make_isolated
+
+exit 0
+
+
