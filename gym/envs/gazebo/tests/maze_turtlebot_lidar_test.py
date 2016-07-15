a47e568d365dc922424fb15954aac5b0b5167f90
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-07-15 16:58:55 +0200

    first _step implementation & simple algorithm test

--- gym/envs/gazebo/tests/maze_turtlebot_lidar_test.py
+++ gym/envs/gazebo/tests/maze_turtlebot_lidar_test.py
@@ -3,8 +3,37 @@ import time
 
 env = gym.make('GazeboMazeTurtlebotLidar-v0')
 
-time.sleep(5)
 
+for x in range(3000):
+    done = False
+    env.reset()
+
+    #Show render every 200 steps for 50 iterations
+    print "Iteration: "+str(x)
+    if (x%100 == 0) and (x != 0):
+        print "starting render"
+        env.render()
+    elif (x%50 == 0) and (x != 0) and (x != 50):
+        print "closing render"
+        env.render(close=True)
+
+
+    for i in range(500):
+
+        #action = env.action_space.sample() #not implemented
+        #observation, reward, done, info = env.step(action)
+
+        env.step(1)
+
+        if (i%25 == 0) and (i != 0):
+            done = True
+        if done:
+            break 
+
+env.close()
+
+
+'''
 print "Render starting"
 env.render()
 
@@ -17,4 +46,4 @@ print "Render starting"
 env.render(close=True)
 
 
-env.close()
+env.close()'''
