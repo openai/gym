a15aaea248e92491a6ca762770463f6243fe4d21
IkerZamora <ikerzamora94@gmail.com>
2016-07-15 11:44:29 +0200

    init+test implemented

--- gym/envs/gazebo/gazebo_maze_turtlebot_lidar.py
+++ gym/envs/gazebo/gazebo_maze_turtlebot_lidar.py
@@ -5,45 +5,46 @@ from gym.envs.gazebo import gazebo_env
 
 class GazeboMazeTurtlebotLidarEnv(gazebo_env.GazeboEnv):
 
-	def __init__(self):
+    def __init__(self):
 
-		# Launch the simulation with the given launchfile name
-		
+        # Launch the simulation with the given launchfile name
+        gazebo_env.GazeboEnv.__init__(self, "GazeboMazeTurtlebotLidar_v0.launch")
 
-	def _spawn_robot(self):
+    def _spawn_robot(self):
 
-		# TODO
-		# Spawn robot
-		# Optional
-		# Another option is to spawn the robot in the __init__ method
+        # TODO
+        # Spawn robot
+        # Optional
+        # Another option is to spawn the robot in the __init__ method
+        pass
+    def _step(self, action):
 
-	def _step(self, action):
+        # TODO
+        # Perform a step in gazebo. E.g. move the robot
+        pass
+    def _reset(self):
 
-		# TODO
-		# Perform a step in gazebo. E.g. move the robot
+        # TODO
+        # Reset world/simulation
+        pass
+    def _render(self, episodes):
 
-	def _reset(self):
+        # Open GUI (if it's not allready opened?)
+        # episodes = number of episodes that GUI is going to be opened. Another option is to use _close to close the gui
+        pass
+    def _close(self):
 
-		# TODO
-		# Reset world/simulation
+        # TODO
+        # From OpenAI API: Perform any necessary cleanup
+        pass
+    def _configure(self):
 
-	def _render(self, episodes):
-
-		# Open GUI (if it's not allready opened?)
-		# episodes = number of episodes that GUI is going to be opened. Another option is to use _close to close the gui
-
-	def _close(self):
-
-		# TODO
-		# From OpenAI API: Perform any necessary cleanup
-
-	def _configure(self):
-
-		# TODO
-		# From OpenAI API: Provides runtime configuration to the enviroment
-		# Maybe set the Real Time Factor?
-
-	def _seed(self):
-		
-		# TODO
-		# From OpenAI API: Sets the seed for this env's random number generator(s)	
+        # TODO
+        # From OpenAI API: Provides runtime configuration to the enviroment
+        # Maybe set the Real Time Factor?
+        pass
+    def _seed(self):
+        
+        # TODO
+        # From OpenAI API: Sets the seed for this env's random number generator(s)  
+        pass
