a6afa7c026173c76b68f206e65681a515794650b
IkerZamora <ikerzamora94@gmail.com>
2016-07-15 16:47:42 +0200

    Add packages from #4 Also order the packages alphabetically

--- gym/envs/gazebo/installation/setup.sh
+++ gym/envs/gazebo/installation/setup.sh
@@ -20,7 +20,20 @@ cd src
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install python-vcstool ros-indigo-sophus libusb-dev libftdi-dev libsdl-image1.2-dev ros-indigo-bfl libspnav-dev pyqt4-dev-tools git libtbb-dev libtbb2
+sudo apt-get install git                            \
+                     libsdl-image1.2-dev            \
+                     libspnav-dev                   \
+                     libtbb-dev                     \
+                     libtbb2                        \
+                     libusb-dev libftdi-dev         \
+                     pyqt4-dev-tools                \
+                     python-vcstool                 \
+                     ros-indigo-bfl                 \
+                     ros-indigo-camera-info-manager \
+                     ros-indigo-driver-base         \
+                     ros-indigo-pcl-ros             \
+                     ros-indigo-polled-camera       \
+                     ros-indigo-sophus
 
 # Import and build dependencies
 vcs import < ../../gazebo.repos
