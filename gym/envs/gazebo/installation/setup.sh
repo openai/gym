72964c7d325a541643d5dea06b653084de047da9
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-07-19 09:58:08 +0200

    add env var needed for launch file

--- gym/envs/gazebo/installation/setup.sh
+++ gym/envs/gazebo/installation/setup.sh
@@ -43,4 +43,10 @@ catkin_make_isolated
 chmod +x $src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
 bash $src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
 
+#add turtlebot launch environment variable
+if [ -z "$GYM_GAZEBO_WORLD_MAZE" ]; then
+  bash -c 'echo "export GYM_GAZEBO_WORLD_MAZE="`pwd`/../assets/worlds/maze.world >> ~/.bashrc'
+  exec bash #reload bashrc
+fi
+
 #exit 0
