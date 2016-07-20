ecbc6881cd0c9ae67c94e1a095e6e36d736ac87a
Nestor Gonzalez <nestor94gonzalez@gmail.com>
2016-07-20 11:47:36 +0200

    Update setup.sh

--- gym/envs/gazebo/installation/setup.sh
+++ gym/envs/gazebo/installation/setup.sh
@@ -12,9 +12,6 @@ src=$ws"/src"
 mkdir -p $src
 cd $src
 catkin_init_workspace
-cd ..
-catkin_make_isolated
-cd src
 
 # Install dependencies
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
