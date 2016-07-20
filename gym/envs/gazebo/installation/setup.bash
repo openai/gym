06aa8450cc6405d7250589c4dab28afb38a9610a
Nestor Gonzalez <nestor94gonzalez@gmail.com>
2016-07-20 16:02:39 +0200

    Update setup.bash add -y option to apt-get install

--- gym/envs/gazebo/installation/setup.bash
+++ gym/envs/gazebo/installation/setup.bash
@@ -17,16 +17,16 @@ catkin_init_workspace
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install git                            \
-                     libsdl-image1.2-dev            \
-                     libspnav-dev                   \
-                     libtbb-dev                     \
-                     libtbb2                        \
-                     libusb-dev libftdi-dev         \
-                     pyqt4-dev-tools                \
-                     python-vcstool                 \
-                     ros-indigo-bfl                 \
-                     python-pip
+sudo apt-get install -y git                            \
+                        libsdl-image1.2-dev            \
+                        libspnav-dev                   \
+                        libtbb-dev                     \
+                        libtbb2                        \
+                        libusb-dev libftdi-dev         \
+                        pyqt4-dev-tools                \
+                        python-vcstool                 \
+                        ros-indigo-bfl                 \
+                        python-pip
 sudo pip install numpy --upgrade
 sudo pip install pandas
 # Import and build dependencies
