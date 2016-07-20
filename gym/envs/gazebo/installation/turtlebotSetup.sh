d1fd9a7035460d0a7e90bfb83ce5494fc3cb4b0a
IkerZamora <ikerzamora94@gmail.com>
2016-07-20 11:32:16 +0200

    Fix turtlebot setup script

--- gym/envs/gazebo/installation/turtlebotSetup.sh
+++ gym/envs/gazebo/installation/turtlebotSetup.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# Create catkin_ws
-ws="catkin_ws"
-if [ -d $ws ]; then
-  echo "Error: catkin_ws directory already exists" 1>&2
-  exit 1
-fi
-src=$ws"/src"
+sh catkin_ws/devel_isolated/setup.sh
 
 if [ -z "$GAZEBO_MODEL_PATH" ]; then
   bash -c 'echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:"`pwd`/../assets/models >> ~/.bashrc'
@@ -14,8 +8,8 @@ if [ -z "$GAZEBO_MODEL_PATH" ]; then
 fi
 
 #Load turtlebot variables. Temporal solution
-chmod +x $src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
-bash $src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
+chmod +x catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
+bash catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
 
 #add turtlebot launch environment variable
 if [ -z "$GYM_GAZEBO_WORLD_MAZE" ]; then
@@ -24,7 +18,7 @@ if [ -z "$GYM_GAZEBO_WORLD_MAZE" ]; then
 fi
 
 #copy altered urdf model
-bash -c "cp -r ../assets/urdf/kobuki_urdf/urdf/ catkin_ws/src/kobuki/kobuki_description"
+cp -r ../assets/urdf/kobuki_urdf/urdf/ catkin_ws/src/kobuki/kobuki_description
 
 if [ -z "$GAZEBO_PLUGIN_PATH" ]; then
   bash -c 'echo "export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:"`pwd`/catkin_ws/build_isolated/iterations_gazebo_plugin >> ~/.bashrc'
