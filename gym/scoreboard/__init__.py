039fb2c290103b3f49ec1f6d1950c027b4ba7d04
rafaelcosman <rafaelcosman@google.com>
2016-07-22 16:56:14 -0700

    Adds description to NChain-V0

--- gym/scoreboard/__init__.py
+++ gym/scoreboard/__init__.py
@@ -593,6 +593,26 @@ add_task(
     group='toy_text',
     experimental=True,
     contributor='machinaut',
+    description="""
+        n-Chain environment
+
+        This game presents moves along a linear chain of states, with two actions:
+         0) forward, which moves along the chain but returns no reward
+         1) backward, which returns to the beginning and has a small reward
+
+        The end of the chain, however, presents a large reward, and by moving
+        'forward' at the end of the chain this large reward can be repeated.
+
+        At each action, there is a small probability that the agent 'slips' and the
+        opposite transition is instead taken.
+
+        The observed state is the current state in the chain (0 to n-1).
+        """,
+    background="""
+        This environment is described in section 6.1 of:
+        A Bayesian Framework for Reinforcement Learning by Malcolm Strens (2000)
+        http://ceit.aut.ac.ir/~shiry/lecture/machine-learning/papers/BRL-2000.pdf
+        """
 )
 
 add_task(
