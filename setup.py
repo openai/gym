645f5a0758204e3ed28a7329ce05eddb03dfc4f7
Jonas Schneider <mail@jonasschneider.com>
2016-07-27 13:57:35 -0700

    Pin scipy

--- setup.py
+++ setup.py
@@ -5,6 +5,23 @@ import sys, os.path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym'))
 from version import VERSION
 
+# Environment-specific dependencies.
+extras = {
+  'atari': ['atari_py>=0.0.17', 'Pillow', 'PyOpenGL'],
+  'board_game' : ['pachi-py>=0.0.19'],
+  'box2d': ['box2d-py'],
+  'classic_control': ['PyOpenGL'],
+  'doom': ['doom_py>=0.0.11'],
+  'mujoco': ['mujoco_py>=0.4.3', 'imageio'],
+  'parameter_tuning': ['keras', 'theano'],
+}
+
+# Meta dependency groups.
+all_deps = []
+for group_name in extras:
+    all_deps += extras[group_name]
+extras['all'] = all_deps
+
 setup(name='gym',
       version=VERSION,
       description='The OpenAI Gym: A toolkit for developing and comparing your reinforcement learning agents.',
@@ -18,24 +35,7 @@ setup(name='gym',
       install_requires=[
           'numpy>=1.10.4', 'requests>=2.0', 'six', 'pyglet>=1.2.0',
       ],
-      extras_require={
-          'all': ['atari_py>=0.0.17', 'Pillow', 'PyOpenGL',
-                  'pachi-py>=0.0.19',
-                  'box2d-py',
-                  'doom_py>=0.0.11',
-                  'mujoco_py>=0.5.2', 'imageio',
-                  'keras', 'theano'],
-
-          # Environment-specific dependencies. Keep these in sync with
-          # 'all'!
-          'atari': ['atari_py>=0.0.17', 'Pillow', 'PyOpenGL'],
-          'board_game' : ['pachi-py>=0.0.19'],
-          'box2d': ['box2d-py'],
-          'classic_control': ['PyOpenGL'],
-          'doom': ['doom_py>=0.0.11'],
-          'mujoco': ['mujoco_py>=0.4.3', 'imageio'],
-          'parameter_tuning': ['keras', 'theano'],
-      },
+      extras_require=extras,
       package_data={'gym': ['envs/mujoco/assets/*.xml', 'envs/classic_control/assets/*.png', 'envs/doom/assets/*.cfg']},
       tests_require=['nose2', 'mock'],
 )
