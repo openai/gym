8812a9472a07969be6030bdb18368524ed6287a9
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-08-03 10:42:24 +0200

    rename tests

---
+++ gym/envs/gazebo/tests/circuit2_turtlebot_lidar_sarsa.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import gym
+import time
+import numpy
+import random
+import time
+
+import matplotlib
+import matplotlib.pyplot as plt
+import sarsa
+
+
+if __name__ == '__main__':
+
+    env = gym.make('GazeboCircuit2TurtlebotLidar-v0')
+
+    outdir = '/tmp/gazebo_gym_experiments'
+    env.monitor.start(outdir, force=True, seed=None)
+    #plotter = LivePlot(outdir)
+
+    last_time_steps = numpy.ndarray(0)
+
+    sarsa = sarsa.Sarsa(actions=range(env.action_space.n),
+                    epsilon=0.9, alpha=0.2, gamma=0.9)
+
+    initial_epsilon = sarsa.epsilon
+
+    epsilon_discount = 0.9986
+
+    start_time = time.time()
+    total_episodes = 10000
+    highest_reward = 0
+
+    for x in range(total_episodes):
+        done = False
+
+        cumulated_reward = 0 #Should going forward give more reward then L/R ?
+
+        observation = env.reset()
+
+        if sarsa.epsilon > 0.05:
+            sarsa.epsilon *= epsilon_discount
+
+        #render() #defined above, not env.render()
+
+        state = ''.join(map(str, observation))
+
+        for i in range(1500):
+
+            # Pick an action based on the current state
+            action = sarsa.chooseAction(state)
+
+            # Execute the action and get feedback
+            observation, reward, done, info = env.step(action)
+            cumulated_reward += reward
+
+            if highest_reward < cumulated_reward:
+                highest_reward = cumulated_reward
+
+            nextState = ''.join(map(str, observation))
+            nextAction = sarsa.chooseAction(nextState)
+       
+            #sarsa.learn(state, action, reward, nextState)
+            sarsa.learn(state, action, reward, nextState, nextAction)
+
+            if not(done):
+                state = nextState
+            else:
+                last_time_steps = numpy.append(last_time_steps, [int(i + 1)])
+                break 
+
+        #if x%100==0:
+        #    plotter.plot()
+
+        m, s = divmod(int(time.time() - start_time), 60)
+        h, m = divmod(m, 60)
+        print "EP: "+str(x+1)+" - [alpha: "+str(round(sarsa.alpha,2))+" - gamma: "+str(round(sarsa.gamma,2))+" - epsilon: "+str(round(sarsa.epsilon,2))+"] - Reward: "+str(cumulated_reward)+"     Time: %d:%02d:%02d" % (h, m, s)
+
+    #Github table content
+    print "\n|"+str(total_episodes)+"|"+str(sarsa.alpha)+"|"+str(sarsa.gamma)+"|"+str(initial_epsilon)+"*"+str(epsilon_discount)+"|"+str(highest_reward)+"| PICTURE |"
+
+    l = last_time_steps.tolist()
+    l.sort()
+
+    #print("Parameters: a="+str)
+    print("Overall score: {:0.2f}".format(last_time_steps.mean()))
+    print("Best 100 score: {:0.2f}".format(reduce(lambda x, y: x + y, l[-100:]) / len(l[-100:])))
+
+    env.monitor.close()
+    env.close()
---
+++ gym/envs/gazebo/tests/sarsa.py
@@ -0,0 +1,40 @@
+import random
+
+class Sarsa:
+    def __init__(self, actions, epsilon, alpha, gamma):
+        self.q = {}
+
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.actions = actions
+
+    def getQ(self, state, action):
+        return self.q.get((state, action), 0.0)
+
+    def learnQ(self, state, action, reward, value):
+        oldv = self.q.get((state, action), None)
+        if oldv is None:
+            self.q[(state, action)] = reward 
+        else:
+            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
+
+    def chooseAction(self, state):
+        if random.random() < self.epsilon:
+            action = random.choice(self.actions)
+        else:
+            q = [self.getQ(state, a) for a in self.actions]
+            maxQ = max(q)
+            count = q.count(maxQ)
+            if count > 1:
+                best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+                i = random.choice(best)
+            else:
+                i = q.index(maxQ)
+
+            action = self.actions[i]
+        return action
+
+    def learn(self, state1, action1, reward, state2, action2):
+        qnext = self.getQ(state2, action2)
+        self.learnQ(state1, action1, reward, reward + self.gamma * qnext)
