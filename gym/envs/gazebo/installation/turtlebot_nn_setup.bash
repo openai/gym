fd570743edb5ee8b72bc5339acd9b5004c5ae5d7
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-08-23 11:06:27 +0200

    Deep Q-learning first approach

---
+++ gym/envs/gazebo/installation/turtlebot_nn_setup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -z "$GAZEBO_MODEL_PATH" ]; then
+  bash -c 'echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:"`pwd`/../assets/models >> ~/.bashrc'
+fi
+
+#Load turtlebot variables. Temporal solution
+chmod +x catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
+bash catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/env-hooks/25.turtlebot-gazebo.sh.em
+
+#add turtlebot launch environment variable
+if [ -z "$GYM_GAZEBO_WORLD_MAZE" ]; then
+  bash -c 'echo "export GYM_GAZEBO_WORLD_MAZE="`pwd`/../assets/worlds/maze.world >> ~/.bashrc'
+fi
+if [ -z "$GYM_GAZEBO_WORLD_CIRCUIT" ]; then
+  bash -c 'echo "export GYM_GAZEBO_WORLD_CIRCUIT="`pwd`/../assets/worlds/circuit.world >> ~/.bashrc'
+fi
+if [ -z "$GYM_GAZEBO_WORLD_CIRCUIT2" ]; then
+  bash -c 'echo "export GYM_GAZEBO_WORLD_CIRCUIT2="`pwd`/../assets/worlds/circuit2.world >> ~/.bashrc'
+fi
+if [ -z "$GYM_GAZEBO_WORLD_ROUND" ]; then
+  bash -c 'echo "export GYM_GAZEBO_WORLD_ROUND="`pwd`/../assets/worlds/round.world >> ~/.bashrc'
+fi
+
+#copy altered urdf model
+cp -r ../assets/urdf/kobuki_nn_urdf/urdf/ catkin_ws/src/kobuki/kobuki_description
+
+#copy laser mesh file
+cp ../assets/meshes/lidar_lite_v2_withRay.dae catkin_ws/src/kobuki/kobuki_description/meshes
+ 
+exec bash # reload bash
+
---
+++ gym/envs/gazebo/tests/circuit2_turtlebot_lidar_dqn.py
@@ -0,0 +1,79 @@
+'''
+Based on: https://github.com/vmayoral/basic_reinforcement_learning
+'''
+
+import gym
+import deepq
+
+if __name__ == '__main__':
+
+    env = gym.make('GazeboCircuit2TurtlebotLidarNn-v0')
+
+    outdir = '/tmp/gazebo_gym_experiments'
+    env.monitor.start(outdir, force=True, seed=None)
+    #Each time we take a sample and update our weights it is called a mini-batch. 
+    #Each time we run through the entire dataset, it's called an epoch.
+    epochs = 1000
+    steps = 1500
+    updateTargetNetwork = 10000
+    explorationRate = 1 #epsilon
+    minibatch_size = 128
+    learnStart = 128
+    learningRate = 0.00025
+    discountFactor = 0.99
+    memorySize = 1000000
+
+    last100Scores = [0] * 100
+    last100ScoresIndex = 0
+    last100Filled = False
+
+    # turtlebot_nn_setup.bash must be executed.
+    deepQ = deepq.DeepQ(100, 2, memorySize, discountFactor, learningRate, learnStart)
+    # deepQ.initNetworks([30,30,30])
+    # deepQ.initNetworks([30,30])
+    deepQ.initNetworks([300,300])
+
+    stepCounter = 0
+
+    # number of reruns
+    for epoch in xrange(epochs):
+        observation = env.reset()
+        # number of timesteps
+        for t in xrange(steps):
+            # env.render()
+            qValues = deepQ.getQValues(observation)
+
+            action = deepQ.selectAction(qValues, explorationRate)
+
+            newObservation, reward, done, info = env.step(action)
+
+            deepQ.addMemory(observation, action, reward, newObservation, done)
+
+            if stepCounter >= learnStart:
+                if stepCounter <= updateTargetNetwork:
+                    deepQ.learnOnMiniBatch(minibatch_size, False)
+                else :
+                    deepQ.learnOnMiniBatch(minibatch_size, True)
+
+            observation = newObservation
+
+            if done:
+                last100Scores[last100ScoresIndex] = t
+                last100ScoresIndex += 1
+                if last100ScoresIndex >= 100:
+                    last100Filled = True
+                    last100ScoresIndex = 0
+                if not last100Filled:
+                    print ("Episode "+str(epoch)+" finished after {} timesteps".format(t+1)+"   Epsilon="+str(round(explorationRate, 2)))
+                else :
+                    print ("Episode "+str(epoch)+" finished after {} timesteps".format(t+1)+" last 100 average: ",(sum(last100Scores)/len(last100Scores)))
+                break
+
+            stepCounter += 1
+            if stepCounter % updateTargetNetwork == 0:
+                deepQ.updateTargetNetwork()
+                print ("updating target network")
+
+        explorationRate *= 0.995 #epsilon decay
+        # explorationRate -= (2.0/epochs)
+        explorationRate = max (0.05, explorationRate)
