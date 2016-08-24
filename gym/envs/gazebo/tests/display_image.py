6759fd47d136f7a8468ce7cdec1263afe1c27b75
IkerZamora <ikerzamora94@gmail.com>
2016-08-24 12:45:50 +0200

    Update display_image.py - Allow all the possible combinations with thethree plot types - When no plot is specified, plot full data

--- gym/envs/gazebo/tests/display_image.py
+++ gym/envs/gazebo/tests/display_image.py
@@ -33,11 +33,22 @@ class LivePlot(object):
         fig = plt.gcf().canvas.set_window_title('averaged_simulation_graph')
         matplotlib.rcParams.update({'font.size': 15})
 
-    def plot(self, average, both=False, interpolated=False):
+    def plot(self, full=True, average=0, interpolated=False):
         results = gym.monitoring.monitor.load_results(self.outdir)
         data =  results[self.data_key]
         avg_data = []
 
+        if full:
+            plt.plot(data, color='blue')
+        if average > 0:
+            average = int(average)
+            for i, val in enumerate(data):
+                if i%average==0:
+                    if (i+average) < len(data):
+                        avg =  sum(data[i:i+average])/average
+                        avg_data.append(avg)
+            new_data = expand(avg_data,average)
+            plt.plot(new_data, color='red', linewidth=2.5) 
         if interpolated:
             avg_data = []
             avg_data_points = []
@@ -63,26 +74,7 @@ class LivePlot(object):
             #print data_fix
             interp = pchip(avg_data_points, avg_data)
             xx = np.linspace(0, len(data)-data_fix, 1000)
-            plt.plot(xx, interp(xx), color='green', linewidth=2.5)
-        else:
-            if average == None:
-                average = len(data)/50
-                if average == 0:
-                    average = 1
-            else:
-                average=int(average)
-            for i, val in enumerate(data):
-                if i%average==0:
-                    if (i+average) < len(data):
-                        avg =  sum(data[i:i+average])/average
-                        avg_data.append(avg)
-            new_data = expand(avg_data,average)
-
-            if both: #both avg and full
-                plt.plot(data, color='blue')
-                plt.plot(new_data, color='red', linewidth=2.5)
-            else:
-                plt.plot(new_data, color=self.line_color)          
+            plt.plot(xx, interp(xx), color='green', linewidth=3.5)        
 
         # pause so matplotlib will display
         # may want to figure out matplotlib animation or use a different library in the future
@@ -102,17 +94,15 @@ if __name__ == '__main__':
     plotter = LivePlot(outdir)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--average", type=int, help="plot an averaged graph using the given number as average size delimiter")
-    parser.add_argument("-b", "--both", action='store_true', help="print both graphs, the averaged given by -a and the full data plot")
+    parser.add_argument("-f", "--full", action='store_true', help="print the full data plot")
+    parser.add_argument("-a", "--average", type=int, metavar="AVG", help="plot an averaged graph using the given integer as average size delimiter")
     parser.add_argument("-i", "--interpolated", action='store_true', help="print an interpolated graph")
     args = parser.parse_args()
 
-    if args.average > 0:
-        if args.both:
-            plotter.plot(args.average, both=True)
-        else:
-            plotter.plot(args.average)
-    elif args.interpolated:
-        plotter.plot(None, interpolated=True)
+    if not args.funcs:
+        # When no arguments given, plot full data graph
+        plotter.plot(full=True)
+    else:
+        plotter.plot(full=args.full, average=args.average, interpolated=args.interpolated)
 
     pause()
