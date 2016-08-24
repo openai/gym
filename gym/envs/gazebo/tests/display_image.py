7f6fdd2e678eb5da24f5761917bfecc4b90e1958
IkerZamora <ikerzamora94@gmail.com>
2016-08-24 11:22:34 +0200

    Clean code and add argparse

--- gym/envs/gazebo/tests/display_image.py
+++ gym/envs/gazebo/tests/display_image.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python
-'''
-This script plots environemnt monitoring data obtained from 
-'/tmp/gazebo_gym_experiments/', which has been created when 
-calling a gazebo environment monitoring function in a test.
 
-Args:
-
-  default    Plots an averaged graph.
-  arg1='i'   Prints an interpolated graph.
-
-  arg1=int   Plots an averaged graph using 'arg1' as average 
-             size delimiter.
-  arg2='b'   Prints both graphs, the averaged one using 'arg1' 
-             and the full data plot. 'arg2' must be 'b'.
-  
-Examples:
-
-  python display_image.py
-  python display_image.py i
-  python display_image.py 20
-  python display_image.py 20 b
-
-'''
 import os
 import gym
 import matplotlib
@@ -55,60 +33,56 @@ class LivePlot(object):
         fig = plt.gcf().canvas.set_window_title('averaged_simulation_graph')
         matplotlib.rcParams.update({'font.size': 15})
 
-    def plot(self, mod1, mod2):
+    def plot(self, average, both=False, interpolated=False):
         results = gym.monitoring.monitor.load_results(self.outdir)
         data =  results[self.data_key]
         avg_data = []
 
-        if mod1 == None or mod1.isdigit():
-            if mod1 == None:
-                mod1 = len(data)/50
-                if mod1 == 0:
-                    mod1 = 1
-            else:
-                mod1=int(mod1)
-            for i, val in enumerate(data):
-                if i%mod1==0:
-                    if (i+mod1) < len(data):
-                        avg =  sum(data[i:i+mod1])/mod1
-                        avg_data.append(avg)
-            new_data = expand(avg_data,mod1)
-            if mod2 == 'b': #both avg and full
-                plt.plot(data, color='blue')
-                plt.plot(new_data, color='red', linewidth=2.5)
-            else:
-                plt.plot(new_data, color=self.line_color)
-
-        elif mod1 == 'i': #interpolate data
+        if interpolated:
             avg_data = []
             avg_data_points = []
-            mod1 = len(data)/50
-            if mod1 == 0:
-                mod1 = 1
+            n = len(data)/50
+            if n == 0:
+                n = 1
             data_fix = 0
             for i, val in enumerate(data):
-                if i%mod1==0:
-                    if (i+mod1) < len(data):
-                        avg =  sum(data[i:i+mod1])/mod1
+                if i%n==0:
+                    if (i+n) < len(data):
+                        avg =  sum(data[i:i+n])/n
                         avg_data.append(avg)
                         avg_data_points.append(i)
-                if (i+mod1) == len(data):
-                    data_fix = mod1
+                if (i+n) == len(data):
+                    data_fix = n
 
             
             x = np.arange(len(avg_data))
             y = np.array(avg_data)
             #print x
             #print y
-            #print str(len(avg_data)*mod1)
+            #print str(len(avg_data)*n)
             #print data_fix
             interp = pchip(avg_data_points, avg_data)
             xx = np.linspace(0, len(data)-data_fix, 1000)
             plt.plot(xx, interp(xx), color='green', linewidth=2.5)
+        else:
+            if average == None:
+                average = len(data)/50
+                if average == 0:
+                    average = 1
+            else:
+                average=int(average)
+            for i, val in enumerate(data):
+                if i%average==0:
+                    if (i+average) < len(data):
+                        avg =  sum(data[i:i+average])/average
+                        avg_data.append(avg)
+            new_data = expand(avg_data,average)
 
-        
-
-        
+            if both: #both avg and full
+                plt.plot(data, color='blue')
+                plt.plot(new_data, color='red', linewidth=2.5)
+            else:
+                plt.plot(new_data, color=self.line_color)          
 
         # pause so matplotlib will display
         # may want to figure out matplotlib animation or use a different library in the future
@@ -126,10 +100,19 @@ if __name__ == '__main__':
 
     outdir = '/tmp/gazebo_gym_experiments'
     plotter = LivePlot(outdir)
-    if len(sys.argv)==1:
-        plotter.plot(None, None)
-    elif len(sys.argv)==2:
-        plotter.plot(sys.argv[1], None)
-    else:
-        plotter.plot(sys.argv[1], sys.argv[2])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--average", type=int, help="plot an averaged graph using the given number as average size delimiter")
+    parser.add_argument("-b", "--both", action='store_true', help="print both graphs, the averaged given by -a and the full data plot")
+    parser.add_argument("-i", "--interpolated", action='store_true', help="print an interpolated graph")
+    args = parser.parse_args()
+
+    if args.average > 0:
+        if args.both:
+            plotter.plot(args.average, both=True)
+        else:
+            plotter.plot(args.average)
+    elif args.interpolated:
+        plotter.plot(None, interpolated=True)
+
     pause()
