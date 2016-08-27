c68a6a7e27cb0ff27e7592f3e647b1d7e729ccdc
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-08-28 00:51:21 +0200

    add keras to setup

--- gym/envs/gazebo/installation/setup.bash
+++ gym/envs/gazebo/installation/setup.bash
@@ -107,6 +107,7 @@ sudo apt-get install gfortran
 git clone git://github.com/Theano/Theano.git
 cd Theano/
 sudo python setup.py develop
+sudo pip install keras
 
 echo "## Theano and Keras installed ##"
 echo "## Installation finished ##"
