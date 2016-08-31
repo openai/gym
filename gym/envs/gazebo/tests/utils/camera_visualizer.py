f9ef68456abef2c5ab019bf04dc2d97213c8731e
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-08-31 14:36:07 +0200

    add camera_visualizer util

---
+++ gym/envs/gazebo/tests/utils/camera_visualizer.py
@@ -0,0 +1,38 @@
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    #cv_image = cv2.resize(cv_image, (64, 48))
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
