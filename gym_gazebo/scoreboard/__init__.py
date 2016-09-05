fd0da2fdd51e1cece25d1bbcc840c68278059cd9
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-09-05 17:14:21 +0200

    comment unnecessary code

--- gym_gazebo/scoreboard/__init__.py
+++ gym_gazebo/scoreboard/__init__.py
@@ -12,9 +12,9 @@ from gym.scoreboard.registration import registry, add_task, add_group
 
 # Discover API key from the environment. (You should never have to
 # change api_base / web_base.)
-api_key = os.environ.get('OPENAI_GYM_API_KEY')
+'''api_key = os.environ.get('OPENAI_GYM_API_KEY')
 api_base = os.environ.get('OPENAI_GYM_API_BASE', 'https://gym-api.openai.com')
-web_base = os.environ.get('OPENAI_GYM_WEB_BASE', 'https://gym.openai.com')
+web_base = os.environ.get('OPENAI_GYM_WEB_BASE', 'https://gym.openai.com')'''
 
 # The following controls how various tasks appear on the
 # scoreboard. These registrations can differ from what's registered in
