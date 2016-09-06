0f4a38f06e934666f937f90d4a835f0e086434fc
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-09-06 15:51:12 +0200

    update CNN changes manually

--- gym_gazebo/envs/gazebo_circuit2c_turtlebot_camera_nn.py
+++ gym_gazebo/envs/gazebo_circuit2c_turtlebot_camera_nn.py
@@ -4,6 +4,7 @@ import roslaunch
 import time
 import numpy as np
 import cv2
+import sys
 
 from gym import utils, spaces
 from gym_gazebo.envs import gazebo_env
@@ -13,6 +14,11 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
 from gym.utils import seeding
 from cv_bridge import CvBridge, CvBridgeError
+
+import skimage as skimage
+from skimage import transform, color, exposure
+from skimage.transform import rotate
+from skimage.viewer import ImageViewer
 
 class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
 
@@ -47,13 +53,30 @@ class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
         except rospy.ServiceException, e:
             print ("/gazebo/unpause_physics service call failed")
 
-        max_ang_speed = 0.3
+        '''max_ang_speed = 0.3
         ang_vel = (action-10)*max_ang_speed*0.1 #from (-0.33 to + 0.33)
 
         vel_cmd = Twist()
         vel_cmd.linear.x = 0.2
         vel_cmd.angular.z = ang_vel
-        self.vel_pub.publish(vel_cmd)
+        self.vel_pub.publish(vel_cmd)'''
+
+        if action == 0: #FORWARD
+            vel_cmd = Twist()
+            vel_cmd.linear.x = 0.2
+            vel_cmd.angular.z = 0.0
+            self.vel_pub.publish(vel_cmd)
+        elif action == 1: #LEFT
+            vel_cmd = Twist()
+            vel_cmd.linear.x = 0.05
+            vel_cmd.angular.z = 0.3
+            self.vel_pub.publish(vel_cmd)
+        elif action == 2: #RIGHT
+            vel_cmd = Twist()
+            vel_cmd.linear.x = 0.05
+            vel_cmd.angular.z = -0.3
+            self.vel_pub.publish(vel_cmd)
+
         data = None
         while data is None:
             try:
@@ -73,6 +96,9 @@ class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
                 #temporal fix
                 if not (cv_image[240,320,0]==178 and cv_image[240,320,1]==178 and cv_image[240,320,2]==178):
                     success = True
+                else:
+                    pass
+                    #print("/camera/rgb/image_raw ERROR, retrying")
             except:
                 pass
 
@@ -83,17 +109,29 @@ class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
         except rospy.ServiceException, e:
             print ("/gazebo/pause_physics service call failed")
 
-
-        state = cv2.resize(cv_image, (64, 48))
-
-        if not done:
+        '''if not done:
             # Straight reward = 5, Max angle reward = 0.5
             reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
             # print ("Action : "+str(action)+" Ang_vel : "+str(ang_vel)+" reward="+str(reward))
         else:
+            reward = -200'''
+
+        if not done:
+            if action == 0:
+                reward = 2
+            else:
+                reward = -0.1
+        else:
             reward = -200
 
-        state = state.reshape(3,64,48)
+        x_t1 = skimage.color.rgb2gray(cv_image)
+        x_t1 = skimage.transform.resize(x_t1,(16,16))
+        #x_t1 = skimage.exposure.rescale_intensity(x_t1, out_range=(0, 255))
+        x_t1 = x_t1.reshape(1, 1, x_t1.shape[0], x_t1.shape[1])
+
+        ###state = np.append(x_t1, s_t[:, :3, :, :], axis=1)
+
+        state = x_t1
         return state, reward, done, {}
 
     def _reset(self):
@@ -125,6 +163,9 @@ class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
                 #temporal fix
                 if not (cv_image[240,320,0]==178 and cv_image[240,320,1]==178 and cv_image[240,320,2]==178):
                     success = True
+                else:
+                    pass
+                    #print("/camera/rgb/image_raw ERROR, retrying")
             except:
                 pass
 
@@ -136,11 +177,17 @@ class GazeboCircuit2cTurtlebotCameraNnEnv(gazebo_env.GazeboEnv):
             print ("/gazebo/pause_physics service call failed")
 
 
-        state = cv2.resize(cv_image, (64, 48))
         #cv2.imshow('img', state)
         #cv2.waitKey()
         #raw_input("ENTER..")
 
         #state = image.data
-        state = state.reshape(3,64,48)
+        #state = state.reshape(3,64,48)
+
+        x_t = skimage.color.rgb2gray(cv_image)
+        x_t = skimage.transform.resize(x_t,(16,16))
+        #x_t = skimage.exposure.rescale_intensity(x_t,out_range=(0,255))
+        
+        ###s_t = np.stack((x_t, x_t, x_t, x_t), axis=0)#not needed
+        state = x_t.reshape(1, 1, x_t.shape[0], x_t.shape[1])
         return state
