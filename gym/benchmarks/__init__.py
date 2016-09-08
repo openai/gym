36d5a95e776eac63016957dce7400bc1e41b0826
Greg Brockman <gdb@gregbrockman.com>
2016-09-07 21:18:45 -0700

    Add experimental benchmark registration

---
+++ gym/benchmarks/__init__.py
@@ -0,0 +1,85 @@
+# EXPERIMENTAL: all may be removed soon
+
+from gym.benchmarks.registration import register_benchmark, benchmark_spec
+
+register_benchmark(
+    id='Atari7Pixel-v0',
+    score_method='average_last_100_episodes',
+    tasks={
+        "BeamRider": {
+            "env_id": "BeamRider-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Breakout": {
+            "env_id": "Breakout-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Enduro": {
+            "env_id": "Enduro-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Pong": {
+            "env_id": "Pong-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Qbert": {
+            "env_id": "Qbert-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Seaquest": {
+            "env_id": "Seaquest-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "SpaceInvaders": {
+            "env_id": "SpaceInvaders-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        }
+    })
+
+register_benchmark(
+    id='Atari7Ram-v0',
+    score_method='average_last_100_episodes',
+    tasks={
+        "BeamRider": {
+            "env_id": "BeamRider-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Breakout": {
+            "env_id": "Breakout-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Enduro": {
+            "env_id": "Enduro-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Pong": {
+            "env_id": "Pong-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Qbert": {
+            "env_id": "Qbert-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "Seaquest": {
+            "env_id": "Seaquest-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        },
+        "SpaceInvaders": {
+            "env_id": "SpaceInvaders-ram-v0",
+            "num_seeds": 1,
+            "num_timesteps": 10000000
+        }
+    })
