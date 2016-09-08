60d03a2c881e5f40e3ca1287a2db0c6aaafe30e4
Greg Brockman <gdb@gregbrockman.com>
2016-09-07 22:46:24 -0700

    Flesh out interface

--- gym/benchmarks/__init__.py
+++ gym/benchmarks/__init__.py
@@ -8,38 +8,38 @@ register_benchmark(
     tasks={
         "BeamRider": {
             "env_id": "BeamRider-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Breakout": {
             "env_id": "Breakout-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Enduro": {
             "env_id": "Enduro-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Pong": {
             "env_id": "Pong-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Qbert": {
             "env_id": "Qbert-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Seaquest": {
             "env_id": "Seaquest-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "SpaceInvaders": {
             "env_id": "SpaceInvaders-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         }
     })
 
@@ -49,37 +49,37 @@ register_benchmark(
     tasks={
         "BeamRider": {
             "env_id": "BeamRider-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Breakout": {
             "env_id": "Breakout-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Enduro": {
             "env_id": "Enduro-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Pong": {
             "env_id": "Pong-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Qbert": {
             "env_id": "Qbert-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "Seaquest": {
             "env_id": "Seaquest-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         },
         "SpaceInvaders": {
             "env_id": "SpaceInvaders-ram-v0",
-            "num_seeds": 1,
-            "num_timesteps": 10000000
+            "seeds": 1,
+            "timesteps": 10000000
         }
     })
