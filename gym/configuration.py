d831826ec23114a3f56b5f327205257c7ca4ac58
Greg Brockman <gdb@gregbrockman.com>
2016-09-28 22:58:57 -0700

    Downgrade to just setting the gym logger

--- gym/configuration.py
+++ gym/configuration.py
@@ -17,10 +17,7 @@ handler.setFormatter(formatter)
 # at initialization time.
 def logger_setup(gym_logger):
     root_logger.addHandler(handler)
-    root_logger.setLevel(logging.INFO)
-    # When set to INFO, this will print out the hostname of every
-    # connection it makes.
-    requests_logger.setLevel(logging.WARN)
+    gym_logger.setLevel(logging.INFO)
 
 def undo_logger_setup():
     """Undoes the automatic logging setup done by OpenAI Gym. You should call
@@ -33,5 +30,4 @@ def undo_logger_setup():
     logger.addHandler(logging.StreamHandler(sys.stderr))
     """
     root_logger.removeHandler(handler)
-    root_logger.setLevel(logging.NOTSET)
-    requests_logger.setLevel(logging.NOTSET)
+    gym.logger.setLevel(logging.NOTSET)
