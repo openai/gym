4d530b9cb1055287a04240bfed4ce3b256df718c
John Schulman <joschu@openai.com>
2017-01-03 22:35:26 -0800

    fix

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -112,7 +112,7 @@ class CartPoleEnv(gym.Env):
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height, display=self.display)
+            self.viewer = rendering.Viewer(screen_width, screen_height)
             l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
             axleoffset =cartheight/4.0
             cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
