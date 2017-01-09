61ecebd97791e778717df5b36b1472e0f79898bb
John Schulman <joschu@openai.com>
2017-01-08 16:34:45 -0800

    add wrappers to gym namespace

--- gym/__init__.py
+++ gym/__init__.py
@@ -47,5 +47,6 @@ from gym.core import Env, Space, Wrapper, ObservationWrapper, ActionWrapper, Rew
 from gym.benchmarks import benchmark_spec
 from gym.envs import make, spec
 from gym.scoreboard.api import upload
+from gym import wrappers
 
-__all__ = ["Env", "Space", "Wrapper", "make", "spec", "upload"]
+__all__ = ["Env", "Space", "Wrapper", "make", "spec", "upload", "wrappers"]
