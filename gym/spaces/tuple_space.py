9f18cf65ae61210c98920a288e0a12c7225d14b0
Henrik Holst <hholst80@gmail.com>
2017-01-24 06:21:37 +0100

    Fix PEP indent warnings

--- gym/spaces/tuple_space.py
+++ gym/spaces/tuple_space.py
@@ -1,5 +1,6 @@
 from gym import Space
 
+
 class Tuple(Space):
     """
     A tuple (i.e., product) of simpler spaces
@@ -18,15 +19,16 @@ class Tuple(Space):
         if isinstance(x, list):
             x = tuple(x)  # Promote list to tuple for contains check
         return isinstance(x, tuple) and len(x) == len(self.spaces) and all(
-            space.contains(part) for (space,part) in zip(self.spaces,x))
+            space.contains(part) for (space, part) in zip(self.spaces, x))
 
     def __repr__(self):
         return "Tuple(" + ", ". join([str(s) for s in self.spaces]) + ")"
 
     def to_jsonable(self, sample_n):
         # serialize as list-repr of tuple of vectors
-        return [space.to_jsonable([sample[i] for sample in sample_n]) \
+        return [space.to_jsonable([sample[i] for sample in sample_n])
                 for i, space in enumerate(self.spaces)]
 
     def from_jsonable(self, sample_n):
-        return zip(*[space.from_jsonable(sample_n[i]) for i, space in enumerate(self.spaces)])
+        return zip(*[space.from_jsonable(sample_n[i])
+                     for i, space in enumerate(self.spaces)])
