53c00fd0c7bf1ee0a27fc2667091d43e224a21aa
John Schulman <joschu@openai.com>
2017-02-01 00:14:55 -0800

    don't apply TimeLimit wrapper in make for VNC envs

--- gym/envs/registration.py
+++ gym/envs/registration.py
@@ -114,7 +114,7 @@ class EnvRegistry(object):
         logger.info('Making new env: %s', id)
         spec = self.spec(id)
         env = spec.make()
-        if env.spec.max_episode_steps is not None:
+        if (env.spec.timestep_limit is not None) and not spec.tags.get('vnc'):
             from gym.wrappers.time_limit import TimeLimit
             env = TimeLimit(env, max_episode_steps=env.spec.max_episode_steps)
         return env
