385a85fd0c1b26ab1374f208fbb370e22647548d
John Schulman <joschu@openai.com>
2017-02-04 23:36:32 -0800

    add MultiBinary space, which is a vector of {0,1}

--- gym/spaces/__init__.py
+++ gym/spaces/__init__.py
@@ -1,7 +1,8 @@
 from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
 from gym.spaces.multi_discrete import MultiDiscrete, DiscreteToMultiDiscrete, BoxToMultiDiscrete
+from gym.spaces.multi_binary import MultiBinary
 from gym.spaces.prng import seed
 from gym.spaces.tuple_space import Tuple
 
-__all__ = ["Box", "Discrete", "MultiDiscrete", "DiscreteToMultiDiscrete", "BoxToMultiDiscrete", "Tuple"]
+__all__ = ["Box", "Discrete", "MultiDiscrete", "DiscreteToMultiDiscrete", "BoxToMultiDiscrete", "MultiBinary", "Tuple"]
