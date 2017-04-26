e84c0d83227d3d4b0924c4eaa64dc103e4a25fcc
VincentYu <stacormed@gmail.com>
2017-04-26 00:23:39 -0400

    verified walker2d in dart matches the one in mujoco

--- gym/envs/dart/walker2d.py
+++ gym/envs/dart/walker2d.py
@@ -6,10 +6,12 @@ from gym.envs.dart import dart_env
 class DartWalker2dEnv(dart_env.DartEnv, utils.EzPickle):
     def __init__(self):
         self.control_bounds = np.array([[1.0]*6,[-1.0]*6])
-        self.action_scale = np.array([100, 100, 20, 100, 100, 20])
+        self.action_scale = np.array([100, 100, 100, 100, 100, 100])
         obs_dim = 17
 
-        dart_env.DartEnv.__init__(self, 'walker2d.skel', 4, obs_dim, self.control_bounds, disableViewer=True)
+        dart_env.DartEnv.__init__(self, 'walker2d.skel', 4, obs_dim, self.control_bounds, disableViewer=False)
+
+        self.dart_world.set_collision_detector(3)  # 3 is ode collision detector
 
         utils.EzPickle.__init__(self)
 
@@ -47,7 +49,7 @@ class DartWalker2dEnv(dart_env.DartEnv, utils.EzPickle):
         vel = (posafter - posbefore) / self.dt
         reward = vel#-(vel-1.0)**2
         reward += alive_bonus
-        reward -= 1e-3 * np.square(a).sum()
+        reward -= 1e-2 * np.square(a).sum()
         reward -= 5e-1 * joint_limit_penalty
         #reward -= 1e-7 * total_force_mag
 
