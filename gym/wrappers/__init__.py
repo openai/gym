e59252372509d21f222a76b380cc9bd8b1251181
ngc92 <ngc92@users.noreply.github.com>
2017-06-14 14:37:15 +0200

    add to __init__.py

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -2,3 +2,4 @@ from gym import error
 from gym.wrappers.frame_skipping import SkipWrapper
 from gym.wrappers.monitoring import Monitor
 from gym.wrappers.time_limit import TimeLimit
+from gym.wrappers.action_wrappers import FlattenedActionWrapper, DiscretizedActionWrapper, RescaledActionWrapper
