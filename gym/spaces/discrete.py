d54f4e62eeaf0c4c4b2687c1c0b5664c2622317a
Geoffrey Irving <irving@naml.us>
2017-08-11 14:03:12 -0700

    Give spaces.Discrete a shape = () property
    This lets code be more generic over spaces.

--- gym/spaces/discrete.py
+++ gym/spaces/discrete.py
@@ -22,6 +22,10 @@ class Discrete(gym.Space):
         else:
             return False
         return as_int >= 0 and as_int < self.n
+
+    @property
+    def shape(self):
+        return ()
     def __repr__(self):
         return "Discrete(%d)" % self.n
     def __eq__(self, other):
