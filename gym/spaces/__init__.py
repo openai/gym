461ba3febdb9e77e526b127fbe7b7320dfd32906
Wojciech Zaremba <woj.zaremba@gmail.com>
2017-09-05 08:49:43 -0700

    Dict space (#711)

--- gym/spaces/__init__.py
+++ gym/spaces/__init__.py
@@ -4,5 +4,6 @@ from gym.spaces.multi_discrete import MultiDiscrete
 from gym.spaces.multi_binary import MultiBinary
 from gym.spaces.prng import seed
 from gym.spaces.tuple_space import Tuple
+from gym.spaces.dict_space import Dict
 
-__all__ = ["Box", "Discrete", "MultiDiscrete", "MultiBinary", "Tuple"]
+__all__ = ["Box", "Discrete", "MultiDiscrete", "MultiBinary", "Tuple", "Dict"]
