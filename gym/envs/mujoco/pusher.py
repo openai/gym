bad0ad3fc106aa57661b017c09386ef12967476b
Aravind Srinivas <31194811+aravind0706@users.noreply.github.com>
2017-09-21 15:14:36 -0700

    Update pusher.py

--- gym/envs/mujoco/pusher.py
+++ gym/envs/mujoco/pusher.py
@@ -50,8 +50,8 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def _get_obs(self):
         return np.concatenate([
-            self.model.data.qpos.flat[:7],
-            self.model.data.qvel.flat[:7],
+            self.sim.data.qpos.flat[:7],
+            self.sim.data.qvel.flat[:7],
             self.get_body_com("tips_arm"),
             self.get_body_com("object"),
             self.get_body_com("goal"),
