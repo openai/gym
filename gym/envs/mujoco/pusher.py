5f05edee7b894f39f6c06ed129db1c5bd3dd2f77
Aravind Srinivas <31194811+aravind0706@users.noreply.github.com>
2017-09-21 20:32:48 -0700

    Update pusher.py

--- gym/envs/mujoco/pusher.py
+++ gym/envs/mujoco/pusher.py
@@ -3,7 +3,7 @@ from gym import utils
 from gym.envs.mujoco import mujoco_env
 
 import mujoco_py
-from mujoco_py.mjlib import mjlib
+#from mujoco_py.import mjlib
 
 class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
