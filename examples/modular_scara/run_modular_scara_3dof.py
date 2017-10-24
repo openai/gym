102f3866d9a4c98174bde766e293efc407d5003c
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2017-10-24 12:33:29 +0200

    Update SCARA3DOF env and sample code

--- examples/modular_scara/run_modular_scara_3dof.py
+++ examples/modular_scara/run_modular_scara_3dof.py
@@ -2,6 +2,12 @@ import gym
 import gym_gazebo
 env = gym.make('GazeboModularScara3DOF-v0')
 env.reset()
+
+# Check the env limits:
+# print(env.action_space) # Box(3,)
+# print(env.observation_space) # Box(9,)
+
 for _ in range(1000):
     # env.render()
-    env.step(env.action_space.sample()) # take a random action
+    observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
+    print("reward: ", reward)
