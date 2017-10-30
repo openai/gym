6814f324ec374caf312d0914469faeb9c67e505a
rkojcev <rkojcev@gmail.com>
2017-10-30 10:02:56 +0100

    adding examples of trpo and ppo1 in the gym

---
+++ examples/modular_scara/modular_scara_3dof_ppo.py
@@ -0,0 +1,36 @@
+import gym
+import gym_gazebo
+import tensorflow as tf
+import argparse
+import copy
+import sys
+import numpy as np
+
+from baselines import logger
+from baselines.common import set_global_seeds, tf_util as U
+from baselines.ppo1 import mlp_policy, pposgd_simple
+
+# from baselines.agent.utility.general_utils import get_ee_points, get_position
+
+
+env = gym.make('GazeboModularScara3DOF-v2')
+initial_observation = env.reset()
+print("Initial observation: ", initial_observation)
+env.render()
+seed = 0
+
+
+U.make_session(num_cpu=1).__enter__()
+set_global_seeds(seed)
+
+env.seed(seed)
+def policy_fn(name, ob_space, ac_space):
+    return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+        hid_size=64, num_hid_layers=2)
+
+pposgd_simple.learn(env, policy_fn,
+        max_timesteps=1e6,
+        timesteps_per_actorbatch=2048,
+        clip_param=0.2, entcoeff=0.0,
+        optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
+        gamma=0.99, lam=0.95, schedule='linear')
---
+++ examples/modular_scara/modular_scara_3dof_trpo.py
@@ -0,0 +1,40 @@
+import gym
+import gym_gazebo
+import tensorflow as tf
+import argparse
+import copy
+import sys
+import numpy as np
+from mpi4py import MPI
+
+from baselines import logger
+from baselines.common import set_global_seeds, tf_util as U
+
+from baselines.agent.utility.general_utils import get_ee_points, get_position
+from baselines.ppo1.mlp_policy import MlpPolicy
+from baselines.common.mpi_fork import mpi_fork
+from baselines.trpo_mpi import trpo_mpi
+
+import baselines.common.tf_util as U
+
+env = gym.make('GazeboModularScara3DOF-v2')
+initial_observation = env.reset()
+print("Initial observation: ", initial_observation)
+env.render()
+seed = 0
+
+sess = U.single_threaded_session()
+sess.__enter__()
+
+rank = MPI.COMM_WORLD.Get_rank()
+if rank != 0:
+    logger.set_level(logger.DISABLED)
+workerseed = seed + 10000 * MPI.COMM_WORLD.Get_rank()
+set_global_seeds(workerseed)
+def policy_fn(name, ob_space, ac_space):
+    return MlpPolicy(name=name, ob_space=env.observation_space, ac_space=env.action_space,
+        hid_size=32, num_hid_layers=2)
+env.seed(workerseed)
+
+trpo_mpi.learn(env, policy_fn, timesteps_per_batch=1024, max_kl=0.01, cg_iters=10, cg_damping=0.1,
+            max_timesteps=1e6, gamma=0.99, lam=0.98, vf_iters=5, vf_stepsize=1e-3)
