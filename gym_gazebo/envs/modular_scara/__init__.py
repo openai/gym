71722b528de5a24f428fb99653d211249b0eb5a5
rkojcev <rkojcev@gmail.com>
2017-11-13 13:19:00 +0100

    adding scara enviroment 3.

--- gym_gazebo/envs/modular_scara/__init__.py
+++ gym_gazebo/envs/modular_scara/__init__.py
@@ -1,3 +1,4 @@
 from gym_gazebo.envs.modular_scara.gazebo_modular_scara_3dof import GazeboModularScara3DOFEnv
 from gym_gazebo.envs.modular_scara.gazebo_modular_scara_3dof_v1 import GazeboModularScara3DOFv1Env
 from gym_gazebo.envs.modular_scara.gazebo_modular_scara_3dof_v2 import GazeboModularScara3DOFv2Env
+from gym_gazebo.envs.modular_scara.gazebo_modular_scara_3dof_v3 import GazeboModularScara3DOFv3Env
