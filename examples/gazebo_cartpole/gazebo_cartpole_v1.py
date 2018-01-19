15488daa24f8e61fa0d3d69fb98154365b37985a
ahcorde <ahcorde@gmail.com>
2018-01-19 16:52:27 +0100

    improving cartpole env

---
+++ examples/gazebo_cartpole/gazebo_cartpole_v1.py
@@ -0,0 +1,172 @@
+import tensorflow as tf
+import numpy as np
+import random
+import gym
+import gym_gazebo
+import math
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
+
+env = None
+class LivePlot(object):
+    def __init__(self, outdir, data_key='episode_rewards', line_color='blue'):
+        """
+        Liveplot renders a graph of either episode_rewards or episode_lengths
+        Args:
+            outdir (outdir): Monitor output file location used to populate the graph
+            data_key (Optional[str]): The key in the json to graph (episode_rewards or episode_lengths).
+            line_color (Optional[dict]): Color of the plot.
+        """
+        self.outdir = outdir
+        self._last_data = None
+        self.data_key = data_key
+        self.line_color = line_color
+
+        #styling options
+        matplotlib.rcParams['toolbar'] = 'None'
+        plt.style.use('ggplot')
+        plt.xlabel("")
+        plt.ylabel(data_key)
+        fig = plt.gcf().canvas.set_window_title('simulation_graph')
+
+    def plot(self, reward):
+        # results = monitoring.load_results(self.outdir)
+        # print(results)
+        # if(results==None): return
+
+        data = reward
+        #only update plot if data is different (plot calls are expensive)
+        # if data !=  self._last_data:
+        self._last_data = data
+        plt.plot(data, color=self.line_color)
+
+        # pause so matplotlib will display
+        # may want to figure out matplotlib animation or use a different library in the future
+        plt.pause(0.05)
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    out = e_x / e_x.sum()
+    return out
+
+
+def policy_gradient():
+    with tf.variable_scope("policy"):
+        params = tf.get_variable("policy_parameters",[4,2])
+        state = tf.placeholder("float",[None,4])
+        actions = tf.placeholder("float",[None,2])
+        advantages = tf.placeholder("float",[None,1])
+        linear = tf.matmul(state,params)
+        probabilities = tf.nn.softmax(linear)
+        good_probabilities = tf.reduce_sum(tf.multiply(probabilities, actions),reduction_indices=[1])
+        eligibility = tf.log(good_probabilities) * advantages
+        loss = -tf.reduce_sum(eligibility)
+        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
+        return probabilities, state, actions, advantages, optimizer
+
+def value_gradient():
+    with tf.variable_scope("value"):
+        state = tf.placeholder("float",[None,4])
+        newvals = tf.placeholder("float",[None,1])
+        w1 = tf.get_variable("w1",[4,10])
+        b1 = tf.get_variable("b1",[10])
+        h1 = tf.nn.relu(tf.matmul(state,w1) + b1)
+        w2 = tf.get_variable("w2",[10,1])
+        b2 = tf.get_variable("b2",[1])
+        calculated = tf.matmul(h1,w2) + b2
+        diffs = calculated - newvals
+        loss = tf.nn.l2_loss(diffs)
+        optimizer = tf.train.AdamOptimizer(0.1).minimize(loss)
+        return calculated, state, newvals, optimizer, loss
+
+def run_episode(env, policy_grad, value_grad, sess):
+    pl_calculated, pl_state, pl_actions, pl_advantages, pl_optimizer = policy_grad
+    vl_calculated, vl_state, vl_newvals, vl_optimizer, vl_loss = value_grad
+    observation = env.reset()
+    totalreward = 0
+    states = []
+    actions = []
+    advantages = []
+    transitions = []
+    update_vals = []
+
+
+    for _ in range(20000):
+        # calculate policy
+        obs_vector = np.expand_dims(observation, axis=0)
+        probs = sess.run(pl_calculated,feed_dict={pl_state: obs_vector})
+        action = 0 if random.uniform(0,1) < probs[0][0] else 1
+        # record the transition
+        states.append(observation)
+        print("angle: ", observation[2]*180/3.14)
+        actionblank = np.zeros(2)
+        actionblank[action] = 1
+        actions.append(actionblank)
+        # take the action in the environment
+        old_observation = observation
+        observation, reward, done, info = env.step(action)
+        transitions.append((old_observation, action, reward))
+        totalreward += reward
+
+        if done:
+            break
+    for index, trans in enumerate(transitions):
+        obs, action, reward = trans
+
+        # calculate discounted monte-carlo return
+        future_reward = 0
+        future_transitions = len(transitions) - index
+        decrease = 1
+        for index2 in range(future_transitions):
+            future_reward += transitions[(index2) + index][2] * decrease
+            decrease = decrease * 0.97
+        obs_vector = np.expand_dims(obs, axis=0)
+        currentval = sess.run(vl_calculated,feed_dict={vl_state: obs_vector})[0][0]
+
+        # advantage: how much better was this action than normal
+        advantages.append(future_reward - currentval)
+
+        # update the value function towards new return
+        update_vals.append(future_reward)
+
+    # update value function
+    update_vals_vector = np.expand_dims(update_vals, axis=1)
+    sess.run(vl_optimizer, feed_dict={vl_state: states, vl_newvals: update_vals_vector})
+    # real_vl_loss = sess.run(vl_loss, feed_dict={vl_state: states, vl_newvals: update_vals_vector})
+
+    advantages_vector = np.expand_dims(advantages, axis=1)
+    sess.run(pl_optimizer, feed_dict={pl_state: states, pl_advantages: advantages_vector, pl_actions: actions})
+
+    return totalreward
+
+
+env = gym.make('GazeboCartPole-v0')
+
+
+outdir = '/tmp/gazebo_gym_experiments'
+plotter = LivePlot(outdir)
+
+#env.monitor.start('cartpole-hill/', force=True)
+policy_grad = policy_gradient()
+value_grad = value_gradient()
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+list_rewards = []
+for i in range(2000):
+    print("Episode ", i)
+    reward = run_episode(env, policy_grad, value_grad, sess)
+    list_rewards.append(reward)
+    if(i%20==0):
+        plotter.plot(list_rewards)
+#    if reward == 200:
+#        print "reward 200"
+#        print i
+#        break
+t = 0
+for _ in range(1000):
+    reward = run_episode(env, policy_grad, value_grad, sess)
+    t += reward
+print (t / 1000)
+#env.monitor.close()
+print ("END!")
