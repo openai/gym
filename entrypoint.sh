cfbbc1bce392584f16a165f4835a2409e6d590b5
Alejandro Hernández Cordero <ahcorde@gmail.com>
2018-01-23 10:03:43 +0100

    Fixing paths to worlds

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ source "/root/ros_catkin_ws/install_isolated/setup.bash"
 # set environment variables
 export ROS_PORT_SIM="11311"
 # export GYM_GAZEBO_ENV_SCARA3="/Users/victor/gym-gazebo/gym_gazebo/envs/assets/worlds/scara_basic.world"
-export GYM_GAZEBO_WORLD_CIRCUIT="/gym-gazebo/gym_gazebo/envs/assets/worlds/circuit.world"
-export GYM_GAZEBO_WORLD_CIRCUIT2="/gym-gazebo/gym_gazebo/envs/assets/worlds/circuit2.world"
+export GYM_GAZEBO_WORLD_CIRCUIT="/root/gym-gazebo/gym_gazebo/envs/assets/worlds/circuit.world"
+export GYM_GAZEBO_WORLD_CIRCUIT2="/root/gym-gazebo/gym_gazebo/envs/assets/worlds/circuit2.world"
 
 # # FIXME
 # rm /opt/ros/kinetic/lib/python2.7/dist-packages/cv2.so
