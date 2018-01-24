420f6845bdfc777f6705de6f8a81265e624f5bd4
Matthias Plappert <matthias@openai.com>
2018-01-24 14:19:35 -0800

    Initial qpos

--- gym/envs/hand/reach.py
+++ gym/envs/hand/reach.py
@@ -362,6 +362,30 @@ class HandEnv(gym.GoalEnv):
 class ReachEnv(HandEnv, utils.EzPickle):
     def __init__(self):
         initial_qpos = {
+            'robot0:WRJ1': -0.16514339750464327,
+            'robot0:WRJ0': -0.31973286565062153,
+            'robot0:FFJ3': 0.14340512546557435,
+            'robot0:FFJ2': 0.32028208333591573,
+            'robot0:FFJ1': 0.7126053607727917,
+            'robot0:FFJ0': 0.6705281001412586,
+            'robot0:MFJ3': 0.000246444303701037,
+            'robot0:MFJ2': 0.3152655251085491,
+            'robot0:MFJ1': 0.7659800313729842,
+            'robot0:MFJ0': 0.7323156897425923,
+            'robot0:RFJ3': 0.00038520700007378114,
+            'robot0:RFJ2': 0.36743546201985233,
+            'robot0:RFJ1': 0.7119514095008576,
+            'robot0:RFJ0': 0.6699446327514138,
+            'robot0:LFJ4': 0.0525442258033891,
+            'robot0:LFJ3': -0.13615534724474673,
+            'robot0:LFJ2': 0.39872030433433003,
+            'robot0:LFJ1': 0.7415570009679252,
+            'robot0:LFJ0': 0.704096378652974,
+            'robot0:THJ4': 0.003673823825070126,
+            'robot0:THJ3': 0.5506291436028695,
+            'robot0:THJ2': -0.014515151997119306,
+            'robot0:THJ1': -0.0015229223564485414,
+            'robot0:THJ0': -0.7894883021600622,
         }
         HandEnv.__init__(
             self, 'reach.xml', n_substeps=20, initial_qpos=initial_qpos)
