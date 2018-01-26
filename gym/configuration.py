9b1195d5b30cd757cc3d117dcd4404a269c9bbd8
John Schulman <joschu@openai.com>
2018-01-25 17:15:54 -0800

    remove configuration.py

--- gym/configuration.py
+++
@@ -1,5 +0,0 @@
-def logger_setup():
-    print('undo_logger_setup is DEPRECATED. If you want to configure logging, use e.g. "from gym import logger; logger.set_level(logger.INFO)"')
-
-def undo_logger_setup():
-    print('undo_logger_setup is DEPRECATED. If you want to turn off logging, use "from gym import logger; logger.set_level(logger.DISABLED)"')
