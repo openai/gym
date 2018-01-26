04c64f9012c065f45349c6d13697391ca6efe6a0
John Schulman <joschu@openai.com>
2018-01-25 17:09:45 -0800

    undo_logger_setup for backwards compat

--- gym/__init__.py
+++ gym/__init__.py
@@ -1,6 +1,7 @@
 import distutils.version
 import os
 import sys
+import warnings
 
 from gym import error
 from gym.utils import reraise
@@ -8,6 +9,9 @@ from gym.version import VERSION as __version__
 
 from gym.core import Env, Space, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper
 from gym.envs import make, spec
-from gym import wrappers, spaces
+from gym import wrappers, spaces, logger
+
+def undo_logger_setup():
+    warnings.warn("gym.undo_logger_setup is deprecated. gym no longer modifies the global logging configuration")
 
 __all__ = ["Env", "Space", "Wrapper", "make", "spec", "wrappers"]
