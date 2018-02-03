a8b30c3d68114a72690250b3b28685f2244b45ca
John Schulman <joschu@openai.com>
2018-02-02 22:49:30 -0800

    fix circular import (py2) by not importing spaces

--- gym/spaces/multi_discrete.py
+++ gym/spaces/multi_discrete.py
@@ -1,17 +1,16 @@
 import gym
-from gym import spaces, Space
 import numpy as np
 
-class MultiDiscrete(Space):
+class MultiDiscrete(gym.Space):
     def __init__(self, nvec):
         """
         nvec: vector of counts of each categorical variable
         """
         self.nvec = np.asarray(nvec, dtype=np.int32)
         assert self.nvec.ndim == 1, 'nvec should be a 1d array (or list) of ints'
-        Space.__init__(self, (self.nvec.size,), np.int8)
+        gym.Space.__init__(self, (self.nvec.size,), np.int8)
     def sample(self):
-        return (spaces.np_random.rand(self.nvec.size) * self.nvec).astype(self.dtype)
+        return (gym.spaces.np_random.rand(self.nvec.size) * self.nvec).astype(self.dtype)
     def contains(self, x):
         return (x < self.nvec).all() and x.dtype.kind in 'ui'
     def to_jsonable(self, sample_n):
