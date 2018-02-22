9f7bd3139331d21482b9f3f4732b8f386e376dc8
Samir Shah <samir@shapesecurity.com>
2018-02-21 21:17:19 -0800

    improve test_spaces

--- gym/spaces/tests/test_spaces.py
+++ gym/spaces/tests/test_spaces.py
@@ -7,20 +7,22 @@ from gym.spaces import Tuple, Box, Discrete, MultiDiscrete, MultiBinary, Dict
 @pytest.mark.parametrize("space", [
               Discrete(3),
               Tuple([Discrete(5), Discrete(10)]),
-              Tuple([Discrete(5), Box(low=np.array([0,0]),high=np.array([1,5]))]),
+              Tuple([Discrete(5), Box(low=np.array([0,0]),high=np.array([1,5]), dtype=np.float32)]),
               Tuple((Discrete(5), Discrete(2), Discrete(2))),
-              MultiDiscrete([ 2, 2, 100]),
-              Dict({"position": Discrete(5), "velocity": Box(low=np.array([0,0]),high=np.array([1,5]))}),
+              MultiDiscrete([2, 2, 100]),
+              MultiBinary(10),
+              Dict({"position": Discrete(5), "velocity": Box(low=np.array([0,0]),high=np.array([1,5]), dtype=np.float32)}),
               ])
 def test_roundtripping(space):
+
     sample_1 = space.sample()
     sample_2 = space.sample()
+
     assert space.contains(sample_1)
     assert space.contains(sample_2)
+
     json_rep = space.to_jsonable([sample_1, sample_2])
-
     json_roundtripped = json.loads(json.dumps(json_rep))
-
     samples_after_roundtrip = space.from_jsonable(json_roundtripped)
     sample_1_prime, sample_2_prime = samples_after_roundtrip
 
@@ -28,5 +30,7 @@ def test_roundtripping(space):
     s1p = space.to_jsonable([sample_1_prime])
     s2 = space.to_jsonable([sample_2])
     s2p = space.to_jsonable([sample_2_prime])
+
     assert s1 == s1p, "Expected {} to equal {}".format(s1, s1p)
     assert s2 == s2p, "Expected {} to equal {}".format(s2, s2p)
+
