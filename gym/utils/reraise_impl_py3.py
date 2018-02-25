7ee8fb2d13c38ce73e7b4658148f1fefd861c844
cclauss <cclauss@bluewin.ch>
2018-02-25 09:38:08 +0100

    Use six.raise_from() and six.reraise() to unify implementation

--- gym/utils/reraise_impl_py3.py
+++ gym/utils/reraise_impl_py3.py
@@ -1,4 +1,8 @@
 # http://stackoverflow.com/a/33822606 -- `from None` disables Python 3'
 # semi-smart exception chaining, which we don't want in this case.
+import six
+
+
 def reraise_impl(e, traceback):
-    raise e.with_traceback(traceback) from None
+    # raise e.with_traceback(traceback) from None
+    six.raise_from(e.with_traceback(traceback), None)
