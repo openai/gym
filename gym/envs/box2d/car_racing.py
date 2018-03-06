e074c8a29f71feaf2cd49016c5079655dff63191
Kay <dustocyno@gmail.com>
2018-03-05 23:55:43 -0800

    Box2d carracing: Add escape key to quit the game

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -459,11 +459,13 @@ if __name__=="__main__":
     a = np.array( [0.0, 0.0, 0.0] )
     def key_press(k, mod):
         global restart
+        global quit
         if k==0xff0d: restart = True
         if k==key.LEFT:  a[0] = -1.0
         if k==key.RIGHT: a[0] = +1.0
         if k==key.UP:    a[1] = +1.0
         if k==key.DOWN:  a[2] = +0.8   # set 1.0 for wheels to block to zero rotation
+        if k==key.ESCAPE:    quit = True
     def key_release(k, mod):
         if k==key.LEFT  and a[0]==-1.0: a[0] = 0
         if k==key.RIGHT and a[0]==+1.0: a[0] = 0
@@ -476,7 +478,8 @@ if __name__=="__main__":
         env.monitor.start('/tmp/video-test', force=True)
     env.viewer.window.on_key_press = key_press
     env.viewer.window.on_key_release = key_release
-    while True:
+    quit = False
+    while not quit:
         env.reset()
         total_reward = 0.0
         steps = 0
@@ -493,5 +496,5 @@ if __name__=="__main__":
             steps += 1
             if not record_video: # Faster, but you can as well call env.render() every time to play full window.
                 env.render()
-            if done or restart: break
+            if quit or done or restart: break
     env.close()
