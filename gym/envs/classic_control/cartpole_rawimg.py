b2226d1c881fbaabaa61ffba0f835fc58d68dd0e
Adam Budai <budai8adam@gmail.com>
2018-03-20 12:36:25 +0100

    Drawer class was added and drawing was implemented.

--- gym/envs/classic_control/cartpole_rawimg.py
+++ gym/envs/classic_control/cartpole_rawimg.py
@@ -6,6 +6,7 @@ values.
 
 from gym.envs.classic_control import cartpole
 import numpy as np
+import math
 
 
 class CartPoleRawImgEnv(cartpole.CartPoleEnv):
@@ -49,6 +50,12 @@ class CartPoleRawImgEnv(cartpole.CartPoleEnv):
     def __del__(self):
         self.close()
 
+colors = {
+        'black': np.array([0, 0, 0]),
+        'blue': np.array([33, 100, 209]),
+        'red': np.array([214, 25, 28])
+    }
+
 
 class DrawImage:
 
@@ -58,6 +65,52 @@ class DrawImage:
         self.canvas = np.zeros((self.height, self.width, 3), dtype=np.uint8) + 255
 
     def draw(self, cart_pos, pol_angl):
+        self.__clear()
+        self.__draw_base_line()
+        self.__draw_box(int((1.0 + cart_pos/2.4) * (self.width // 2)))
+        self.__draw_rod(int((1.0 + cart_pos/2.4) * (self.width // 2)), pol_angl)
 
         return self.canvas
 
+    # Draw the elements of the image
+
+    def __clear(self):
+
+        self.canvas[:, :, :] = 255
+
+    def __draw_base_line(self):
+
+        start_x = 5
+        end_x = self.width - 5
+
+        start_end_y = self.height // 2 # the line is horizontal
+
+        self.canvas[start_end_y, start_x:end_x] = colors['black']
+
+    def __draw_box(self, x):
+        y = self.height // 2
+        left = x - 20
+        right = x + 20
+        top = y - 10
+        bottom = y + 10
+
+        self.canvas[top:bottom, left:right] = colors['blue']
+
+    def __draw_rod(self, pos, angle):
+
+        def draw_square(x, y):
+            self.canvas[y-2:y+2, x-2:x+2] = colors['red']
+
+        rod_length = 40
+        x0 = pos
+        y0 = self.height // 2 - 10
+
+        x1 = int(x0 + rod_length / 2 * math.sin(angle))
+        y1 = int(y0 - rod_length / 2 * math.cos(angle))
+
+        x2 = int(x0 + rod_length * math.sin(angle))
+        y2 = int(y0 - rod_length * math.cos(angle))
+
+        draw_square(x0, y0)
+        draw_square(x1, y1)
+        draw_square(x2, y2)
