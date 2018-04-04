2411fe77d8879356f27ad0a0cf363f84c8d68d9f
Chris Spencer <chrisspen@gmail.com>
2018-04-04 00:49:08 -0400

    Cleaned up comments.

--- gym/envs/classic_control/carttable.py
+++ gym/envs/classic_control/carttable.py
@@ -37,8 +37,7 @@ class CartTableEnv(gym.Env):
         self.tau = 0.02  # seconds between state updates
 
         # Angle at which to fail the episode
-        # self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.theta_threshold_radians = 45 * math.pi / 360
+        self.theta_threshold_radians = 12 * 2 * math.pi / 360
         self.x_threshold = 2.4
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
@@ -64,7 +63,6 @@ class CartTableEnv(gym.Env):
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
         state = self.state
-        # print('state:', state)
         x, x_dot, theta, theta_dot = state
 
         if action == 0:
@@ -75,11 +73,6 @@ class CartTableEnv(gym.Env):
         # force = self.force_mag if action == 1 else -self.force_mag
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
-        #temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass # ang accel of pole due to force of cart push?
-        #thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
-
-        # F = m*a => a = F/m
-        #xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
 
         # Calculate the initial 2D position (x, pole_length) of cart due to theta.
         xp = x*math.cos(theta) - self.length*math.sin(theta)
@@ -104,8 +97,7 @@ class CartTableEnv(gym.Env):
         thetaacc = arm_torque/(self.masscart * arm_length**2)
 
         # Calculate acceleration of the cart due to applied force.
-        # a = (V1 - V0)/(t1 - t0)
-        # a = F/m
+        # a = (V1 - V0)/(t1 - t0) = F/m
         xacc = force * self.masscart #TODO:include gravity component as table tilts?
 
         # Calculate change in the cart's position due to velocity over time.
@@ -123,7 +115,6 @@ class CartTableEnv(gym.Env):
         theta_dot = theta_dot + self.tau * thetaacc
 
         self.state = (x, x_dot, theta, theta_dot)
-        # print('state1:', self.state)
 
         done = x < -self.x_threshold or x > self.x_threshold or theta < -self.theta_threshold_radians or theta > self.theta_threshold_radians
         done = bool(done)
