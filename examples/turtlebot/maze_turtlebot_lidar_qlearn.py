6cbfaab399c8cdc95e4fe98d887828684931e66f
Matthew Hansen <matthew.k.hansen@intel.com>
2018-04-12 16:21:43 -0700

    Refactor liveplot out of maze example

--- examples/turtlebot/maze_turtlebot_lidar_qlearn.py
+++ examples/turtlebot/maze_turtlebot_lidar_qlearn.py
@@ -59,40 +59,6 @@ class QLearn:
         maxqnew = max([self.getQ(state2, a) for a in self.actions])
         self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
 
-class LivePlot(object):
-    def __init__(self, outdir, data_key='episode_rewards', line_color='blue'):
-        """
-        Liveplot renders a graph of either episode_rewards or episode_lengths
-        Args:
-            outdir (outdir): Monitor output file location used to populate the graph
-            data_key (Optional[str]): The key in the json to graph (episode_rewards or episode_lengths).
-            line_color (Optional[dict]): Color of the plot.
-        """
-        self.outdir = outdir
-        self._last_data = None
-        self.data_key = data_key
-        self.line_color = line_color
-
-        #styling options
-        matplotlib.rcParams['toolbar'] = 'None'
-        plt.style.use('ggplot')
-        plt.xlabel("Episodes")
-        plt.ylabel(data_key)
-        fig = plt.gcf().canvas.set_window_title('simulation_graph')
-
-    def plot(self):
-        results = gym.monitoring.monitor.load_results(self.outdir)
-        data =  results[self.data_key]
-
-        #only update plot if data is different (plot calls are expensive)
-        if data !=  self._last_data:
-            self._last_data = data
-            plt.plot(data, color=self.line_color)
-
-            # pause so matplotlib will display
-            # may want to figure out matplotlib animation or use a different library in the future
-            plt.pause(0.000001)
-
 def render():
     render_skip = 0 #Skip first X episodes.
     render_interval = 50 #Show render Every Y episodes.
