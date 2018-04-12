2558b85ea0e5efcf04ec01ac9d22b36acd1ed2a8
ahcorde <ahcorde@gmail.com>
2018-04-12 15:42:31 +0200

    Adding MAIRA robot random examples

---
+++ examples/MAIRA/gazebo_maira_top_3dof_random.py
@@ -0,0 +1,18 @@
+import gym
+import gym_gazebo
+env = gym.make('MAIRATop3DOF-v0')
+env.reset()
+import time
+# Check the env limits:
+# print(env.action_space) # Box(3,)
+# print(env.observation_space) # Box(9,)
+import random
+
+for i in range(100):
+    env.reset()
+    time.sleep(1)
+    # print("Reset!")
+    for _ in range(50):
+        observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
+        time.sleep(0.1)
+        # print("reward: ", reward)
