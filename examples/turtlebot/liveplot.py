b0a971803ec3a9b3364595c32b1b56f7f0b74da2
Matthew Hansen <matthew.k.hansen@intel.com>
2018-04-13 12:41:49 -0700

    Fix liveplot monitor bug

--- examples/turtlebot/liveplot.py
+++ examples/turtlebot/liveplot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import matplotlib
 import matplotlib.pyplot as plt
+import gym
+
+rewards_key = 'episode_rewards'
 
 class LivePlot(object):
-    def __init__(self, outdir, data_key='episode_rewards', line_color='blue'):
+    def __init__(self, outdir, data_key=rewards_key, line_color='blue'):
         """
         Liveplot renders a graph of either episode_rewards or episode_lengths
         Args:
@@ -12,7 +15,6 @@ class LivePlot(object):
             line_color (Optional[dict]): Color of the plot.
         """
         self.outdir = outdir
-        self._last_data = None
         self.data_key = data_key
         self.line_color = line_color
 
@@ -23,15 +25,14 @@ class LivePlot(object):
         plt.ylabel(data_key)
         fig = plt.gcf().canvas.set_window_title('simulation_graph')
 
-    def plot(self):
-        results = gym.monitoring.monitor.load_results(self.outdir)
-        data =  results[self.data_key]
+    def plot(self, env):
+        if self.data_key is rewards_key:
+            data = gym.wrappers.Monitor.get_episode_rewards(env)
+        else:
+            data = gym.wrappers.Monitor.get_episode_lengths(env)
 
-        #only update plot if data is different (plot calls are expensive)
-        if data !=  self._last_data:
-            self._last_data = data
-            plt.plot(data, color=self.line_color)
+        plt.plot(data, color=self.line_color)
 
-            # pause so matplotlib will display
-            # may want to figure out matplotlib animation or use a different library in the future
-            plt.pause(0.000001)
+        # pause so matplotlib will display
+        # may want to figure out matplotlib animation or use a different library in the future
+        plt.pause(0.000001)
