1500292a39cb975e7424e1ff911a9720e359c9dd
Rampagy <ajsimon@mtu.edu>
2018-04-30 21:12:22 -0500

    Got implicit and standard euler mixed up

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -25,7 +25,7 @@ class CartPoleEnv(gym.Env):
         self.polemass_length = (self.masspole * self.length)
         self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
-        self.kinematics_integrator = 'euler'
+        self.kinematics_integrator = 'semi-implicit-euler'
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
@@ -62,15 +62,15 @@ class CartPoleEnv(gym.Env):
         thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
         xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
         if self.kinematics_integrator == 'euler':
-            x_dot = x_dot + self.tau * xacc
             x  = x + self.tau * x_dot
-            theta_dot = theta_dot + self.tau * thetaacc
+            x_dot = x_dot + self.tau * xacc
             theta = theta + self.tau * theta_dot
+            theta_dot = theta_dot + self.tau * thetaacc
         else: # semi-implicit euler
-            x  = x + self.tau * x_dot
             x_dot = x_dot + self.tau * xacc
-            theta = theta + self.tau * theta_dot
+            x  = x + self.tau * x_dot
             theta_dot = theta_dot + self.tau * thetaacc
+            theta = theta + self.tau * theta_dot
         self.state = (x,x_dot,theta,theta_dot)
         done =  x < -self.x_threshold \
                 or x > self.x_threshold \
