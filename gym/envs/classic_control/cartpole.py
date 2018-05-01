efb28c0250e718821165f025ce2315492dbfe706
Rampagy <ajsimon@mtu.edu>
2018-04-30 20:38:46 -0500

    Added semi-implicit euler option

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -25,6 +25,7 @@ class CartPoleEnv(gym.Env):
         self.polemass_length = (self.masspole * self.length)
         self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
+        self.kinematics_integrator = 'euler'
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
@@ -60,10 +61,16 @@ class CartPoleEnv(gym.Env):
         temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
         thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
         xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        x_dot = x_dot + self.tau * xacc
-        x  = x + self.tau * x_dot
-        theta_dot = theta_dot + self.tau * thetaacc
-        theta = theta + self.tau * theta_dot
+        if self.kinematics_integrator == 'euler':
+            x_dot = x_dot + self.tau * xacc
+            x  = x + self.tau * x_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+            theta = theta + self.tau * theta_dot
+        else: # semi-implicit euler
+            x  = x + self.tau * x_dot
+            x_dot = x_dot + self.tau * xacc
+            theta = theta + self.tau * theta_dot
+            theta_dot = theta_dot + self.tau * thetaacc
         self.state = (x,x_dot,theta,theta_dot)
         done =  x < -self.x_threshold \
                 or x > self.x_threshold \
