7cb2f6043d83b01ef2790faa8c09c5d4e17e3d2c
Greg Brockman <gdb@gregbrockman.com>
2018-05-03 20:24:16 -0700

    Support code which expects gym.spaces.foo to work without a direct import of gym.spaces

--- gym/__init__.py
+++ gym/__init__.py
@@ -14,4 +14,21 @@ from gym import logger
 def undo_logger_setup():
     warnings.warn("gym.undo_logger_setup is deprecated. gym no longer modifies the global logging configuration")
 
-__all__ = ["Env", "Space", "Wrapper", "make", "spec", "wrappers"]
+# Upon one acccess to gym.spaces.foo (or a manually-called import
+# gym.spaces), gym.spaces will be imported and override the stub
+# object.
+class Spaces(object):
+    def __getattr__(self, k):
+        warnings.warn('DEPRECATION WARNING: to improve load times, gym no longer automatically loads gym.spaces. Please run "import gym.spaces" to load gym.spaces on your own. This warning will turn into an error in a future version of gym.')
+        import gym.spaces
+        return getattr(gym.spaces, k)
+spaces = Spaces()
+
+class Wrappers(object):
+    def __getattr__(self, k):
+        warnings.warn('DEPRECATION WARNING: to improve load times, gym no longer automatically loads gym.wrappers. Please run "import gym.wrappers" to load gym.wrappers on your own. This warning will turn into an error in a future version of gym.')
+        import gym.wrappers
+        return getattr(gym.wrappers, k)
+wrappers = Wrappers()
+
+__all__ = ["Env", "Space", "Wrapper", "make", "spec", "wrappers", "spaces"]
