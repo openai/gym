0c75ad7596479745b75ce41c7f484f38fbd3c28b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-08-16 14:37:28 +0200

    allow normal install of package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys, os.path
 
 # Don't import gym module here, since deps may not be installed
@@ -6,6 +6,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym-gazebo'))
 
 setup(name='gym-gazebo',
       version='0.0.2',
+      packages=find_packages(),
       install_requires=['gym>=0.2.3'],
       description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using Gazebo and ROS.',
       url='https://github.com/erlerobot/gym',
