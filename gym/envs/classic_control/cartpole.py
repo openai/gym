75f28faf9b2c4b6bdba076579b43a7b3dbc0871c
Chris Miles <chris.john.miles@gmail.com>
2018-08-24 19:30:17 -0400

    Update docstrings of environment classes (#1123)
    * update reference to Sutton and Barto in docstring of CliffWalkingEnv class
    
    * update reference to Sutton and Barto in docstring of BlackjackEnv class
    
    * added detailed description of taxi environment in doc string
    
    * Fixed typo. I swapped north and south in actions list
    
    * placed wiki description of cartpole to a docstring
    
    * fixed formatting in docstring
    
    * fixed formating in docstring

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -11,6 +11,43 @@ from gym.utils import seeding
 import numpy as np
 
 class CartPoleEnv(gym.Env):
+    """
+    Description:
+        A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's velocity.
+
+    Source:
+        This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson
+
+    Observation: 
+        Type: Box(4)
+        Num	Observation                 Min         Max
+        0	Cart Position             -2.4            2.4
+        1	Cart Velocity             -Inf            Inf
+        2	Pole Angle                ~-41.8°         ~41.8°
+        3	Pole Velocity At Tip      -Inf            Inf
+        
+    Actions:
+        Type: Discrete(2)
+        Num	Action
+        0	Push cart to the left
+        1	Push cart to the right
+        
+        Note: The amount the velocity is reduced or increased is not fixed as it depends on the angle the pole is pointing. This is because the center of gravity of the pole increases the amount of energy needed to move the cart underneath it
+
+    Reward:
+        Reward is 1 for every step taken, including the termination step
+
+    Starting State:
+        All observations are assigned a uniform random value between ±0.05
+
+    Episode Termination:
+        Pole Angle is more than ±12°
+        Cart Position is more than ±2.4 (center of the cart reaches the edge of the display)
+        Episode length is greater than 200
+        Solved Requirements
+        Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
+    """
+    
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second' : 50
