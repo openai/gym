72ef72022d370bca1ff96b5c4cfa1da968dd9f3a
Peter Zhokhov <peterz@openai.com>
2018-08-27 11:26:39 -0700

    merged Oleg's fix

--- gym/envs/box2d/test_lunar_lander.py
+++ gym/envs/box2d/test_lunar_lander.py
@@ -6,22 +6,21 @@ def test_lunar_lander():
 def test_lunar_lander_continuous():
     _test_lander(LunarLanderContinuous())
 
-
 def _test_lander(env):
-    s = env.reset()
     total_reward = 0
     steps = 0
+    s = env.reset()
     while True:
         a = heuristic(env, s)
         s, r, done, info = env.step(a)
-        env.render()
+        still_open = env.render()
+        if still_open==False: break
         total_reward += r
         if steps % 20 == 0 or done:
-            print(["{:+0.2f}".format(x) for x in s])
+            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
             print("step {} total_reward {:+0.2f}".format(steps, total_reward))
         steps += 1
         if done: break
-
     assert total_reward > 100
 
 
