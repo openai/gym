83c6e6a6b0c5f18637a93f46e86998bf82396077
Peter Zhokhov <peterz@openai.com>
2018-08-27 16:19:24 -0700

    added rendering back to lunar_lander

--- gym/envs/box2d/test_lunar_lander.py
+++ gym/envs/box2d/test_lunar_lander.py
@@ -1,4 +1,10 @@
-from .lunar_lander import LunarLander, LunarLanderContinuous, heuristic
+from gym.envs.box2d.lunar_lander import LunarLander, LunarLanderContinuous, heuristic
+
+# test LunarLander environment with a hand-coded policy. 
+# To visualize, run
+#
+#   python test_lunar_lander.py
+#
 
 def test_lunar_lander():
     _test_lander(LunarLander(), seed=0)
@@ -6,7 +12,7 @@ def test_lunar_lander():
 def test_lunar_lander_continuous():
     _test_lander(LunarLanderContinuous(), seed=0)
 
-def _test_lander(env, seed=None):
+def _test_lander(env, seed=None, render=False):
     env.seed(seed)
     total_reward = 0
     steps = 0
@@ -15,6 +21,11 @@ def _test_lander(env, seed=None):
         a = heuristic(env, s)
         s, r, done, info = env.step(a)
         total_reward += r
+
+        if render:
+            still_open = env.render()
+            if still_open == False: break
+
         if steps % 20 == 0 or done:
             print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
             print("step {} total_reward {:+0.2f}".format(steps, total_reward))
@@ -23,7 +34,7 @@ def _test_lander(env, seed=None):
     assert total_reward > 100
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     env = LunarLander()
-    _test_lander(env)
+    _test_lander(env, render=True)
 
