a860029e9f6f42602461239c37750d0e38eac30d
Peter Zhokhov <peterz@openai.com>
2018-08-28 10:12:32 -0700

    move enjoy_lander into lunar_lander.py

--- gym/envs/box2d/lunar_lander.py
+++ gym/envs/box2d/lunar_lander.py
@@ -390,3 +390,30 @@ def heuristic(env, s):
         elif angle_todo < -0.05: a = 3
         elif angle_todo > +0.05: a = 1
     return a
+
+def enjoy_lander(env, seed=None, render=False):
+    env.seed(seed)
+    total_reward = 0
+    steps = 0
+    s = env.reset()
+    while True:
+        a = heuristic(env, s)
+        s, r, done, info = env.step(a)
+        total_reward += r
+
+        if render:
+            still_open = env.render()
+            if still_open == False: break
+
+        if steps % 20 == 0 or done:
+            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
+            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+        steps += 1
+        if done: break
+    return total_reward
+
+
+if __name__ == '__main__':
+    enjoy_lander(LunarLander(), render=True)
+    
+    
--- gym/envs/box2d/test_lunar_lander.py
+++ gym/envs/box2d/test_lunar_lander.py
@@ -1,10 +1,4 @@
-from gym.envs.box2d.lunar_lander import LunarLander, LunarLanderContinuous, heuristic
-
-# test LunarLander environment with a hand-coded policy. 
-# To visualize, run
-#
-#   python test_lunar_lander.py
-#
+from .lunar_lander import LunarLander, LunarLanderContinuous, enjoy_lander
 
 def test_lunar_lander():
     _test_lander(LunarLander(), seed=0)
@@ -13,28 +7,7 @@ def test_lunar_lander_continuous():
     _test_lander(LunarLanderContinuous(), seed=0)
 
 def _test_lander(env, seed=None, render=False):
-    env.seed(seed)
-    total_reward = 0
-    steps = 0
-    s = env.reset()
-    while True:
-        a = heuristic(env, s)
-        s, r, done, info = env.step(a)
-        total_reward += r
-
-        if render:
-            still_open = env.render()
-            if still_open == False: break
-
-        if steps % 20 == 0 or done:
-            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
-            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-        steps += 1
-        if done: break
+    total_reward = enjoy_lander(env, seed=seed, render=render)
     assert total_reward > 100
 
-
-if __name__ == "__main__":
-    env = LunarLander()
-    _test_lander(env, render=True)
 
