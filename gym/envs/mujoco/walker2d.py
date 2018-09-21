a11557aad260c70eb21082158c61982d567a5f98
Peter Zhokhov <peterz@openai.com>
2018-09-21 11:51:34 -0700

    remove vertical camera motion on env.reset()

--- gym/envs/mujoco/walker2d.py
+++ gym/envs/mujoco/walker2d.py
@@ -36,5 +36,5 @@ class Walker2dEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def viewer_setup(self):
         self.viewer.cam.trackbodyid = 2
         self.viewer.cam.distance = self.model.stat.extent * 0.5
-        self.viewer.cam.lookat[2] += .8
+        self.viewer.cam.lookat[2] = 1.15
         self.viewer.cam.elevation = -20
