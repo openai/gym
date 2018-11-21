943246476a988b37e98156887d6086c62ccc80e9
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-11-22 00:09:42 +0100

    Update __init__.py

--- gym/__init__.py
+++ gym/__init__.py
@@ -7,8 +7,8 @@ from gym import error
 from gym.utils import reraise
 from gym.version import VERSION as __version__
 
-from gym.core import Env, GoalEnv, Space, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper
+from gym.core import Env, GoalEnv, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper
 from gym.envs import make, spec
 from gym import logger
 
-__all__ = ["Env", "Space", "Wrapper", "make", "spec"]
+__all__ = ["Env", "Wrapper", "make", "spec"]
