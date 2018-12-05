e92ac9956a394751464570e0dec32134d3d9d87f
Peter Zhokhov <peterz@openai.com>
2018-12-05 11:06:43 -0800

    added a check nvec > 0

--- gym/spaces/multi_discrete.py
+++ gym/spaces/multi_discrete.py
@@ -6,14 +6,15 @@ class MultiDiscrete(gym.Space):
         """
         nvec: vector of counts of each categorical variable
         """
-        self.nvec = np.asarray(nvec, dtype=np.int32)
+        assert (nvec > 0).all(), 'nvec (counts) have to be positive'
+        self.nvec = np.asarray(nvec, dtype=np.uint32)
         gym.Space.__init__(self, self.nvec.shape, np.uint32)
 
     def sample(self):
         return (gym.spaces.np_random.random_sample(self.nvec.shape) * self.nvec).astype(self.dtype)
 
     def contains(self, x):
-        # if nvec is int32 and space dtype is unit32, then 0 <= x < self.nvec guarantees that x
+        # if nvec is uint32 and space dtype is uint32, then 0 <= x < self.nvec guarantees that x
         # is within correct bounds for space dtype (even though x does not have to be unsigned)
         return (0 <= x).all() and (x < self.nvec).all()
 
