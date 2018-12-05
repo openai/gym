01a381669bc8129545ac347cf3b7f9346f6ff898
pzhokhov <peterzhokhoff@gmail.com>
2018-12-05 15:54:49 -0800

    small fix to multidiscrete (#1249)
    * small fix to multidiscrete
    
    * added a check nvec > 0
    
    * fix the comparison in multidiscrete space

--- gym/spaces/multi_discrete.py
+++ gym/spaces/multi_discrete.py
@@ -6,14 +6,17 @@ class MultiDiscrete(gym.Space):
         """
         nvec: vector of counts of each categorical variable
         """
-        self.nvec = np.asarray(nvec, dtype=np.int32)
-        gym.Space.__init__(self, self.nvec.shape, np.int8)
+        assert (np.array(nvec) > 0).all(), 'nvec (counts) have to be positive'
+        self.nvec = np.asarray(nvec, dtype=np.uint32)
+        gym.Space.__init__(self, self.nvec.shape, np.uint32)
 
     def sample(self):
         return (gym.spaces.np_random.random_sample(self.nvec.shape) * self.nvec).astype(self.dtype)
 
     def contains(self, x):
-        return (0 <= x).all() and (x < self.nvec).all() and x.dtype.kind in 'ui'
+        # if nvec is uint32 and space dtype is uint32, then 0 <= x < self.nvec guarantees that x
+        # is within correct bounds for space dtype (even though x does not have to be unsigned)
+        return (0 <= x).all() and (x < self.nvec).all()
 
     def to_jsonable(self, sample_n):
         return [sample.tolist() for sample in sample_n]
