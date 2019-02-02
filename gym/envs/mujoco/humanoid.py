b2a439f74f8be390c806515f77849e04776f91bb
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-02-01 22:10:33 -0800

    Refactor gym envs to support dynamic arguments

--- gym/envs/mujoco/humanoid.py
+++ gym/envs/mujoco/humanoid.py
@@ -2,50 +2,148 @@ import numpy as np
 from gym.envs.mujoco import mujoco_env
 from gym import utils
 
+
+DEFAULT_CAMERA_CONFIG = {
+    'trackbodyid': 1,
+    'distance': 4.0,
+    'lookat': np.array((0.0, 0.0, 2.0)),
+    'elevation': -20.0,
+}
+
+
 def mass_center(model, sim):
-    mass = np.expand_dims(model.body_mass, 1)
+    mass = np.expand_dims(model.body_mass, axis=1)
     xpos = sim.data.xipos
-    return (np.sum(mass * xpos, 0) / np.sum(mass))[0]
+    return (np.sum(mass * xpos, axis=0) / np.sum(mass))[0]
+
 
 class HumanoidEnv(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self):
+    def __init__(self,
+                 forward_reward_weight=0.25,
+                 ctrl_cost_weight=0.1,
+                 contact_cost_weight=5e-7,
+                 contact_cost_range=(-np.inf, 10.0),
+                 healthy_reward=5.0,
+                 terminate_when_unhealthy=True,
+                 healthy_z_range=(1.0, 2.0),
+                 reset_noise_scale=1e-2,
+                 exclude_current_positions_from_observation=True):
+        utils.EzPickle.__init__(**locals())
+
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._contact_cost_weight = contact_cost_weight
+        self._contact_cost_range = contact_cost_range
+        self._healthy_reward = healthy_reward
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        self._healthy_z_range = healthy_z_range
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation)
+
         mujoco_env.MujocoEnv.__init__(self, 'humanoid.xml', 5)
-        utils.EzPickle.__init__(self)
+
+    @property
+    def healthy_reward(self):
+        return float(
+            self.is_healthy
+            or self._terminate_when_unhealthy
+        ) * self._healthy_reward
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(
+            np.square(self.sim.data.ctrl))
+        return control_cost
+
+    @property
+    def contact_cost(self):
+        contact_forces = self.sim.data.cfrc_ext
+        contact_cost = self._contact_cost_weight * np.sum(
+            np.square(contact_forces))
+        min_cost, max_cost = self._contact_cost_range
+        contact_cost = np.clip(contact_cost, min_cost, max_cost)
+        return contact_cost
+
+    @property
+    def is_healthy(self):
+        min_z, max_z = self._healthy_z_range
+        is_healthy = min_z < self.sim.data.qpos[2] < max_z
+
+        return is_healthy
+
+    @property
+    def done(self):
+        done = ((not self.is_healthy)
+                if self._terminate_when_unhealthy
+                else False)
+        return done
 
     def _get_obs(self):
-        data = self.sim.data
-        return np.concatenate([data.qpos.flat[2:],
-                               data.qvel.flat,
-                               data.cinert.flat,
-                               data.cvel.flat,
-                               data.qfrc_actuator.flat,
-                               data.cfrc_ext.flat])
+        position = self.sim.data.qpos.flat.copy()
+        velocity = self.sim.data.qvel.flat.copy()
 
-    def step(self, a):
-        pos_before = mass_center(self.model, self.sim)
-        self.do_simulation(a, self.frame_skip)
-        pos_after = mass_center(self.model, self.sim)
-        alive_bonus = 5.0
-        data = self.sim.data
-        lin_vel_cost = 0.25 * (pos_after - pos_before) / self.model.opt.timestep
-        quad_ctrl_cost = 0.1 * np.square(data.ctrl).sum()
-        quad_impact_cost = .5e-6 * np.square(data.cfrc_ext).sum()
-        quad_impact_cost = min(quad_impact_cost, 10)
-        reward = lin_vel_cost - quad_ctrl_cost - quad_impact_cost + alive_bonus
-        qpos = self.sim.data.qpos
-        done = bool((qpos[2] < 1.0) or (qpos[2] > 2.0))
-        return self._get_obs(), reward, done, dict(reward_linvel=lin_vel_cost, reward_quadctrl=-quad_ctrl_cost, reward_alive=alive_bonus, reward_impact=-quad_impact_cost)
+        com_inertia = self.sim.data.cinert.flat.copy()
+        com_velocity = self.sim.data.cvel.flat.copy()
+
+        actuator_forces = self.sim.data.qfrc_actuator.flat.copy()
+        external_contact_forces = self.sim.data.cfrc_ext.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        return np.concatenate((
+            position,
+            velocity,
+            com_inertia,
+            com_velocity,
+            actuator_forces,
+            external_contact_forces,
+        ))
+
+    def step(self, action):
+        x_position_before = mass_center(self.model, self.sim)
+        self.do_simulation(action, self.frame_skip)
+        x_position_after = mass_center(self.model, self.sim)
+
+        x_velocity = ((x_position_after - x_position_before)
+                      / self.model.opt.timestep)
+
+        ctrl_cost = self.control_cost(action)
+        contact_cost = self.contact_cost
+
+        forward_reward = self._forward_reward_weight * x_velocity
+        healthy_reward = self.healthy_reward
+
+        rewards = forward_reward + healthy_reward
+        costs = ctrl_cost + contact_cost
+
+        observation = self._get_obs()
+        reward = rewards - costs
+        done = self.done
+        info = {
+            'reward_linvel': forward_reward,
+            'reward_quadctrl': -ctrl_cost,
+            'reward_alive': healthy_reward,
+            'reward_impact': -contact_cost,
+        }
+
+        return observation, reward, done, info
 
     def reset_model(self):
-        c = 0.01
-        self.set_state(
-            self.init_qpos + self.np_random.uniform(low=-c, high=c, size=self.model.nq),
-            self.init_qvel + self.np_random.uniform(low=-c, high=c, size=self.model.nv,)
-        )
-        return self._get_obs()
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nv)
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+        return observation
 
     def viewer_setup(self):
-        self.viewer.cam.trackbodyid = 1
-        self.viewer.cam.distance = self.model.stat.extent * 1.0
-        self.viewer.cam.lookat[2] = 2.0
-        self.viewer.cam.elevation = -20
+        for key, value in DEFAULT_CAMERA_CONFIG.items():
+            setattr(self.viewer.cam, key, value)
