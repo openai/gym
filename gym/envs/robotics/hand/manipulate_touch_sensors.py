0d71b27103cd434173eb24ca50a99b3968866cfd
Andrew Melnik <andrew.melnik.git@gmail.com>
2019-02-03 20:40:31 +0100

    add shared_touch_sensors_92.xml; solve pytest . failure
    Co-authored-by: Luca Lach <llach@techfak.uni-bielefeld.de>

--- gym/envs/robotics/hand/manipulate_touch_sensors.py
+++ gym/envs/robotics/hand/manipulate_touch_sensors.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
-from gym import utils
+
+from gym import utils, error, spaces
 from gym.envs.robotics.hand import manipulate
 
 # Ensure we get the path separator correct on windows
-MANIPULATE_BLOCK_XML = os.path.join('hand', 'manipulate_block_touch_sensors_85.xml')
-MANIPULATE_EGG_XML = os.path.join('hand', 'manipulate_egg_touch_sensors_85.xml')
-MANIPULATE_PEN_XML = os.path.join('hand', 'manipulate_pen_touch_sensors_85.xml')
+MANIPULATE_BLOCK_XML = os.path.join('hand', 'manipulate_block_touch_sensors.xml')
+MANIPULATE_EGG_XML = os.path.join('hand', 'manipulate_egg_touch_sensors.xml')
+MANIPULATE_PEN_XML = os.path.join('hand', 'manipulate_pen_touch_sensors.xml')
 
 
 class ManipulateTouchSensorsEnv(manipulate.ManipulateEnv, utils.EzPickle):
@@ -21,8 +22,8 @@ class ManipulateTouchSensorsEnv(manipulate.ManipulateEnv, utils.EzPickle):
 
         Args:
             touch_visualisation (string): how touch sensor sites are visualised
-                - "always": always shows touch sensor sites
                 - "on_touch": shows touch sensor sites only when touch values > 0
+                - "always": always shows touch sensor sites
                 - "off" or else: does not show touch sensor sites
             touch_get_obs (string): touch sensor readings
                 - "boolean": returns 1 if touch sensor reading != 0.0 else 0
@@ -32,9 +33,7 @@ class ManipulateTouchSensorsEnv(manipulate.ManipulateEnv, utils.EzPickle):
         """
         self.touch_visualisation = touch_visualisation
         self.touch_get_obs = touch_get_obs
-        self._tsensor_id2name = {}
-        self._tsensor_id2siteid = {}
-        self._site_id2intial_rgba = {}  # dict for initial rgba values for debugging
+        self._touch_sensor_id_site_id = []
         self.touch_color = [1, 0, 0, 0.4]
         self.notouch_color = [0, 0.5, 0, 0.3]
 
@@ -47,26 +46,31 @@ class ManipulateTouchSensorsEnv(manipulate.ManipulateEnv, utils.EzPickle):
         )
         utils.EzPickle.__init__(self)
 
-        for k, v in self.sim.model._sensor_id2name.items():  # get touch sensor ids and their site names
-            if 'TS' in v:
-                self._tsensor_id2name[k] = v
-                self._tsensor_id2siteid[k] = self.sim.model._site_name2id[v.replace('TS', 'T')]
-                self._site_id2intial_rgba[self._tsensor_id2siteid[k]] = self.sim.model.site_rgba[self._tsensor_id2siteid[k]].copy()  # get initial rgba values
+        for k, v in self.sim.model._sensor_name2id.items():  # get touch sensor ids and their site names
+            if 'robot0:TS_' in k:
+                self._touch_sensor_id_site_id.append((v, self.sim.model._site_name2id[k.replace('robot0:TS_', 'robot0:T_')]))
 
         if self.touch_visualisation == 'off':  # set touch sensors rgba values
-            for k, v in self._tsensor_id2name.items():
-                self.sim.model.site_rgba[self._tsensor_id2siteid[k]] = [0, 0, 0, 0]
+            for touch_sensor_id, site_id in self._touch_sensor_id_site_id:
+                self.sim.model.site_rgba[site_id][3] = 0.0
         elif self.touch_visualisation == 'always':
             pass
+
+        obs = self._get_obs()
+        self.observation_space = spaces.Dict(dict(
+            desired_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
+            achieved_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
+            observation=spaces.Box(-np.inf, np.inf, shape=obs['observation'].shape, dtype='float32'),
+        ))
 
     def _render_callback(self):
         super(ManipulateTouchSensorsEnv, self)._render_callback()
         if self.touch_visualisation == 'on_touch':
-            for k, v in self._tsensor_id2name.items():
-                if self.sim.data.sensordata[k] != 0.0:
-                    self.sim.model.site_rgba[self._tsensor_id2siteid[k]] = self.touch_color
+            for touch_sensor_id, site_id in self._touch_sensor_id_site_id:
+                if self.sim.data.sensordata[touch_sensor_id] != 0.0:
+                    self.sim.model.site_rgba[site_id] = self.touch_color
                 else:
-                    self.sim.model.site_rgba[self._tsensor_id2siteid[k]] = self.notouch_color
+                    self.sim.model.site_rgba[site_id] = self.notouch_color
 
     def _get_obs(self):
         robot_qpos, robot_qvel = manipulate.robot_get_obs(self.sim)
@@ -74,12 +78,12 @@ class ManipulateTouchSensorsEnv(manipulate.ManipulateEnv, utils.EzPickle):
         achieved_goal = self._get_achieved_goal().ravel()  # this contains the object position + rotation
         touch_values = []  # get touch sensor readings. if there is one, set value to 1
         if self.touch_get_obs == 'boolean':
-            for k, v in self._tsensor_id2name.items():
-                value = 1.0 if self.sim.data.sensordata[k] != 0.0 else 0.0
+            for touch_sensor_id, site_id in self._touch_sensor_id_site_id:
+                value = 1.0 if self.sim.data.sensordata[touch_sensor_id] != 0.0 else 0.0
                 touch_values.append(value)
         elif self.touch_get_obs == 'log':
-            for k, v in self._tsensor_id2name.items():
-                value = self.sim.data.sensordata[k]
+            for touch_sensor_id, site_id in self._touch_sensor_id_site_id:
+                value = self.sim.data.sensordata[touch_sensor_id]
                 touch_values.append(value)
             if len(touch_values) > 0:
                 touch_values = np.log(np.array(touch_values) + 1.0)
