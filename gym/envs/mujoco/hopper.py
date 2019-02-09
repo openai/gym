63145a039e0029cdc78de3c827a5efea5d211f0b
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-02-08 16:48:58 -0800

    Revert "Refactor gym envs to support dynamic arguments"
    This reverts commit b2a439f74f8be390c806515f77849e04776f91bb.

--- gym/envs/mujoco/hopper.py
+++ gym/envs/mujoco/hopper.py
@@ -1,133 +1,40 @@
 import numpy as np
-from gym.envs.mujoco import mujoco_env
 from gym import utils
-
-
-DEFAULT_CAMERA_CONFIG = {
-    'trackbodyid': 2,
-    'distance': 3.0,
-    'lookat': np.array((0.0, 0.0, 1.15)),
-    'elevation': -20.0,
-}
-
+from gym.envs.mujoco import mujoco_env
 
 class HopperEnv(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self,
-                 forward_reward_weight=1.0,
-                 ctrl_cost_weight=1e-3,
-                 healthy_reward=1.0,
-                 terminate_when_unhealthy=True,
-                 healthy_state_range=(-100.0, 100.0),
-                 healthy_z_range=(0.7, float('inf')),
-                 healthy_angle_range=(-0.2, 0.2),
-                 reset_noise_scale=5e-3,
-                 exclude_current_positions_from_observation=True):
-        utils.EzPickle.__init__(**locals())
-
-        self._forward_reward_weight = forward_reward_weight
-
-        self._ctrl_cost_weight = ctrl_cost_weight
-
-        self._healthy_reward = healthy_reward
-        self._terminate_when_unhealthy = terminate_when_unhealthy
-
-        self._healthy_state_range = healthy_state_range
-        self._healthy_z_range = healthy_z_range
-        self._healthy_angle_range = healthy_angle_range
-
-        self._reset_noise_scale = reset_noise_scale
-
-        self._exclude_current_positions_from_observation = (
-            exclude_current_positions_from_observation)
-
+    def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'hopper.xml', 4)
+        utils.EzPickle.__init__(self)
 
-    @property
-    def healthy_reward(self):
-        return float(
-            self.is_healthy
-            or self._terminate_when_unhealthy
-        ) * self._healthy_reward
-
-    def control_cost(self, action):
-        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
-        return control_cost
-
-    @property
-    def is_healthy(self):
-        z, angle = self.sim.data.qpos[1:3]
-        state = self.state_vector()[2:]
-
-        min_state, max_state = self._healthy_state_range
-        min_z, max_z = self._healthy_z_range
-        min_angle, max_angle = self._healthy_angle_range
-
-        healthy_state = np.all(
-            np.logical_and(min_state < state, state < max_state))
-        healthy_z = min_z < z < max_z
-        healthy_angle = min_angle < angle < max_angle
-
-        is_healthy = all((healthy_state, healthy_z, healthy_angle))
-
-        return is_healthy
-
-    @property
-    def done(self):
-        done = (not self.is_healthy
-                if self._terminate_when_unhealthy
-                else False)
-        return done
+    def step(self, a):
+        posbefore = self.sim.data.qpos[0]
+        self.do_simulation(a, self.frame_skip)
+        posafter, height, ang = self.sim.data.qpos[0:3]
+        alive_bonus = 1.0
+        reward = (posafter - posbefore) / self.dt
+        reward += alive_bonus
+        reward -= 1e-3 * np.square(a).sum()
+        s = self.state_vector()
+        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+                    (height > .7) and (abs(ang) < .2))
+        ob = self._get_obs()
+        return ob, reward, done, {}
 
     def _get_obs(self):
-        position = self.sim.data.qpos.flat.copy()
-        velocity = np.clip(
-            self.sim.data.qvel.flat.copy(), -10, 10)
-
-        if self._exclude_current_positions_from_observation:
-            position = position[1:]
-
-        observation = np.concatenate((position, velocity)).ravel()
-        return observation
-
-    def step(self, action):
-        x_position_before = self.sim.data.qpos[0]
-        self.do_simulation(action, self.frame_skip)
-        x_position_after = self.sim.data.qpos[0]
-        x_velocity = ((x_position_after - x_position_before)
-                      / self.dt)
-
-        ctrl_cost = self.control_cost(action)
-
-        forward_reward = self._forward_reward_weight * x_velocity
-        healthy_reward = self.healthy_reward
-
-        rewards = forward_reward + healthy_reward
-        costs = ctrl_cost
-
-        observation = self._get_obs()
-        reward = rewards - costs
-        done = self.done
-        info = {
-            'x_position': x_position_after,
-            'x_velocity': x_velocity,
-        }
-
-        return observation, reward, done, info
+        return np.concatenate([
+            self.sim.data.qpos.flat[1:],
+            np.clip(self.sim.data.qvel.flat, -10, 10)
+        ])
 
     def reset_model(self):
-        noise_low = -self._reset_noise_scale
-        noise_high = self._reset_noise_scale
-
-        qpos = self.init_qpos + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nq)
-        qvel = self.init_qvel + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nv)
-
+        qpos = self.init_qpos + self.np_random.uniform(low=-.005, high=.005, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.uniform(low=-.005, high=.005, size=self.model.nv)
         self.set_state(qpos, qvel)
-
-        observation = self._get_obs()
-        return observation
+        return self._get_obs()
 
     def viewer_setup(self):
-        for key, value in DEFAULT_CAMERA_CONFIG.items():
-            setattr(self.viewer.cam, key, value)
+        self.viewer.cam.trackbodyid = 2
+        self.viewer.cam.distance = self.model.stat.extent * 0.75
+        self.viewer.cam.lookat[2] = 1.15
+        self.viewer.cam.elevation = -20
--- gym/envs/mujoco/swimmer.py
+++ gym/envs/mujoco/swimmer.py
@@ -1,77 +1,31 @@
 import numpy as np
-from gym.envs.mujoco import mujoco_env
 from gym import utils
-
+from gym.envs.mujoco import mujoco_env
 
 class SwimmerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self,
-                 forward_reward_weight=1.0,
-                 ctrl_cost_weight=1e-4,
-                 reset_noise_scale=0.1,
-                 exclude_current_positions_from_observation=True):
-        utils.EzPickle.__init__(**locals())
-
-        self._forward_reward_weight = forward_reward_weight
-        self._ctrl_cost_weight = ctrl_cost_weight
-
-        self._reset_noise_scale = reset_noise_scale
-
-        self._exclude_current_positions_from_observation = (
-            exclude_current_positions_from_observation)
-
+    def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'swimmer.xml', 4)
+        utils.EzPickle.__init__(self)
 
-    def control_cost(self, action):
-        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
-        return control_cost
-
-    def step(self, action):
-        x_position_before = self.sim.data.qpos[0]
-        self.do_simulation(action, self.frame_skip)
-        x_position_after = self.sim.data.qpos[0]
-
-        x_velocity = ((x_position_after - x_position_before)
-                      / self.dt)
-        forward_reward = self._forward_reward_weight * x_velocity
-
-        ctrl_cost = self.control_cost(action)
-
-        xy_positions = self.sim.data.qpos[0:2]
-
-        observation = self._get_obs()
-        reward = forward_reward - ctrl_cost
-        done = False
-        info = {
-            'x_position': xy_positions[0],
-            'y_position': xy_positions[1],
-            'xy_position': np.linalg.norm(xy_positions, ord=2),
-
-            'x_velocity': x_velocity,
-            'forward_reward': forward_reward,
-        }
-
-        return observation, reward, done, info
+    def step(self, a):
+        ctrl_cost_coeff = 0.0001
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(a, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+        reward_fwd = (xposafter - xposbefore) / self.dt
+        reward_ctrl = - ctrl_cost_coeff * np.square(a).sum()
+        reward = reward_fwd + reward_ctrl
+        ob = self._get_obs()
+        return ob, reward, False, dict(reward_fwd=reward_fwd, reward_ctrl=reward_ctrl)
 
     def _get_obs(self):
-        position = self.sim.data.qpos.flat.copy()
-        velocity = self.sim.data.qvel.flat.copy()
-
-        if self._exclude_current_positions_from_observation:
-            position = position[2:]
-
-        observation = np.concatenate([position, velocity]).ravel()
-        return observation
+        qpos = self.sim.data.qpos
+        qvel = self.sim.data.qvel
+        return np.concatenate([qpos.flat[2:], qvel.flat])
 
     def reset_model(self):
-        noise_low = -self._reset_noise_scale
-        noise_high = self._reset_noise_scale
-
-        qpos = self.init_qpos + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nq)
-        qvel = self.init_qvel + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nv)
-
-        self.set_state(qpos, qvel)
-
-        observation = self._get_obs()
-        return observation
+        self.set_state(
+            self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq),
+            self.init_qvel + self.np_random.uniform(low=-.1, high=.1, size=self.model.nv)
+        )
+        return self._get_obs()
