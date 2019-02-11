7c3b766f906e35ab64313889aa9133469511eb8f
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-02-12 00:54:02 +0100

    Update box.py

--- gym/spaces/box.py
+++ gym/spaces/box.py
@@ -1,46 +1,33 @@
 import numpy as np
 
-import gym
-from gym import logger
 from .space import Space
 
 
 class Box(Space):
-    """
-    A box in R^n.
-    I.e., each coordinate is bounded.
+    """A box in R^n, i.e.each coordinate is bounded.
+    
+    There are two common use cases:
 
-    Example usage:
-    self.action_space = spaces.Box(low=-10, high=10, shape=(1,))
+    * Identical bound for each dimension::
+        >>> Box(low=-1.0, high=2.0, shape=[3, 4], dtype=np.float32)
+        Box(3, 4)
+
+    * Independent bound for each dimension::
+        >>> Box(low=np.array([-1.0, -2.0]), high=np.array([3.0, 4.0]), dtype=np.float32)
+        Box(2,)
     """
     def __init__(self, low, high, shape=None, dtype=np.float32):
-        r"""Define the lower and upper bound for this space. 
-        
-        There are two common use cases:
-            
-        * Identical bound for each dimension::
-            >>> Box(low=-1.0, high=2.0, dtype=np.float32, shape=[3, 4])
-            Box(3, 4)
-            
-        * Independent bound for each dimension::
-        
-            >>> Box(low=np.array([-1.0, -2.0]), high=np.array([3.0, 4.0]), dtype=np.float32)
-            Box(2,)
-        
-        """
         assert dtype is not None, 'dtype must be explicitly provided. '
         self.dtype = np.dtype(dtype)
 
         if shape is None:
             assert low.shape == high.shape
             self.shape = low.shape
-
             self.low = low
             self.high = high
         else:
             assert np.isscalar(low) and np.isscalar(high)
             self.shape = tuple(shape)
-
             self.low = np.full(self.shape, low)
             self.high = np.full(self.shape, high)
 
@@ -59,13 +46,13 @@ class Box(Space):
     @property
     def flat_dim(self):
         return int(np.prod(self.shape))
-    
+
     def flatten(self, x):
         return np.asarray(x).flatten()
-    
+
     def unflatten(self, x):
         return np.asarray(x).reshape(self.shape)
-    
+
     def contains(self, x):
         return x.shape == self.shape and np.all(x >= self.low) and np.all(x <= self.high)
 
