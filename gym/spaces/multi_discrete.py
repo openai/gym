d80d01d474288494efa926722af2da25cb027d78
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-02-12 23:34:15 +0100

    Update multi_discrete.py

--- gym/spaces/multi_discrete.py
+++ gym/spaces/multi_discrete.py
@@ -9,7 +9,7 @@ class MultiDiscrete(Space):
         assert (np.array(nvec) > 0).all(), 'nvec (counts) have to be positive'
         self.nvec = np.asarray(nvec, dtype=np.uint32)
 
-        super().__init__(self.nvec.shape, np.uint32)
+        super(MultiDiscrete, self).__init__(self.nvec.shape, np.uint32)
 
     def sample(self):
         return (self.np_random.random_sample(self.nvec.shape)*self.nvec).astype(self.dtype)
