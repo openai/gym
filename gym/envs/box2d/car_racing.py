66cb17517cc17540e82c1367e9c492625e886620
Peter Zhokhov <peterz@openai.com>
2019-03-01 14:32:08 -0800

    reduce window size of car racing to 1000x800 for human game

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -43,8 +43,8 @@ STATE_W = 96   # less than Atari 160x192
 STATE_H = 96
 VIDEO_W = 600
 VIDEO_H = 400
-WINDOW_W = 1200
-WINDOW_H = 1000
+WINDOW_W = 1000
+WINDOW_H = 800
 
 SCALE       = 6.0        # Track scale
 TRACK_RAD   = 900/SCALE  # Track is heavily morphed circle with this radius
