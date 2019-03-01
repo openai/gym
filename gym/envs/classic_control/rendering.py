5244513c6c6494b162db7eee2baeda22fd56d933
Peter Zhokhov <peterz@openai.com>
2019-03-01 15:21:53 -0800

    remove reraise import in classic_control/rendering.py

--- gym/envs/classic_control/rendering.py
+++ gym/envs/classic_control/rendering.py
@@ -11,7 +11,6 @@ if "Apple" in sys.version:
         os.environ['DYLD_FALLBACK_LIBRARY_PATH'] += ':/usr/lib'
         # (JDS 2016/04/15): avoid bug on Anaconda 2.3.0 / Yosemite
 
-from gym.utils import reraise
 from gym import error
 
 try:
