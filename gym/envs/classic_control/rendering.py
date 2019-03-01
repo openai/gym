693c1536eaf7bb793a173b1b39eb3e87eaa9d7e7
Peter Zhokhov <peterz@openai.com>
2019-03-01 14:54:52 -0800

    remove reraise logic - replace with re-raising import errors in the only place it is used

--- gym/envs/classic_control/rendering.py
+++ gym/envs/classic_control/rendering.py
@@ -17,12 +17,22 @@ from gym import error
 try:
     import pyglet
 except ImportError as e:
-    reraise(suffix="HINT: you can install pyglet directly via 'pip install pyglet'. But if you really just want to install all Gym dependencies and not have to think about it, 'pip install -e .[all]' or 'pip install gym[all]' will do it.")
+    raise ImportError('''
+    Cannot import pyglet.
+    HINT: you can install pyglet directly via 'pip install pyglet'.
+    But if you really just want to install all Gym dependencies and not have to think about it,
+    'pip install -e .[all]' or 'pip install gym[all]' will do it.
+    ''')
 
 try:
     from pyglet.gl import *
 except ImportError as e:
-    reraise(prefix="Error occured while running `from pyglet.gl import *`",suffix="HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'. If you're running on a server, you may need a virtual frame buffer; something like this should work: 'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'")
+    raise ImportError('''
+    Error occured while running `from pyglet.gl import *`
+    HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'.
+    If you're running on a server, you may need a virtual frame buffer; something like this should work:
+    'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'
+    ''')
 
 import math
 import numpy as np
