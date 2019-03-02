d5e6c7700cd695fb67d714433c9ac2d3585f647b
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-03-02 21:26:02 +0100

    Update tuple_space.py

--- gym/spaces/tuple_space.py
+++ gym/spaces/tuple_space.py
@@ -1,10 +1,10 @@
-import gym
+import numpy as np
+
 from .space import Space
 
 
 class Tuple(Space):
-    """
-    A tuple (i.e., product) of simpler spaces
+    """A tuple (i.e., product) of simpler spaces
 
     Example usage:
     self.observation_space = spaces.Tuple((spaces.Discrete(2), spaces.Discrete(3)))
@@ -13,11 +13,11 @@ class Tuple(Space):
         self.spaces = spaces
         super(Tuple, self).__init__(None, None)
 
-    def seed(self, seed):
-        [space.seed(seed) for space in self.spaces]
-
     def sample(self):
         return tuple([space.sample() for space in self.spaces])
+
+    def seed(self, seed):
+        [space.seed(seed) for space in self.spaces]
 
     def contains(self, x):
         if isinstance(x, list):
@@ -36,11 +36,5 @@ class Tuple(Space):
     def from_jsonable(self, sample_n):
         return [sample for sample in zip(*[space.from_jsonable(sample_n[i]) for i, space in enumerate(self.spaces)])]
 
-    def __getitem__(self, index):
-        return self.spaces[index]
-
-    def __len__(self):
-        return len(self.spaces)
-      
-    def __eq__(self, other):
-        return self.spaces == other.spaces
+    def __eq__(self, x):
+        return isinstance(x, Tuple) and tuple(x.spaces) == tuple(self.spaces)
