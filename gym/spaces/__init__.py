fbf14f31e1f748a87afc811caaa5248525240cca
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-03-02 21:18:25 +0100

    Update __init__.py

--- gym/spaces/__init__.py
+++ gym/spaces/__init__.py
@@ -3,7 +3,7 @@ from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
 from gym.spaces.multi_discrete import MultiDiscrete
 from gym.spaces.multi_binary import MultiBinary
-from gym.spaces.tuple_space import Tuple
-from gym.spaces.dict_space import Dict
+from gym.spaces.tuple import Tuple
+from gym.spaces.dict import Dict
 
 __all__ = ["Space", "Box", "Discrete", "MultiDiscrete", "MultiBinary", "Tuple", "Dict"]
