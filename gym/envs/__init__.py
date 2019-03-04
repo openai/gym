e2bac311c8b95dc7f96ec3ba4fba677809bc1c0e
Likhitha-k <48185491+Likhitha-k@users.noreply.github.com>
2019-03-04 11:04:27 +0530

    Update __init__.py

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -67,8 +67,8 @@ register(
 register(
     id='CartPole-v2',
     entry_point='gym.envs.classic_control:CartPoleEnv',
-    max_episode_steps=500,
-    reward_threshold=475.0,
+    max_episode_steps=200,
+    reward_threshold=195.0,
 )
 
 register(
