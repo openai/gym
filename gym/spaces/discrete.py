e71378e8e49605bc7ee73b1d88927d04d1420531
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-03-18 11:40:37 +0100

    Update discrete.py

--- gym/spaces/discrete.py
+++ gym/spaces/discrete.py
@@ -1,37 +1,25 @@
 import numpy as np
-
+import gym
 from .space import Space
 
 
 class Discrete(Space):
-    r"""A discrete space in :math:`\{ 0, 1, \dots, n-1 \}`. 
-    
-    Example::
-    
-        >>> Discrete(2)
-        
+    """
+    {0,1,...,n-1}
+
+    Example usage:
+    self.observation_space = spaces.Discrete(2)
     """
     def __init__(self, n):
-        assert n >= 0
         self.n = n
         super(Discrete, self).__init__((), np.int64)
+        self.np_random = np.random.RandomState()
+
+    def seed(self, seed):
+        self.np_random.seed(seed)
 
     def sample(self):
         return self.np_random.randint(self.n)
-
-    @property
-    def flat_dim(self):
-        return int(self.n)
-
-    def flatten(self, x):
-        # One-hot representation
-        onehot = np.zeros(self.n)
-        onehot[x] = 1.0
-        return onehot
-
-    def unflatten(self, x):
-        # Extract index from one-hot representation
-        return int(np.nonzero(x)[0][0])
 
     def contains(self, x):
         if isinstance(x, int):
@@ -45,5 +33,5 @@ class Discrete(Space):
     def __repr__(self):
         return "Discrete(%d)" % self.n
 
-    def __eq__(self, x):
-        return isinstance(x, Discrete) and x.n == self.n
+    def __eq__(self, other):
+        return self.n == other.n
