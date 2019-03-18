58fb2756dd5ac044ec7aa8c4ae898b5c9ffc19f1
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-03-18 11:41:02 +0100

    Update multi_binary.py

--- gym/spaces/multi_binary.py
+++ gym/spaces/multi_binary.py
@@ -1,5 +1,5 @@
+import gym
 import numpy as np
-
 from .space import Space
 
 
@@ -7,19 +7,13 @@ class MultiBinary(Space):
     def __init__(self, n):
         self.n = n
         super(MultiBinary, self).__init__((self.n,), np.int8)
+        self.np_random = np.random.RandomState()
+
+    def seed(self, seed):
+        self.np_random.seed(seed)
 
     def sample(self):
-        return self.np_random.randint(low=0, high=2, size=self.n, dtype=self.dtype)
-
-    @property
-    def flat_dim(self):
-        return int(self.n)
-
-    def flatten(self, x):
-        return np.asarray(x).flatten()
-
-    def unflatten(self, x):
-        return np.asarray(x).reshape(self.shape)
+        return self.np_random.randint(low=0, high=2, size=self.n).astype(self.dtype)
 
     def contains(self, x):
         return ((x==0) | (x==1)).all()
