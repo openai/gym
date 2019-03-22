11181ac0bf57cc5261da24fd51a09c18e4f68bee
InstanceLabs <instance01@users.noreply.github.com>
2019-03-22 23:09:53 +0100

    Improve BFS implementation in frozen_lake env (#1391)

--- gym/envs/toy_text/frozen_lake.py
+++ gym/envs/toy_text/frozen_lake.py
@@ -38,40 +38,34 @@ def generate_random_map(size=8, p=0.8):
     :param p: probability that a tile is frozen
     """
     valid = False
-    
-    #BFS to check that it's a valid path
+
+    # BFS to check that it's a valid path.
     def is_valid(arr, r=0, c=0):
         if arr[r][c] == 'G':
             return True
-        
+
         tmp = arr[r][c]
         arr[r][c] = "#"
-        
-        if r+1 < size and arr[r+1][c] not in '#H':
-            if is_valid(arr, r+1, c) == True:
-                arr[r][c] = tmp
-                return True
-        
-        if c+1 < size and arr[r][c+1] not in '#H':
-            if is_valid(arr, r, c+1) == True:
-                arr[r][c] = tmp
-                return True
-        
-        if r-1 >= 0 and arr[r-1][c] not in '#H':
-            if is_valid(arr, r-1, c) == True:
-                arr[r][c] = tmp
-                return True
-        
-        if c-1 >= 0 and arr[r][c-1] not in '#H':
-            if is_valid(arr,r, c-1) == True:
-                arr[r][c] = tmp
-                return True
+
+        # Recursively check in all four directions.
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for x, y in directions:
+            r_new = r + x
+            c_new = c + y
+            if r_new < 0 or r_new >= size or c_new < 0 or c_new >= size:
+                continue
+
+            if arr[r_new][c_new] not in '#H':
+                if is_valid(arr, r_new, c_new):
+                    arr[r][c] = tmp
+                    return True
+
         arr[r][c] = tmp
         return False
 
     while not valid:
         p = min(1, p)
-        res = np.random.choice(['F','H'], (size, size), p=[p, 1-p])
+        res = np.random.choice(['F', 'H'], (size, size), p=[p, 1-p])
         res[0][0] = 'S'
         res[-1][-1] = 'G'
         valid = is_valid(res)
