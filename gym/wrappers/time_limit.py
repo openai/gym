5ab62d4d5290d1712d85674d54c743f9e88c7f21
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-03-23 03:12:09 +0100

    Update time_limit.py

--- gym/wrappers/time_limit.py
+++ gym/wrappers/time_limit.py
@@ -2,21 +2,20 @@ import gym
 
 
 class TimeLimit(gym.Wrapper):
-    def __init__(self, env, max_episode_steps=None, done_on_limit=True):
+    def __init__(self, env, max_episode_steps=None):
         super(TimeLimit, self).__init__(env)
         if max_episode_steps is not None:
             self.env.spec.max_episode_steps = max_episode_steps
         self._max_episode_steps = max_episode_steps
-        self._done_on_limit = done_on_limit
         self._elapsed_steps = 0
 
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
         self._elapsed_steps += 1
         if self._elapsed_steps >= self._max_episode_steps:
-            if self._done_on_limit:
-                done = True
-            info['TimeLimit.truncated'] = True
+            if not done:
+                info['TimeLimit.truncated'] = True
+            done = True
         return observation, reward, done, info
 
     def reset(self, **kwargs):
