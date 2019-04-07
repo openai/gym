60743e5977cac362fa45f11d426c1bcad3707606
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-04-07 15:54:56 +0200

    Update registration.py

--- gym/envs/registration.py
+++ gym/envs/registration.py
@@ -26,22 +26,18 @@ class EnvSpec(object):
     Args:
         id (str): The official environment ID
         entry_point (Optional[str]): The Python entrypoint of the environment class (e.g. module.name:Class)
-        trials (int): The number of trials to average reward over
         reward_threshold (Optional[int]): The reward threshold before the task is considered solved
-        local_only: True if the environment is to be used only on the local machine (e.g. debugging envs)
         kwargs (dict): The kwargs to pass to the environment class
         nondeterministic (bool): Whether this environment is non-deterministic even after seeding
         tags (dict[str:any]): A set of arbitrary key-value tags on this environment, including simple property=True tags
 
     Attributes:
         id (str): The official environment ID
-        trials (int): The number of trials run in official evaluation
     """
 
-    def __init__(self, id, entry_point=None, trials=100, reward_threshold=None, local_only=False, kwargs=None, nondeterministic=False, tags=None, max_episode_steps=None, max_episode_seconds=None, timestep_limit=None):
+    def __init__(self, id, entry_point=None, reward_threshold=None, kwargs=None, nondeterministic=False, tags=None, max_episode_steps=None, timestep_limit=None):
         self.id = id
         # Evaluation parameters
-        self.trials = trials
         self.reward_threshold = reward_threshold
         # Environment properties
         self.nondeterministic = nondeterministic
@@ -64,7 +60,6 @@ class EnvSpec(object):
             warnings.warn("register(timestep_limit={}) is deprecated. Use register(max_episode_steps={}) instead.".format(timestep_limit, timestep_limit))
 
         self.max_episode_steps = max_episode_steps
-        self.max_episode_seconds = max_episode_seconds
 
         # We may make some of these other parameters public if they're
         # useful.
@@ -73,7 +68,6 @@ class EnvSpec(object):
             raise error.Error('Attempted to register malformed environment ID: {}. (Currently all IDs must be of the form {}.)'.format(id, env_id_re.pattern))
         self._env_name = match.group(1)
         self._entry_point = entry_point
-        self._local_only = local_only
         self._kwargs = {} if kwargs is None else kwargs
 
     def make(self, **kwargs):
