fb6f087400476cf5ce8495dca8fc77743e3643e7
Hugo <hugovk@users.noreply.github.com>
2019-04-20 19:40:50 +0300

    Add Trove classifiers to clarify support on PyPI

--- setup.py
+++ setup.py
@@ -46,4 +46,12 @@ setup(name='gym',
       },
       tests_require=['pytest', 'mock'],
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
 )
