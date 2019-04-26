65c786a586074ed09a64c4534671440618bbc2ac
Peter Zhokhov <peterz@openai.com>
2019-04-26 16:06:01 -0700

    test for correct observation space keys in GoalEnv

--- gym/core.py
+++ gym/core.py
@@ -170,9 +170,8 @@ class GoalEnv(Env):
         # Enforce that each GoalEnv uses a Goal-compatible observation space.
         if not isinstance(self.observation_space, gym.spaces.Dict):
             raise error.Error('GoalEnv requires an observation space of type gym.spaces.Dict')
-        result = super(GoalEnv, self).reset()
         for key in ['observation', 'achieved_goal', 'desired_goal']:
-            if key not in result:
+            if key not in self.observation_space.spaces:
                 raise error.Error('GoalEnv requires the "{}" key to be part of the observation dictionary.'.format(key))
         return result
 
