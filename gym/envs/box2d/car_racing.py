9780e2cac95d8a1ff0bfa89e0e9a93f93aecb543
Abhinav Sagar <40603139+abhinavsagar@users.noreply.github.com>
2019-05-03 19:17:49 +0530

    Update car_racing.py

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -18,7 +18,7 @@ from pyglet import gl
 #
 # State consists of STATE_W x STATE_H pixels.
 #
-# Reward is for -0.1 every frame and +1000/N for every track tile visited, where N is
+# Reward is -0.1 every frame and +1000/N for every track tile visited, where N is
 # the total number of tiles visited in the track. For example, if you have finished in 732 frames,
 # your reward is 1000 - 0.1*732 = 926.8 points.
 #
