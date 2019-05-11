73732fa394582ac29282e8d0211cb3910c3e59ee
Peter Zhokhov <peterz@openai.com>
2019-05-10 17:32:16 -0700

    more fixes for non-atari (py2.7) builds

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -2,4 +2,3 @@ from gym import error
 from gym.wrappers.monitor import Monitor
 from gym.wrappers.time_limit import TimeLimit
 from gym.wrappers.dict import FlattenDictWrapper
-from gym.wrappers.atari_preprocessing import AtariPreprocessing
