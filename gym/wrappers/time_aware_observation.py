44c2719b1ecdf869c12f52b19094d309ae3d5ecb
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-14 15:13:27 +0200

    Create time_aware_observation.py

---
+++ gym/wrappers/time_aware_observation.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from gym.spaces import Box
+from gym import ObservationWrapper
+
+
+class TimeAwareObservation(ObservationWrapper):
+    r"""Augment the observation with current time step in the trajectory. 
+
+    .. note::
+        Currently it only works with one-dimensional observation space. It doesn't
+        support pixel observation space yet. 
+
+    """
+    def __init__(self, env):
+        super().__init__(env)
+        low = np.append(self.observation_space.low, 0.0)
+        high = np.append(self.observation_space.high, np.inf)
+        self.observation_space = Box(low, high, dtype=np.float32)
+
+    def observation(self, observation):
+        return np.append(observation, self.t)
+
+    def step(self, action):
+        self.t += 1
+        return super().step(action)
+
+    def reset(self, **kwargs):
+        self.t = 0
+        return super().reset(**kwargs)
