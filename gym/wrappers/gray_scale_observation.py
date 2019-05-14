acf69159aabdb2d36d5e45cac0b853389a6db8aa
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-14 15:22:04 +0200

    Update gray_scale_observation.py

--- gym/wrappers/gray_scale_observation.py
+++ gym/wrappers/gray_scale_observation.py
@@ -8,7 +8,7 @@ from gym import ObservationWrapper
 class GrayScaleObservation(ObservationWrapper):
     r"""Convert the image observation from RGB to gray scale. """
     def __init__(self, env, keep_dim=False):
-        super().__init__(env)
+        super(GrayScaleObservation, self).__init__(env)
         self.keep_dim = keep_dim
 
         assert len(env.observation_space.shape) == 3 and env.observation_space.shape[-1] == 3
