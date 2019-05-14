9f9c9f32f70a1a454398eabf934a0a8d04630462
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-14 15:21:03 +0200

    Update clip_action.py

--- gym/wrappers/clip_action.py
+++ gym/wrappers/clip_action.py
@@ -8,7 +8,7 @@ class ClipAction(ActionWrapper):
     r"""Clip the continuous action within the valid bound. """
     def __init__(self, env):
         assert isinstance(env.action_space, Box)
-        super().__init__(env)
+        super(ClipAction, self).__init__(env)
 
     def action(self, action):
         return np.clip(action, self.action_space.low, self.action_space.high)
