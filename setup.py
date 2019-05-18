e90d955db45b381850728e83dee3b61a80c02606
Jesse Farebrother <jessefarebro@gmail.com>
2019-05-17 20:54:33 -0400

    ALE 0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from version import VERSION
 
 # Environment-specific dependencies.
 extras = {
-  'atari': ['atari_py~=0.1.4', 'Pillow', 'opencv-python'],
+  'atari': ['atari_py~=0.2.0', 'Pillow', 'opencv-python'],
   'box2d': ['box2d-py~=2.3.5'],
   'classic_control': [],
   'mujoco': ['mujoco_py>=1.50, <2.1', 'imageio'],
