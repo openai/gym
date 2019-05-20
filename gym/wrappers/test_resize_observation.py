437f63075214ceecf39a5fe5dbc7a09f27b9a9f9
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-20 16:01:19 +0200

    Update test_resize_observation.py

--- gym/wrappers/test_resize_observation.py
+++ gym/wrappers/test_resize_observation.py
@@ -10,12 +10,16 @@ except ImportError:
 
 @pytest.mark.skipif(atari_py is None, reason='Only run this test when atari_py is installed')
 @pytest.mark.parametrize('env_id', ['Pong-v0', 'SpaceInvaders-v0'])
-@pytest.mark.parametrize('size', [16, 32])
-def test_resize_observation(env_id, size):
+@pytest.mark.parametrize('shape', [16, 32, (8, 5)])
+def test_resize_observation(env_id, shape):
     env = gym.make(env_id)
-    env = ResizeObservation(env, size)
+    env = ResizeObservation(env, shape)
 
     assert env.observation_space.shape[-1] == 3
-    assert env.observation_space.shape[:2] == (size, size)
     obs = env.reset()
-    assert obs.shape == (size, size, 3)
+    if isinstance(shape, int):
+        assert env.observation_space.shape[:2] == (shape, shape)
+        assert obs.shape == (shape, shape, 3)
+    else:
+        assert env.observation_space.shape[:2] == shape
+        assert obs.shape == (*shape, 3)
