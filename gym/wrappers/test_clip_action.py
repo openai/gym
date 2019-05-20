b10842805b2bcb284bc88862d23a8ea4619f9ee2
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-20 15:03:24 +0200

    Update test_clip_action.py

--- gym/wrappers/test_clip_action.py
+++ gym/wrappers/test_clip_action.py
@@ -4,19 +4,20 @@ import gym
 from gym.wrappers import ClipAction
 
 
-# mountaincar: action-based rewards
-env = gym.make('MountainCarContinuous-v0')
-wrapped_env = ClipAction(env)
+def test_clip_action():
+    # mountaincar: action-based rewards
+    env = gym.make('MountainCarContinuous-v0')
+    wrapped_env = ClipAction(env)
 
-seed = 0
-env.seed(seed)
-wrapped_env.seed(seed)
+    seed = 0
+    env.seed(seed)
+    wrapped_env.seed(seed)
 
-env.reset()
-wrapped_env.reset()
+    env.reset()
+    wrapped_env.reset()
 
-actions = [[.4], [1.2], [-0.3], [0.0], [-2.5]]
-for action in actions:
-    _, r1, _, _ = env.step(np.clip(action, env.action_space.low, env.action_space.high))
-    _, r2, _, _ = wrapped_env.step(action)
-    assert np.allclose(r1, r2)
+    actions = [[.4], [1.2], [-0.3], [0.0], [-2.5]]
+    for action in actions:
+        _, r1, _, _ = env.step(np.clip(action, env.action_space.low, env.action_space.high))
+        _, r2, _, _ = wrapped_env.step(action)
+        assert np.allclose(r1, r2)
