a4ce40a5c942aec9ddadf551c78a71f7dd5b1ccb
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-20 15:58:17 +0200

    Update resize_observation.py

--- gym/wrappers/resize_observation.py
+++ gym/wrappers/resize_observation.py
@@ -6,17 +6,19 @@ from gym import ObservationWrapper
 
 class ResizeObservation(ObservationWrapper):
     r"""Downsample the image observation to a square image. """
-    def __init__(self, env, size):
+    def __init__(self, env, shape):
         super(ResizeObservation, self).__init__(env)
-        assert size > 0
-        self.size = size
+        if isinstance(shape, int):
+            shape = (shape, shape)
+        assert all(x > 0 for x in shape), shape
+        self.shape = tuple(shape)
 
-        shape = (self.size, self.size) + self.observation_space.shape[2:]
-        self.observation_space = Box(low=0, high=255, shape=shape, dtype=np.uint8)
+        obs_shape = shape + self.observation_space.shape[2:]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
 
     def observation(self, observation):
         import cv2
-        observation = cv2.resize(observation, (self.size, self.size), interpolation=cv2.INTER_AREA)
+        observation = cv2.resize(observation, self.shape, interpolation=cv2.INTER_AREA)
         if observation.ndim == 2:
             observation = np.expand_dims(observation, -1)
         return observation
