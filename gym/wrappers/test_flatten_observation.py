4053b64726f018a278aeee00019a045b30bb38be
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-27 13:32:21 +0200

    Update test_flatten_observation.py

--- gym/wrappers/test_flatten_observation.py
+++ gym/wrappers/test_flatten_observation.py
@@ -2,8 +2,13 @@ import pytest
 
 import gym
 from gym.wrappers import FlattenObservation
+try:
+    import atari_py
+except ImportError:
+    atari_py = None
 
 
+@pytest.mark.skipif(atari_py is None, reason='Only run this test when atari_py is installed')
 @pytest.mark.parametrize('env_id', ['Pong-v0', 'SpaceInvaders-v0'])
 def test_flatten_observation(env_id):
     env = gym.make(env_id)
