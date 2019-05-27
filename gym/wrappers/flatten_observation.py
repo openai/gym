c8eab442b8434072cd50ac2fc1878d53eba6b61a
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-27 13:46:13 +0200

    Update flatten_observation.py

--- gym/wrappers/flatten_observation.py
+++ gym/wrappers/flatten_observation.py
@@ -1,8 +1,14 @@
-from gym.spaces import flatten
+import gym.spaces as spaces
 from gym import ObservationWrapper
 
 
 class FlattenObservation(ObservationWrapper):
     r"""Observation wrapper that flattens the observation."""
+    def __init__(self, env, shape):
+        super(FlattenObservation, self).__init__(env)
+
+        shape = spaces.flatdim(env.observation_space)
+        self.observation_space = spaces.Box(low=-float('inf'), high=float('inf'), shape=shape, dtype=np.float32)
+
     def observation(self, observation):
-        return flatten(self.observation_space, observation)
+        return spaces.flatten(self.observation_space, observation)
