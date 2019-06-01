a1f214d71c3d61aa447a885183696e158f962c68
Tristan Deleu <tristan.deleu@gmail.com>
2019-06-01 19:33:32 -0400

    Add example in documentation for batch_space

--- gym/vector/utils/spaces.py
+++ gym/vector/utils/spaces.py
@@ -23,6 +23,15 @@ def batch_space(space, n=1):
     batched_space : `gym.spaces.Space` instance
         Space (e.g. the observation space) for a batch of environments in the
         vectorized environment.
+
+    Example
+    -------
+    >>> from gym.spaces import Box, Dict
+    >>> space = Dict({
+    ... 'position': Box(low=0, high=1, shape=(3,), dtype=np.float32),
+    ... 'velocity': Box(low=0, high=1, shape=(2,), dtype=np.float32)})
+    >>> batch_space(space, n=5)
+    Dict(position:Box(5, 3), velocity:Box(5, 2))
     """
     if isinstance(space, _BaseGymSpaces):
         return batch_space_base(space, n=n)
