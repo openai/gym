ad6b83e209be257a0e27e7ecd1003b368f546f7f
Tristan Deleu <tristan.deleu@gmail.com>
2019-06-01 19:30:40 -0400

    Add tests for copy in AsyncVectorEnv

--- gym/vector/tests/test_async_vector_env.py
+++ gym/vector/tests/test_async_vector_env.py
@@ -63,6 +63,32 @@ def test_step_async_vector_env(shared_memory):
 
 
 @pytest.mark.parametrize('shared_memory', [True, False])
+def test_copy_async_vector_env(shared_memory):
+    env_fns = [make_env('CubeCrash-v0', i) for i in range(8)]
+    try:
+        env = AsyncVectorEnv(env_fns, shared_memory=shared_memory,
+                             copy=True)
+        observations = env.reset()
+        observations[0] = 128
+        assert not np.all(env.observations[0] == 128)
+    finally:
+        env.close()
+
+
+@pytest.mark.parametrize('shared_memory', [True, False])
+def test_no_copy_async_vector_env(shared_memory):
+    env_fns = [make_env('CubeCrash-v0', i) for i in range(8)]
+    try:
+        env = AsyncVectorEnv(env_fns, shared_memory=shared_memory,
+                             copy=False)
+        observations = env.reset()
+        observations[0] = 128
+        assert np.all(env.observations[0] == 128)
+    finally:
+        env.close()
+
+
+@pytest.mark.parametrize('shared_memory', [True, False])
 def test_reset_timeout_async_vector_env(shared_memory):
     env_fns = [make_slow_env(0.3, i) for i in range(4)]
     with pytest.raises(TimeoutError):
