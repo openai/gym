b0fecfc31bc230e7f016a1298cd5a930e399cfaf
Abhinav <abhinav.sagar2016@vitstudent.ac.in>
2019-06-02 15:04:49 +0530

    Fix gravity constant

--- gym/envs/classic_control/pendulum.py
+++ gym/envs/classic_control/pendulum.py
@@ -26,7 +26,7 @@ class PendulumEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self,u):
+    def step(self,u,**kwargs):
         th, thdot = self.state # th := theta
 
         g = 10.
@@ -44,6 +44,8 @@ class PendulumEnv(gym.Env):
 
         self.state = np.array([newth, newthdot])
         return self._get_obs(), -costs, False, {}
+
+    step(g=9.8)   
 
     def reset(self):
         high = np.array([np.pi, 1])
