3ef62e3ca11270c7ae4b71afb1a1d62d41245ac8
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-06-05 13:28:21 +0200

    Update test_frame_stack.py

--- gym/wrappers/test_frame_stack.py
+++ gym/wrappers/test_frame_stack.py
@@ -14,15 +14,12 @@ except ImportError:
     lz4 = None
 
 
+@pytest.mark.skipif(atari_py is None, reason='Only run this test when atari_py is installed')
+@pytest.mark.skipif(lz4 is None, reason='Only run this test when lz4 is installed')
 @pytest.mark.parametrize('env_id', ['CartPole-v1', 'Pendulum-v0', 'Pong-v0'])
 @pytest.mark.parametrize('num_stack', [2, 3, 4])
 @pytest.mark.parametrize('lz4_compress', [True, False])
 def test_frame_stack(env_id, num_stack, lz4_compress):
-    if env_id == 'Pong-v0' and atari_py is None:
-        return
-    if lz4_compress and lz4 is None:
-        return
-
     env = gym.make(env_id)
     shape = env.observation_space.shape
     env = FrameStack(env, num_stack, lz4_compress)
