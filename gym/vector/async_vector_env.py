2e56028e9aecff96ffb6bc32d488c227933466ed
Tristan Deleu <tristan.deleu@gmail.com>
2019-06-21 22:14:29 -0400

    Handle KeyboardInterrupt exception in AsyncVectorEnv (Fixes #1543)

--- gym/vector/async_vector_env.py
+++ gym/vector/async_vector_env.py
@@ -359,7 +359,7 @@ def _worker(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
                 raise RuntimeError('Received unknown command `{0}`. Must '
                     'be one of {`reset`, `step`, `seed`, `close`, '
                     '`_check_observation_space`}.'.format(command))
-    except Exception:
+    except (KeyboardInterrupt, Exception):
         error_queue.put((index,) + sys.exc_info()[:2])
         pipe.send(None)
     finally:
@@ -398,7 +398,7 @@ def _worker_shared_memory(index, env_fn, pipe, parent_pipe, shared_memory, error
                 raise RuntimeError('Received unknown command `{0}`. Must '
                     'be one of {`reset`, `step`, `seed`, `close`, '
                     '`_check_observation_space`}.'.format(command))
-    except Exception:
+    except (KeyboardInterrupt, Exception):
         error_queue.put((index,) + sys.exc_info()[:2])
         pipe.send(None)
     finally:
