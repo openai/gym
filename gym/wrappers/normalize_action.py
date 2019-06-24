90bbe8ede7c030500e601aa2a2ee664ed947c5d7
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-06-24 16:30:20 +0200

    Update normalize_action.py

--- gym/wrappers/normalize_action.py
+++ gym/wrappers/normalize_action.py
@@ -4,16 +4,19 @@ import gym
 from gym import spaces
 
 
-class NormalizeAction(gym.ActionWrapper):
-    r"""Rescale the continuous action space of the environment from [-1, 1]. """
-    def __init__(self, env):
+class RescaleAction(gym.ActionWrapper):
+    r"""Rescale the continuous action space of the environment from a range of [a, b]. """
+    def __init__(self, env, a, b):
         assert isinstance(env.action_space, spaces.Box), 'expected Box action space.'
+        assert np.less_equal(a, b).all()
         super(NormalizeAction, self).__init__(env)
+        self.a = a
+        self.b = b
 
     def action(self, action):
-        assert np.all(action >= -1.0) and np.all(action <= 1.0), 'expected range within [-1, 1], e.g. use tanh'
+        assert np.greater_equal(action, self.a).all() and np.less_equal(action, self.b).all()
         low = self.env.action_space.low
         high = self.env.action_space.high
-        action = low + (1.0 + action)*(high - low)/2.0
+        action = low + (high - low)*((action - self.a)/(self.b - self.a))
         action = np.clip(action, low, high)
         return action
