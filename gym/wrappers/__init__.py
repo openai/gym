e80141f6668b257b2076196a3c228083dc55559b
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-06-24 16:31:07 +0200

    Update __init__.py

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -3,7 +3,7 @@ from gym.wrappers.monitor import Monitor
 from gym.wrappers.time_limit import TimeLimit
 from gym.wrappers.dict import FlattenDictWrapper
 from gym.wrappers.atari_preprocessing import AtariPreprocessing
-from gym.wrappers.normalize_action import NormalizeAction
+from gym.wrappers.rescale_action import RescaleAction
 from gym.wrappers.resize_observation import ResizeObservation
 from gym.wrappers.clip_action import ClipAction
 from gym.wrappers.clip_reward import ClipReward
