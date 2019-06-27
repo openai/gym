19ff6b3459a776f6c1b497539e6a10cb01e8d0ac
Albert Ge <lbertge@gmail.com>
2019-06-26 18:33:36 -0700

    Recursively check for nested gym.spaces.Dict objects in observation_space.spaces

--- gym/wrappers/dict.py
+++ gym/wrappers/dict.py
@@ -14,11 +14,19 @@ class FlattenDictWrapper(gym.ObservationWrapper):
         self.dict_keys = dict_keys
 
         # Figure out observation_space dimension.
+        size = self.get_dict_size(self.env.observation_space.spaces, dict_keys)
+        self.observation_space = gym.spaces.Box(-np.inf, np.inf, shape=(size,), dtype='float32')
+
+    def get_dict_size(self, spaces, dict_keys):
         size = 0
         for key in dict_keys:
-            shape = self.env.observation_space.spaces[key].shape
-            size += np.prod(shape)
-        self.observation_space = gym.spaces.Box(-np.inf, np.inf, shape=(size,), dtype='float32')
+            space = spaces[key]
+            if isinstance(space, gym.spaces.Dict):
+                size += self.get_dict_size(space.spaces, space.spaces.keys())
+            else:
+                shape = space.shape
+                size += np.prod(shape, dtype=np.int64)
+        return size
 
     def observation(self, observation):
         assert isinstance(observation, dict)
