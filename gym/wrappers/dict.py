cbf6f888255eec65308c5cf6ef1eada1d17542de
Albert Ge <lbertge@gmail.com>
2019-07-01 13:24:32 -0700

    add tests; support nested tuples; asserts for dtype and space

--- gym/wrappers/dict.py
+++ gym/wrappers/dict.py
@@ -12,6 +12,7 @@ class FlattenDictWrapper(gym.ObservationWrapper):
     def __init__(self, env, dict_keys):
         super(FlattenDictWrapper, self).__init__(env)
         self.dict_keys = dict_keys
+        self.dtype = None
 
         # Figure out observation_space dimension.
         size = self.get_dict_size(self.env.observation_space.spaces, dict_keys)
@@ -23,10 +24,31 @@ class FlattenDictWrapper(gym.ObservationWrapper):
             space = spaces[key]
             if isinstance(space, gym.spaces.Dict):
                 size += self.get_dict_size(space.spaces, space.spaces.keys())
+            elif isinstance(space, gym.spaces.Tuple):
+                size += self.get_tuple_size(space.spaces)
             else:
-                shape = space.shape
-                size += np.prod(shape, dtype=np.int64)
+                size += self.get_box_size(space)
         return size
+
+    def get_tuple_size(self, spaces):
+        size = 0
+        for space in spaces:
+            if isinstance(space, gym.spaces.Dict):
+                size += self.get_dict_size(space.spaces, space.spaces.keys())
+            elif isinstance(space, gym.spaces.Tuple):
+                size += self.get_tuple_size(space.spaces)
+            else:
+                size += self.get_box_size(space)
+        return size
+
+    def get_box_size(self, space):
+        assert isinstance(space, gym.spaces.Box), "Only spaces of type Box are supported."
+        if self.dtype is not None:
+            assert space.dtype == self.dtype, "All spaces must have the same dtype."
+        else:
+            self.dtype = space.dtype
+        shape = space.shape
+        return np.prod(shape, dtype=np.int64)
 
     def observation(self, observation):
         assert isinstance(observation, dict)
