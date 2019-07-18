0c574ba5e40450f0772f0533995de0cfb0a4e4d5
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-07-18 13:27:07 +0300

    replace data with get_data()

--- gym/envs/classic_control/rendering.py
+++ gym/envs/classic_control/rendering.py
@@ -102,7 +102,7 @@ class Viewer(object):
         if return_rgb_array:
             buffer = pyglet.image.get_buffer_manager().get_color_buffer()
             image_data = buffer.get_image_data()
-            arr = np.frombuffer(image_data.data, dtype=np.uint8)
+            arr = np.frombuffer(image_data.get_data(), dtype=np.uint8)
             # In https://github.com/openai/gym-http-api/issues/2, we
             # discovered that someone using Xmonad on Arch was having
             # a window of size 598 x 398, though a 600 x 400 window
