ae4784e8c7ff3ec699dc6b3c4cfcedac1300e1ce
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-07-19 18:13:24 +0200

    Update __init__.py

--- gym/vector/__init__.py
+++ gym/vector/__init__.py
@@ -54,6 +54,8 @@ def make(id, num_envs=1, asynchronous=True, wrappers=None, **kwargs):
             elif isinstance(wrappers, Iterable) and all([callable(w) for w in wrappers]):
                 for wrapper in wrappers:
                     env = wrapper(env)
+            else:
+                raise NotImplementedError
         return env
     env_fns = [_make_env for _ in range(num_envs)]
     return AsyncVectorEnv(env_fns) if asynchronous else SyncVectorEnv(env_fns)
