60c0285241f8c6767551fe02c73941d2684ac610
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-07-28 21:18:36 +0200

    Update gym/wrappers/rescale_action.py
    That's a very clean way ! Thanks !
    
    Co-Authored-By: Kristian Hartikainen <kristian.hartikainen@gmail.com>

--- gym/wrappers/rescale_action.py
+++ gym/wrappers/rescale_action.py
@@ -11,7 +11,7 @@ class RescaleAction(gym.ActionWrapper):
             "expected Box action space, got {}".format(type(env.action_space)))
         assert np.less_equal(a, b).all(), (a, b)
         super(RescaleAction, self).__init__(env)
-        if np.isscalar(a):
+        self.a = np.zeros(env.action_space.shape, dtype=env.action_space.dtype) + a
             self.a = np.full(env.action_space.shape, a, dtype=env.action_space.dtype)
         else:
             self.a = a
