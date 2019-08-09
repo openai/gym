ccbe7e62abd622797470cde8637c54892dd5b160
Peter Zhokhov <peterz@openai.com>
2019-08-09 14:57:20 -0700

    fix skipif argument name for hand serialization tests

--- tests/gym/envs/robotics/hand/test_reach.py
+++ tests/gym/envs/robotics/hand/test_reach.py
@@ -6,7 +6,7 @@ from gym import envs
 from gym.envs.tests.spec_list import skip_mujoco, SKIP_MUJOCO_WARNING_MESSAGE
 
 
-@pytest.mark.skipif(skip_mujoco, SKIP_MUJOCO_WARNING_MESSAGE)
+@pytest.mark.skipif(skip_mujoco, reason=SKIP_MUJOCO_WARNING_MESSAGE)
 def test_serialize_deserialize():
     env1 = envs.make('HandReach-v0', distance_threshold=1e-6)
     env1.reset()
