436a3b58797ada34c2f3468536b70270d209a886
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-08-09 15:16:44 -0700

    Fix hand reach v0 pickling (#1497)
    * Add test for HandReach-v0 serialization
    
    * Fix HandReach-v0 serialization
    
    * Add test_{manipulate,manipulate_touch_sensors}.py
    
    * Fix v2 -> v3 mujoco test naming
    
    * Skip hand tests if `skip_mujoco`
    
    * Fix v2 to v3 conversion test naming
    
    * Use pytest.mark.skipif instead of unittest.skipIf
    
    * SKIP_MUJOCO_MESSAGE -> SKIP_MUJOCO_WARNING_MESSAGE rename
    
    * fix skipif argument name for hand serialization tests

---
+++ tests/gym/envs/robotics/hand/test_manipulate_touch_sensors.py
@@ -0,0 +1,24 @@
+import pickle
+
+import pytest
+
+from gym import envs
+from gym.envs.tests.spec_list import skip_mujoco, SKIP_MUJOCO_WARNING_MESSAGE
+
+
+ENVIRONMENT_IDS = (
+    'HandManipulateEggTouchSensors-v1',
+    'HandManipulatePenTouchSensors-v0',
+    'HandManipulateBlockTouchSensors-v0',
+)
+
+
+@pytest.mark.skipif(skip_mujoco, reason=SKIP_MUJOCO_WARNING_MESSAGE)
+@pytest.mark.parametrize("environment_id", ENVIRONMENT_IDS)
+def test_serialize_deserialize(environment_id):
+    env1 = envs.make(environment_id, target_position='fixed')
+    env1.reset()
+    env2 = pickle.loads(pickle.dumps(env1))
+
+    assert env1.target_position == env2.target_position, (
+        env1.target_position, env2.target_position)
