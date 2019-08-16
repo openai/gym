4c9e71fb5ac78e9f235bb4fa2ac0c5d8b7556310
Peter Zhokhov <peterz@openai.com>
2019-08-16 16:27:28 -0700

    re-enable and fix atari preprocessing wrappers pixel test

--- gym/wrappers/test_atari_preprocessing.py
+++ gym/wrappers/test_atari_preprocessing.py
@@ -1,24 +1,25 @@
 import numpy as np
 import gym
-import pytest
 from gym.wrappers import AtariPreprocessing
-try:
-    import atari_py
-except ImportError:
-    atari_py = None
+import pytest
+pytest.importorskip('atari_py')
 
-@pytest.mark.skipif(atari_py is None, reason='Only run this test when atari_py is installed')
 def test_atari_preprocessing():
     import cv2
-    env1 = gym.make('PongNoFrameskip-v0')
-    env2 = AtariPreprocessing(env1, screen_size=84, grayscale_obs=True)
-    env3 = AtariPreprocessing(env1, screen_size=84, grayscale_obs=False)
-    obs1 = env1.reset()
-    assert obs1.shape == (210, 160, 3)
+    env_fn = lambda: gym.make('PongNoFrameskip-v4')
+    env1 = env_fn()
+    env2 = AtariPreprocessing(env_fn(), screen_size=84, grayscale_obs=True, frame_skip=1, noop_max=0)
+    env3 = AtariPreprocessing(env_fn(), screen_size=84, grayscale_obs=False, frame_skip=1, noop_max=0)
+    env1.reset()
+    # take these steps to imitate actions of FireReset logic
+    env1.step(1)
+    obs1 = env1.step(2)[0]
     obs2 = env2.reset()
-    assert obs2.shape == (84, 84)
     obs3 = env3.reset()
+    assert obs1.shape == (210, 160, 3)
+    assert obs2.shape == (84, 84)
     assert obs3.shape == (84, 84, 3)
-    # TODO peterz - figure out why assertions below are faliing and fix
-    # np.testing.assert_allclose(obs3, cv2.resize(obs1, (84, 84), interpolation=cv2.INTER_AREA))
-    # np.testing.assert_allclose(obs2, cv2.cvtColor(obs3, cv2.COLOR_RGB2GRAY))
+    np.testing.assert_allclose(obs3, cv2.resize(obs1, (84, 84), interpolation=cv2.INTER_AREA))
+    obs3_gray = cv2.cvtColor(obs3, cv2.COLOR_RGB2GRAY)
+    # the edges of the numbers do not render quite the same in the grayscale, so we ignore them
+    np.testing.assert_allclose(obs2[10:], obs3_gray[10:])
