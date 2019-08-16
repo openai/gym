7dd6c108b052b720436a09090320b18a0b8e9e10
Peter Zhokhov <peterz@openai.com>
2019-08-16 15:02:10 -0700

    make framestack tests actually run (with and without lz4)

--- gym/wrappers/frame_stack.py
+++ gym/wrappers/frame_stack.py
@@ -17,17 +17,18 @@ class LazyFrames(object):
 
     """
     def __init__(self, frames, lz4_compress=False):
-        from lz4.block import compress
-        from lz4.block import decompress
         if lz4_compress:
+            from lz4.block import compress
             self.shape = frames[0].shape
+            self.dtype = frames[0].dtype
             frames = [compress(frame) for frame in frames]
         self._frames = frames
         self.lz4_compress = lz4_compress
 
     def __array__(self, dtype=None):
         if self.lz4_compress:
-            frames = [np.frombuffer(decompress(frame), dtype=np.uint8).reshape(self.shape) for frame in self._frames]
+            from lz4.block import decompress
+            frames = [np.frombuffer(decompress(frame), dtype=self.dtype).reshape(self.shape) for frame in self._frames]
         else:
             frames = self._frames
         out = np.stack(frames, axis=0)
--- gym/wrappers/test_frame_stack.py
+++ gym/wrappers/test_frame_stack.py
@@ -1,24 +1,21 @@
 import pytest
+pytest.importorskip("atari_py")
 
 import numpy as np
-
 import gym
 from gym.wrappers import FrameStack
-try:
-    import atari_py
-except ImportError:
-    atari_py = None
 try:
     import lz4
 except ImportError:
     lz4 = None
 
 
-@pytest.mark.skipif(atari_py is None, reason='Only run this test when atari_py is installed')
-@pytest.mark.skipif(lz4 is None, reason='Only run this test when lz4 is installed')
 @pytest.mark.parametrize('env_id', ['CartPole-v1', 'Pendulum-v0', 'Pong-v0'])
 @pytest.mark.parametrize('num_stack', [2, 3, 4])
-@pytest.mark.parametrize('lz4_compress', [True, False])
+@pytest.mark.parametrize('lz4_compress', [
+    pytest.param(True, marks=pytest.mark.skipif(lz4 is None, reason="Need lz4 to run tests with compression")),
+    False
+])
 def test_frame_stack(env_id, num_stack, lz4_compress):
     env = gym.make(env_id)
     shape = env.observation_space.shape
