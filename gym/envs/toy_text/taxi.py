a0264410f26a6ea50f13d748ca0ad20c2dc1cc40
kobotics <kobotics@users.noreply.github.com>
2019-08-23 18:43:09 -0400

    Updated Taxi-v2 to Taxi-v3 (map correction + cleaner domain description) (#1653)

--- gym/envs/toy_text/taxi.py
+++ gym/envs/toy_text/taxi.py
@@ -8,7 +8,7 @@ import numpy as np
 MAP = [
     "+---------+",
     "|R: | : :G|",
-    "| : : : : |",
+    "| : | : : |",
     "| : : : : |",
     "| | : | : |",
     "|Y| : |B: |",
@@ -23,12 +23,25 @@ class TaxiEnv(discrete.DiscreteEnv):
     by Tom Dietterich
 
     Description:
-    There are four designated locations in the grid world indicated by R(ed), B(lue), G(reen), and Y(ellow). When the episode starts, the taxi starts off at a random square and the passenger is at a random location. The taxi drive to the passenger's location, pick up the passenger, drive to the passenger's destination (another one of the four specified locations), and then drop off the passenger. Once the passenger is dropped off, the episode ends.
+    There are four designated locations in the grid world indicated by R(ed), G(reen), Y(ellow), and B(lue). When the episode starts, the taxi starts off at a random square and the passenger is at a random location. The taxi drives to the passenger's location, picks up the passenger, drives to the passenger's destination (another one of the four specified locations), and then drops off the passenger. Once the passenger is dropped off, the episode ends.
 
     Observations: 
-    There are 500 discrete states since there are 25 taxi positions, 5 possible locations of the passenger (including the case when the passenger is the taxi), and 4 destination locations. 
+    There are 500 discrete states since there are 25 taxi positions, 5 possible locations of the passenger (including the case when the passenger is in the taxi), and 4 destination locations. 
     
-    Actions: 
+    Passenger locations:
+    - 0: R(ed)
+    - 1: G(reen)
+    - 2: Y(ellow)
+    - 3: B(lue)
+    - 4: in taxi
+    
+    Destinations:
+    - 0: R(ed)
+    - 1: G(reen)
+    - 2: Y(ellow)
+    - 3: B(lue)
+        
+    Actions:
     There are 6 discrete deterministic actions:
     - 0: move south
     - 1: move north
@@ -46,15 +59,8 @@ class TaxiEnv(discrete.DiscreteEnv):
     - magenta: destination
     - yellow: empty taxi
     - green: full taxi
-    - other letters (R, G, B and Y): locations for passengers and destinations
-
-    actions:
-    - 0: south
-    - 1: north
-    - 2: east
-    - 3: west
-    - 4: pickup
-    - 5: dropoff
+    - other letters (R, G, Y and B): locations for passengers and destinations
+    
 
     state space is represented by:
         (taxi_row, taxi_col, passenger_location, destination)
