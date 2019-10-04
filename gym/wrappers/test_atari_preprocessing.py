1b5671e36ce78c9ee90ef0b1aba5953c57c15d87
Anurag Koul <koulanurag@gmail.com>
2019-10-04 14:19:00 -0700

    Adding Frame Normalization in Atari Pre-Processing (#1680)
    * added frame scaling in atari preprocessing
    - default value is False
    
    * added frame scaling in atari preprocessing
    - default value is False
    
    * corrected logic for selection of observation_space params
    
    * initial commit for scale test
    
    * added test-case for observation scaling in atari
    
    * added message related to framestack in docstring.
    
    * refactored scaled obs test for atari preprocessing

--- gym/wrappers/test_atari_preprocessing.py
+++ gym/wrappers/test_atari_preprocessing.py
@@ -2,11 +2,17 @@ import numpy as np
 import gym
 from gym.wrappers import AtariPreprocessing
 import pytest
+
 pytest.importorskip('atari_py')
 
-def test_atari_preprocessing():
+
+@pytest.fixture(scope='module')
+def env_fn():
+    return lambda: gym.make('PongNoFrameskip-v4')
+
+
+def test_atari_preprocessing_grayscale(env_fn):
     import cv2
-    env_fn = lambda: gym.make('PongNoFrameskip-v4')
     env1 = env_fn()
     env2 = AtariPreprocessing(env_fn(), screen_size=84, grayscale_obs=True, frame_skip=1, noop_max=0)
     env3 = AtariPreprocessing(env_fn(), screen_size=84, grayscale_obs=False, frame_skip=1, noop_max=0)
@@ -23,3 +29,28 @@ def test_atari_preprocessing():
     obs3_gray = cv2.cvtColor(obs3, cv2.COLOR_RGB2GRAY)
     # the edges of the numbers do not render quite the same in the grayscale, so we ignore them
     np.testing.assert_allclose(obs2[10:], obs3_gray[10:])
+
+    env1.close()
+    env2.close()
+    env3.close()
+
+
+def test_atari_preprocessing_scale(env_fn):
+    # arbitrarily chosen number for stepping into env. and ensuring all observations are in the required range
+    max_test_steps = 10
+
+    for grayscale in [True, False]:
+        for scaled in [True, False]:
+            env = AtariPreprocessing(env_fn(), screen_size=84, grayscale_obs=grayscale, scale_obs=scaled,
+                                     frame_skip=1, noop_max=0)
+            obs = env.reset().flatten()
+            done, step_i = False, 0
+            max_obs = 1 if scaled else 255
+            assert (0 <= obs).all() and (obs <= max_obs).all(), 'Obs. must be in range [0,{}]'.format(max_obs)
+            while not done or step_i <= max_test_steps:
+                obs, _, done, _ = env.step(env.action_space.sample())
+                obs = obs.flatten()
+                assert (0 <= obs).all() and (obs <= max_obs).all(), 'Obs. must be in range [0,{}]'.format(max_obs)
+                step_i += 1
+
+            env.close()
