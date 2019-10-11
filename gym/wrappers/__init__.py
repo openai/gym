ed8b13e11afe82b1352fb84a9d8b29331ec4f654
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-10-11 23:58:04 +0200

    [Wrappers]: add TransformObservation (#1670)
    * Create transform_observation.py
    
    * Create test_transform_observation.py
    
    * Update __init__.py

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -8,6 +8,7 @@ from gym.wrappers.flatten_observation import FlattenObservation
 from gym.wrappers.gray_scale_observation import GrayScaleObservation
 from gym.wrappers.frame_stack import LazyFrames
 from gym.wrappers.frame_stack import FrameStack
+from gym.wrappers.transform_observation import TransformObservation
 from gym.wrappers.transform_reward import TransformReward
 from gym.wrappers.resize_observation import ResizeObservation
 from gym.wrappers.clip_action import ClipAction
