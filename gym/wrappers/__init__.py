f304c94cbd9ead834ef5fe96b0fb10327805e51d
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-10-19 00:53:24 +0200

    [Delete dict.py]: replace with FilterObservation + FlattenObservation (#1669)
    * Delete dict.py
    
    * Update __init__.py
    
    * Update nested_dict_test.py
    
    * update nested_dict_test
    
    * oh huh revert commented out part of the test, now the test fails due to the lack of proper error :(
    
    * remove incorrect argument test - now all combinations of spaces and dtypes are possible to flatten (eveything is reduced to float32, discrete are one-hot encoded)

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -1,7 +1,6 @@
 from gym import error
 from gym.wrappers.monitor import Monitor
 from gym.wrappers.time_limit import TimeLimit
-from gym.wrappers.dict import FlattenDictWrapper
 from gym.wrappers.filter_observation import FilterObservation
 from gym.wrappers.atari_preprocessing import AtariPreprocessing
 from gym.wrappers.flatten_observation import FlattenObservation
