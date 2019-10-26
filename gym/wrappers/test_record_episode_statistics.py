5139614b93cf5868a83f4f5e82ee4ccc3244048e
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-10-26 08:59:22 +0200

    Update test_record_episode_statistics.py

--- gym/wrappers/test_record_episode_statistics.py
+++ gym/wrappers/test_record_episode_statistics.py
@@ -13,12 +13,12 @@ def test_record_episode_statistics(env_id, deque_size):
     for n in range(5):
         env.reset()
         assert env.episode_return == 0.0
-        assert env.episode_horizon == 0
+        assert env.episode_length == 0
         for t in range(env.spec.max_episode_steps):
             _, _, done, info = env.step(env.action_space.sample())
             if done:
                 assert 'episode' in info
-                assert all([item in info['episode'] for item in ['return', 'horizon', 'time']])
+                assert all([item in info['episode'] for item in ['r', 'l', 't']])
                 break
     assert len(env.return_queue) == deque_size
-    assert len(env.horizon_queue) == deque_size
+    assert len(env.length_queue) == deque_size
