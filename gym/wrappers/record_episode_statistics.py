081ff6f849e3ba9ca4e8191b7f74067a1fe944bf
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-10-26 08:58:19 +0200

    Update record_episode_statistics.py

--- gym/wrappers/record_episode_statistics.py
+++ gym/wrappers/record_episode_statistics.py
@@ -9,26 +9,26 @@ class RecordEpisodeStatistics(gym.Wrapper):
         super(RecordEpisodeStatistics, self).__init__(env)
         self.t0 = time.time()  # TODO: use perf_counter when gym removes Python 2 support
         self.episode_return = 0.0
-        self.episode_horizon = 0
+        self.episode_length = 0
         self.return_queue = deque(maxlen=deque_size)
-        self.horizon_queue = deque(maxlen=deque_size)
+        self.length_queue = deque(maxlen=deque_size)
 
     def reset(self, **kwargs):
         observation = super(RecordEpisodeStatistics, self).reset(**kwargs)
         self.episode_return = 0.0
-        self.episode_horizon = 0
+        self.episode_length = 0
         return observation
 
     def step(self, action):
         observation, reward, done, info = super(RecordEpisodeStatistics, self).step(action)
         self.episode_return += reward
-        self.episode_horizon += 1
+        self.episode_length += 1
         if done:
-            info['episode'] = {'return': self.episode_return, 
-                               'horizon': self.episode_horizon, 
-                               'time': round(time.time() - self.t0, 4)}
+            info['episode'] = {'r': self.episode_return, 
+                               'l': self.episode_length, 
+                               't': round(time.time() - self.t0, 6)}
             self.return_queue.append(self.episode_return)
-            self.horizon_queue.append(self.episode_horizon)
+            self.length_queue.append(self.episode_length)
             self.episode_return = 0.0
-            self.episode_horizon = 0
+            self.episode_length = 0
         return observation, reward, done, info
