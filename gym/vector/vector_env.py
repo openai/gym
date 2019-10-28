f46992049df16300bc645fbd1b4424c889d742b8
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-10-28 15:17:12 +0100

    Update vector_env.py

--- gym/vector/vector_env.py
+++ gym/vector/vector_env.py
@@ -74,44 +74,11 @@ class VectorEnvWrapper(VectorEnv):
     def __init__(self, env):
         assert isinstance(env, VectorEnv)
         self.env = env
-        self.metadata = env.metadata
-        self.num_envs = env.num_envs
-        self.observation_space = env.observation_space
-        self.action_space = env.action_space
-        self.single_observation_space = env.single_observation_space
-        self.single_action_space = env.single_action_space
-        self.reward_range = env.reward_range
 
     def __getattr__(self, name):
         if name.startswith('_'):
             raise AttributeError("attempted to get missing private attribute '{}'".format(name))
         return getattr(self.env, name)
-
-    @property
-    def spec(self):
-        return self.env.spec
-
-    @classmethod
-    def class_name(cls):
-        return cls.__name__
-
-    def seed(self, seeds=None):
-        return self.env.seed(seeds)
-
-    def reset(self):
-        return self.env.reset()
-
-    def step(self, actions):
-        return self.env.step(actions)
-
-    def get_images(self):
-        return self.env.get_images()
-
-    def render(self, mode='human', **kwargs):
-        return self.env.render(mode, **kwargs)
-
-    def close(self, **kwargs):
-        return self.env.close(**kwargs)
 
     @property
     def unwrapped(self):
