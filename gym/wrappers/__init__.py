97ac555c03c6e50966bbbb381aaa69484c855794
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-11-01 22:27:39 +0100

    [Wrapper]: RecordEpisodeStatistics (#1628)
    * Create record_episode_statistics.py
    
    * Create test_record_episode_statistics.py
    
    * Update __init__.py
    
    * Update record_episode_statistics.py
    
    * Update record_episode_statistics.py
    
    * Update test_record_episode_statistics.py
    
    * Update record_episode_statistics.py
    
    * Update test_record_episode_statistics.py

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -12,3 +12,4 @@ from gym.wrappers.transform_observation import TransformObservation
 from gym.wrappers.transform_reward import TransformReward
 from gym.wrappers.resize_observation import ResizeObservation
 from gym.wrappers.clip_action import ClipAction
+from gym.wrappers.record_episode_statistics import RecordEpisodeStatistics
