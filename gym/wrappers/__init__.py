a67da20dd3a0b8d8b7ffbb2fdec9a133e7bbfc51
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-11-21 15:54:29 +0100

    Update __init__.py

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -13,3 +13,4 @@ from gym.wrappers.transform_reward import TransformReward
 from gym.wrappers.resize_observation import ResizeObservation
 from gym.wrappers.clip_action import ClipAction
 from gym.wrappers.record_episode_statistics import RecordEpisodeStatistics
+from gym.wrappers.timestep_env import TimeStepEnv
