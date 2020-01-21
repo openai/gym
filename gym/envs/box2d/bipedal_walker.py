a5f4202c1b2193401f992ca7805b3eb226c51675
chozabu <chozabu@gmail.com>
2020-01-21 03:30:08 +0000

    additional fix for raycast failing to get nearest point

--- gym/envs/box2d/bipedal_walker.py
+++ gym/envs/box2d/bipedal_walker.py
@@ -365,7 +365,7 @@ class BipedalWalker(gym.Env, EzPickle):
         class LidarCallback(Box2D.b2.rayCastCallback):
             def ReportFixture(self, fixture, point, normal, fraction):
                 if (fixture.filterData.categoryBits & 1) == 0:
-                    return 1
+                    return -1
                 self.p2 = point
                 self.fraction = fraction
                 return fraction
