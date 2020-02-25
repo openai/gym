d77639ff9289d6598f55386663c365f1947f9c98
Peter Zhokhov <peterz@openai.com>
2020-02-25 13:25:21 +0100

    oops i did not realize python 3.9 is still in alpha

--- setup.py
+++ setup.py
@@ -50,6 +50,5 @@ setup(name='gym',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
       ],
 )
