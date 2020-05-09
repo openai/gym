b8adf945b6069aecc97e78833468e5c85a82e70d
instance01 <nibbadont@protonmail.com>
2020-05-09 10:47:41 +0200

    Improve algorithmic environments.
    Improve legibility and adhere to PEP-8.

--- gym/envs/algorithmic/reverse.py
+++ gym/envs/algorithmic/reverse.py
@@ -9,7 +9,9 @@ class ReverseEnv(algorithmic_env.TapeAlgorithmicEnv):
     MIN_REWARD_SHORTFALL_FOR_PROMOTION = -.1
 
     def __init__(self, base=2):
-        super(ReverseEnv, self).__init__(base=base, chars=True, starting_min_length=1)
+        super(ReverseEnv, self).__init__(
+            base=base, chars=True, starting_min_length=1
+        )
         self.last = 50
 
     def target_from_input_data(self, input_str):
