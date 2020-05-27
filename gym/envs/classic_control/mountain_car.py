1d5325b9a7f4a58142c3b266e323d507f9b6f67d
Pierluigi Urru <5012027+stwykd@users.noreply.github.com>
2020-05-27 22:01:37 +0100

    Change tabs into spaces
    Tabs were mixed up with spaces, resulting in the file being weirdly formatted in editors were tabs are not rendered with 4 spaces (e.g. GitHub https://github.com/openai/gym/edit/master/gym/envs/classic_control/mountain_car.py)

--- gym/envs/classic_control/mountain_car.py
+++ gym/envs/classic_control/mountain_car.py
@@ -22,15 +22,15 @@ class MountainCarEnv(gym.Env):
     
     Observation: 
          Type: Box(2)
-         Num	Observation               Min            Max
-         0	    Car Position             -1.2            0.6
-         1	    Car Velocity             -0.07           0.07
-    
+         Num    Observation               Min            Max
+         0      Car Position              -1.2           0.6
+         1      Car Velocity              -0.07          0.07
+
     Actions:
          Type: Discrete(3)
-         Num	Action
-         0	    Accelerate to the Left
-         1	    Dont accelerate
+         Num    Action
+         0      Accelerate to the Left
+         1      Don't accelerate
          2      Accelerate to the Right
         
          Note: This does not affect the amount of velocity affected by the gravitational pull acting on the car
