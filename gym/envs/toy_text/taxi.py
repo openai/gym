5cbb9c3bc9ea2dfc7765464fe9f4c973421fedcd
Miguel Morales <mimoralea@gmail.com>
2020-06-19 15:49:44 -0600

    Update taxi.py (#1943)
    * Update taxi.py
    
    Clarify reward comments
    
    * Update taxi.py

--- gym/envs/toy_text/taxi.py
+++ gym/envs/toy_text/taxi.py
@@ -51,7 +51,9 @@ class TaxiEnv(discrete.DiscreteEnv):
     - 5: dropoff passenger
     
     Rewards: 
-    There is a reward of -1 for each action and an additional reward of +20 for delivering the passenger. There is a reward of -10 for executing actions "pickup" and "dropoff" illegally.
+    There is a default per-step reward of -1,
+    except for delivering the passenger, which is +20,
+    or executing "pickup" and "drop-off" actions illegally, which is -10.
     
 
     Rendering:
