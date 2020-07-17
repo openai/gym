5d68a5f91351a23d69dff4f96326d13eb23e5b35
instance01 <nibbadont@protonmail.com>
2020-07-17 06:26:44 +0200

    Improve description and remove unused imports.

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -1,34 +1,36 @@
 """
-
-Easiest continuous control task to learn from pixels, a top-down racing environment.
-Discrete control is reasonable in this environment as well, on/off discretization is
-fine.
+Easiest continuous control task to learn from pixels, a top-down racing
+environment.
+Discrete control is reasonable in this environment as well, on/off
+discretization is fine.
 
 State consists of STATE_W x STATE_H pixels.
 
-The reward is -0.1 every frame and +1000/N for every track tile visited, where N is
-the total number of tiles visited in the track. For example, if you have finished in 732 frames,
-your reward is 1000 - 0.1*732 = 926.8 points.
+The reward is -0.1 every frame and +1000/N for every track tile visited, where
+N is the total number of tiles visited in the track. For example, if you have
+finished in 732 frames, your reward is 1000 - 0.1*732 = 926.8 points.
 
-The game is solved when the agent consistently gets 900+ points. The generated track is random every episode.
+The game is solved when the agent consistently gets 900+ points. The generated
+track is random every episode.
 
-The episode finishes when all the tiles are visited. The car also can go outside of the PLAYFIELD -  that
-is far off the track, then it will get -100 and die.
+The episode finishes when all the tiles are visited. The car also can go
+outside of the PLAYFIELD -  that is far off the track, then it will get -100
+and die.
 
-Some indicators are shown at the bottom of the window along with the state RGB buffer. From
-left to right: the true speed, four ABS sensors, the steering wheel position and gyroscope.
+Some indicators are shown at the bottom of the window along with the state RGB
+buffer. From left to right: the true speed, four ABS sensors, the steering
+wheel position and gyroscope.
 
 To play yourself (it's rather fast for humans), type:
 
 python gym/envs/box2d/car_racing.py
 
-Remember it's a powerful rear-wheel drive car -  don't press the accelerator and turn at the
-same time.
+Remember it's a powerful rear-wheel drive car -  don't press the accelerator
+and turn at the same time.
 
 Created by Oleg Klimov. Licensed on the same terms as the rest of OpenAI Gym.
 """
-
-import sys, math
+import math
 import numpy as np
 
 import Box2D
@@ -37,7 +39,7 @@ from Box2D.b2 import (edgeShape, circleShape, fixtureDef, polygonShape, revolute
 import gym
 from gym import spaces
 from gym.envs.box2d.car_dynamics import Car
-from gym.utils import colorize, seeding, EzPickle
+from gym.utils import seeding, EzPickle
 
 import pyglet
 from pyglet import gl
