574385fea61cab355909941a2694e7482de1583c
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-10-18 17:02:13 -0400

    BatchedVectorEnv follows VectorEnv Async/wait API
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- gym/vector/batched_vector_env.py
+++ gym/vector/batched_vector_env.py
@@ -11,14 +11,15 @@ import gym
 import numpy as np
 from gym import spaces
 
-from .sync_vector_env import SyncVectorEnv
-from .async_vector_env import AsyncVectorEnv
+from gym.vector.vector_env import VectorEnv
+from gym.vector.sync_vector_env import SyncVectorEnv
+from gym.vector.async_vector_env import AsyncVectorEnv
 
 
 T = TypeVar("T")
 
 
-class BatchedVectorEnv(gym.Env):
+class BatchedVectorEnv(VectorEnv):
     """ Batched vectorized environment.
 
     Adds the following features, compared to using the vectorized Async and Sync
@@ -41,12 +42,14 @@ class BatchedVectorEnv(gym.Env):
         In the first environment (env_a), each env will contain
         ceil(n_envs / n_workers) each. If env_b is needed, then each of its envs
         will contain floor(n_envs / n_workers) environments.
-
+    
     The observations/actions/rewards are reshaped to be (n_envs, *shape), i.e.
     they don't have an extra 'chunk' dimension.
 
-    NOTE: In order to get this to work, I had to modify the `if done:` statement
-    in the worker to be `if done if isinstance(done, bool) else all(done):`.
+    -   When some environments have `done=True` while stepping, those
+        environments are reset, as was done previously. Additionally, the final
+        observation for those environments is placed in the info dict at key
+        FINAL_STATE_KEY (currently 'final_state').
     """
     def __init__(self,
                  env_fns,
@@ -54,7 +57,20 @@ class BatchedVectorEnv(gym.Env):
                  **kwargs):
         assert env_fns, "need at least one env_fn."
         self.batch_size: int = len(env_fns)
-        
+
+        # Use one of the env_fns to get the observation/action space.
+        with env_fns[0]() as temp_env:
+            single_observation_space = temp_env.observation_space
+            single_action_space = temp_env.action_space
+            self.reward_range = temp_env.reward_range
+        del temp_env
+
+        super().__init__(
+            num_envs=self.batch_size,
+            observation_space=single_observation_space,
+            action_space=single_action_space,
+        )
+
         if n_workers is None:
             n_workers = mp.cpu_count()
         self.n_workers: int = n_workers
@@ -66,16 +82,18 @@ class BatchedVectorEnv(gym.Env):
         groups = distribute(env_fns, self.n_workers)
 
         # Find the first index where the group has a different length.
-        self.chunk_a = len(groups[0])
-        self.chunk_b = 0
-        # First, assume there is no need for another environment, as all the
-        # groups have the same length.
+        self.chunk_length_a = len(groups[0])
+        self.chunk_length_b = 0
+
+        # First, assume there is no need for another environment (all the
+        # groups have the same length).
         self.start_index_b = self.n_workers
         for i, group in enumerate(groups):
-            if len(group) != self.chunk_a:
+            if len(group) != self.chunk_length_a:
                 self.start_index_b = i
-                self.chunk_b = len(group)
+                self.chunk_length_b = len(group)
                 break
+
         # Total number of envs in each environment.
         self.n_a = sum(map(len, groups[:self.start_index_b]))
         self.n_b = sum(map(len, groups[self.start_index_b:]))
@@ -93,91 +111,70 @@ class BatchedVectorEnv(gym.Env):
         if env_b_fns:
             self.env_b = AsyncVectorEnv(env_fns=env_b_fns, **kwargs)
 
-        self.observation_space: gym.Space  # type: ignore
-        self.action_space: gym.Space  # type: ignore
+        # Unbatch & join the observations/actions spaces.        
 
-        # Unbatch & join the observations/actions spaces.
-        flat_obs_a = remove_extra_batch_dim(self.env_a.observation_space)  # type: ignore
-        flat_act_a = remove_extra_batch_dim(self.env_a.action_space)  # type: ignore
+    def reset_async(self):
+        self.env_a.reset_async()
         if self.env_b:
-            flat_obs_b = remove_extra_batch_dim(self.env_b.observation_space)  # type: ignore
-            flat_act_b = remove_extra_batch_dim(self.env_b.action_space)  # type: ignore
-            
-            self.observation_space = concat_spaces(flat_obs_a, flat_obs_b)
-            self.action_space = concat_spaces(flat_act_a, flat_act_b)
-        else:
-            self.observation_space = flat_obs_a
-            self.action_space = flat_act_a
+            self.env_b.reset_async()
 
-        self.reward_range = self.env_a.reward_range
-
-    def reset(self):
-        obs_a = self.env_a.reset()
+    def reset_wait(self, timeout=None, **kwargs):
+        obs_a = self.env_a.reset_wait(timeout=timeout)
         if self.env_b:
-            obs_b = self.env_b.reset()
-            return self.unchunk(obs_a, obs_b)
-        return self.unchunk(obs_a)
-
-    def step(self, action: Sequence):
+            obs_b = self.env_b.reset_wait(timeout=timeout)
+            return unchunk(obs_a, obs_b)
+        return unchunk(obs_a)
+    
+    def step_async(self, action: Sequence):
         if self.env_b:
             flat_actions_a, flat_actions_b = action[:self.n_a], action[self.n_a:]
-            actions_a = self.chunk(flat_actions_a, self.chunk_a)
-            actions_b = self.chunk(flat_actions_b, self.chunk_b)
+            actions_a = chunk(flat_actions_a, self.chunk_length_a)
+            actions_b = chunk(flat_actions_b, self.chunk_length_b)
+            self.env_a.step_async(actions_a)
+            self.env_b.step_async(actions_b)
 
-            obs_a, rew_a, done_a, info_a = self.env_a.step(actions_a)
-            obs_b, rew_b, done_b, info_b = self.env_b.step(actions_b)
-
-            observations = self.unchunk(obs_a, obs_b)
-            rewards = self.unchunk(rew_a, rew_b)
-            done = self.unchunk(done_a, done_b)
-            info = self.unchunk(info_a, info_b)
         else:
-            action = self.chunk(action, self.chunk_a)
-            observations, rewards, done, info = self.env_a.step(action)
+            action = chunk(action, self.chunk_length_a)
+            self.env_a.step_async(action)
 
-            observations = self.unchunk(observations)
-            rewards = self.unchunk(rewards)
-            done = self.unchunk(done)
-            info = self.unchunk(info)
+    def step_wait(self, timeout: Union[int, float]=None):
+        if self.env_b:
+            obs_a, rew_a, done_a, info_a = self.env_a.step_wait(timeout)
+            obs_b, rew_b, done_b, info_b = self.env_b.step_wait(timeout)
+
+            observations = unchunk(obs_a, obs_b)
+            rewards = unchunk(rew_a, rew_b)
+            done = unchunk(done_a, done_b)
+            info = unchunk(info_a, info_b)
+        else:
+            observations, rewards, done, info = self.env_a.step_wait(timeout)
+
+            observations = unchunk(observations)
+            rewards = unchunk(rewards)
+            done = unchunk(done)
+            info = unchunk(info)
         return observations, rewards, done, info
 
-    def seed(self, seeds: Union[Optional[int], Sequence[Optional[int]]] = None):
+
+    def seed(self, seeds: Union[int, Sequence[Optional[int]]] = None):
         if seeds is None:
             seeds = [None for _ in range(self.batch_size)]
         if isinstance(seeds, int):
             seeds = [seeds + i for i in range(self.batch_size)]
         assert len(seeds) == self.batch_size
 
-        seeds_a = self.chunk(seeds[:self.n_a], self.chunk_a)
-        seeds_b = self.chunk(seeds[self.n_a:], self.chunk_b)
+        seeds_a = chunk(seeds[:self.n_a], self.chunk_length_a)
+        seeds_b = chunk(seeds[self.n_a:], self.chunk_length_b)
         self.env_a.seed(seeds_a)
         if self.env_b:
             self.env_b.seed(seeds_b)       
 
-    def close(self):
-        self.env_a.close()
+
+    def close_extras(self, **kwargs):
+        r"""Clean up the extra resources e.g. beyond what's in this base class. """
+        self.env_a.close_extras(**kwargs)
         if self.env_b:
-            self.env_b.close()
-
-    @staticmethod
-    def unchunk(*values: Sequence[Sequence[T]]) -> Sequence[T]:
-        """ Combine 'chunked' results coming from the envs into a single
-        batch.
-        """
-        all_values: List[T] = []
-        for sequence in values:
-            all_values.extend(itertools.chain.from_iterable(sequence))
-        if isinstance(values[0], np.ndarray):
-            return np.array(all_values)
-        return all_values
-
-    @staticmethod
-    def chunk(values: Sequence[T], chunk_length: int) -> Sequence[Sequence[T]]:
-        """ Add the 'chunk'/second batch dimension to the list of items. """
-        groups = list(n_consecutive(values, chunk_length))
-        if isinstance(values, np.ndarray):
-            groups = np.array(groups)
-        return groups
+            self.env_b.close_extras(**kwargs)
 
 
 def distribute(values: Sequence[T], n_groups: int) -> List[Sequence[T]]:
@@ -214,9 +211,30 @@ def distribute(values: Sequence[T], n_groups: int) -> List[Sequence[T]]:
     return groups
 
 
+def unchunk(*values: Sequence[Sequence[T]]) -> Sequence[T]:
+    """ Combine 'chunked' results coming from the envs into a single
+    batch.
+    """
+    all_values: List[T] = []
+    for sequence in values:
+        all_values.extend(itertools.chain.from_iterable(sequence))
+    if isinstance(values[0], np.ndarray):
+        return np.array(all_values)
+    return all_values
+
+
+def chunk(values: Sequence[T], chunk_length: int) -> Sequence[Sequence[T]]:
+    """ Add the 'chunk'/second batch dimension to the list of items. """
+    groups = list(n_consecutive(values, chunk_length))
+    if isinstance(values, np.ndarray):
+        groups = np.array(groups)
+    return groups
+
+
 def n_consecutive(items: Iterable[T], n: int=2, yield_last_batch=True) -> Iterable[Tuple[T, ...]]:
     """Collect data into chunks of up to `n` elements.
-    Adapted from the itertools recipes.
+    
+    (Adapted from the itertools recipes.)
 
     When `yield_last_batch` is True, the final chunk (which might have fewer
     than `n` items) will also be yielded.
@@ -232,63 +250,6 @@ def n_consecutive(items: Iterable[T], n: int=2, yield_last_batch=True) -> Iterab
             values.clear()
     if values and yield_last_batch:
         yield tuple(values)
-
-
-def concat_spaces(space_a: gym.Space, space_b: gym.Space) -> gym.Space:
-    """ Concatenate two gym spaces of the same types. """
-    if type(space_a) != type(space_b):
-        raise RuntimeError(f"Can only concat spaces of the same type: {space_a} {space_b}")
-    
-    if isinstance(space_a, spaces.Box):
-        assert isinstance(space_b, spaces.Box)
-
-        assert space_a.shape[1:] == space_b.shape[1:]
-        new_low = np.concatenate([space_a.low, space_b.low])
-        new_high = np.concatenate([space_a.high, space_b.high])
-        return spaces.Box(low=new_low, high=new_high, dtype=space_a.dtype)
-
-    if isinstance(space_a, spaces.Tuple):
-        assert isinstance(space_b, spaces.Tuple)
-
-        new_spaces = space_a.spaces + space_b.spaces
-        return spaces.Tuple(new_spaces)
-    
-    if isinstance(space_a, spaces.Dict):
-        assert isinstance(space_b, spaces.Dict)
-        new_spaces = {}
-        for key in space_a.spaces:
-            value_a = space_a[key]
-            value_b = space_b[key]
-            new_spaces[key] = concat_spaces(value_a, value_b)
-        return spaces.Dict(new_spaces)
-    
-    raise NotImplementedError(f"Unsupported spaces {space_a} {space_b}")
-
-
-def remove_extra_batch_dim(space: gym.Space):
-    if isinstance(space, spaces.Box):
-        dims = space.shape
-        assert len(space.shape) >= 2, space
-        new_shape = tuple([dims[0] * dims[1], *dims[2:]])
-        new_low = space.low.reshape(new_shape)
-        new_high = space.high.reshape(new_shape)
-        return spaces.Box(low=new_low, high=new_high, dtype=space.dtype)
-
-    if isinstance(space, spaces.Tuple):
-        assert len(space.spaces) and isinstance(space.spaces[0], spaces.Tuple)
-        all_spaces: List[gym.Space] = []
-        for sub_space in space.spaces:
-            all_spaces.extend(sub_space.spaces)
-        return spaces.Tuple(all_spaces)
-
-    if isinstance(space, spaces.Dict):
-        return spaces.Dict({
-            k: remove_extra_batch_dim(sub_space)
-            for k, sub_space in space.spaces.items()
-        })
-
-    raise NotImplementedError(f"Unsupported space {space}")
-
 
 
 if __name__ == "__main__":
