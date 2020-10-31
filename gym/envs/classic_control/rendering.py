d7e5372dc4a2b18305b575e55519dc29b80e34a4
Iheb Haboubi <iheb.haboubi56@gmail.com>
2020-10-31 21:40:52 +0100

    Fix typo in docstring

--- gym/envs/classic_control/rendering.py
+++ gym/envs/classic_control/rendering.py
@@ -26,7 +26,7 @@ try:
 except ImportError as e:
     raise ImportError('''
     Error occurred while running `from pyglet.gl import *`
-    HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'.
+    HINT: make sure you have OpenGL installed. On Ubuntu, you can run 'apt-get install python-opengl'.
     If you're running on a server, you may need a virtual frame buffer; something like this should work:
     'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'
     ''')
