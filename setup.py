58340d6e848b42f6612b87c7f0836e616bbba94f
Justin Terry <justinkterry@gmail.com>
2020-11-04 00:44:02 -0500

    remove python 3.5, update travis

--- setup.py
+++ setup.py
@@ -42,10 +42,9 @@ setup(name='gym',
         'envs/robotics/assets/textures/*.png']
       },
       tests_require=['pytest', 'mock'],
-      python_requires='>=3.5',
+      python_requires='>=3.6',
       classifiers=[
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
