2c2a2faa68bb5dfe5b433e605787192d106eef1d
Justin Terry <justinkterry@gmail.com>
2020-11-05 23:32:35 -0500

    try removing pillow minimum version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from version import VERSION
 
 # Environment-specific dependencies.
 extras = {
-  'atari': ['atari_py~=0.2.0', 'Pillow>=6.2.1', 'opencv-python>=3.'],
+  'atari': ['atari_py~=0.2.0', 'Pillow', 'opencv-python>=3.'],
   'box2d': ['box2d-py~=2.3.5'],
   'classic_control': [],
   'mujoco': ['mujoco_py>=1.50, <2.0', 'imageio'],
