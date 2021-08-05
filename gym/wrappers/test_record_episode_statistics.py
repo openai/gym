72ac78d13d7cac6df54e354841229a6fb8f6499b
Costa Huang <costa.huang@outlook.com>
2021-08-05 16:29:56 -0400

    fix test cases again

--- gym/wrappers/test_record_episode_statistics.py
+++ gym/wrappers/test_record_episode_statistics.py
@@ -25,7 +25,7 @@ def test_record_episode_statistics(env_id, deque_size):
 
 
 @pytest.mark.parametrize("num_envs", [1, 4])
-def test_record_episode_statistics_with_vectorenv(env_id, num_envs):
+def test_record_episode_statistics_with_vectorenv(num_envs):
     envs = gym.vector.make("CartPole-v0", num_envs=num_envs, asynchronous=False)
     envs = RecordEpisodeStatistics(envs)
     envs.reset()
