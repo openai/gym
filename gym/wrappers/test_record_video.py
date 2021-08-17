f9c5ed043033f7508a3aff4a7b1dca60676ddb25
Costa Huang <costa.huang@outlook.com>
2021-08-17 18:09:11 -0400

    reformat

--- gym/wrappers/test_record_video.py
+++ gym/wrappers/test_record_video.py
@@ -13,7 +13,7 @@ def test_record_video():
     )
     env.reset()
     for _ in range(200):
-        action = (env.action_space.sample())
+        action = env.action_space.sample()
         _, _, done, _ = env.step(action)
         if done:
             env.close()
@@ -21,15 +21,20 @@ def test_record_video():
     assert os.path.isdir("videos")
     shutil.rmtree("videos")
 
+
 def make_env(gym_id, seed):
     def thunk():
         env = gym.make(gym_id)
         env.seed(seed)
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
-        env = gym.wrappers.RecordVideo(env, "videos", record_video_trigger=lambda x: x % 100 == 0)
+        env = gym.wrappers.RecordVideo(
+            env, "videos", record_video_trigger=lambda x: x % 100 == 0
+        )
         return env
+
     return thunk
+
 
 def test_record_video_vector():
     envs = gym.vector.SyncVectorEnv([make_env("CartPole-v1", 1 + i) for i in range(2)])
