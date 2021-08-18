477106d2922520f4fe7e7d3fff1628d68b8725fe
Justin Terry <justinkterry@gmail.com>
2021-08-18 17:16:47 -0400

    black

--- gym/wrappers/rescale_action.py
+++ gym/wrappers/rescale_action.py
@@ -20,17 +20,29 @@ class RescaleAction(gym.ActionWrapper):
         assert np.less_equal(min_action, max_action).all(), (min_action, max_action)
 
         super(RescaleAction, self).__init__(env)
-        self.min_action = np.zeros(env.action_space.shape, dtype=env.action_space.dtype) + min_action
-        self.max_action = np.zeros(env.action_space.shape, dtype=env.action_space.dtype) + max_action
+        self.min_action = (
+            np.zeros(env.action_space.shape, dtype=env.action_space.dtype) + min_action
+        )
+        self.max_action = (
+            np.zeros(env.action_space.shape, dtype=env.action_space.dtype) + max_action
+        )
         self.action_space = spaces.Box(
-            low=min_action, high=max_action, shape=env.action_space.shape, dtype=env.action_space.dtype
+            low=min_action,
+            high=max_action,
+            shape=env.action_space.shape,
+            dtype=env.action_space.dtype,
         )
 
     def action(self, action):
-        assert np.all(np.greater_equal(action, self.min_action)), (action, self.min_action)
+        assert np.all(np.greater_equal(action, self.min_action)), (
+            action,
+            self.min_action,
+        )
         assert np.all(np.less_equal(action, self.max_action)), (action, self.max_action)
         low = self.env.action_space.low
         high = self.env.action_space.high
-        action = low + (high - low) * ((action - self.min_action) / (self.max_action - self.min_action))
+        action = low + (high - low) * (
+            (action - self.min_action) / (self.max_action - self.min_action)
+        )
         action = np.clip(action, low, high)
         return action
