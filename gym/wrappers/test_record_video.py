06debd07da7e21b9ad8a0cb1d253daa4a7dbc28e
Costa Huang <costa.huang@outlook.com>
2021-08-24 17:34:43 -0400

    Fix a RecordVideo bug (#2354)
    * fix a RecordVideo bug
    
    * add test cases

--- gym/wrappers/test_record_video.py
+++ gym/wrappers/test_record_video.py
@@ -12,13 +12,15 @@ def test_record_video():
         env, "videos", record_video_trigger=lambda x: x % 100 == 0
     )
     env.reset()
-    for _ in range(200):
+    for _ in range(199):
         action = env.action_space.sample()
         _, _, done, _ = env.step(action)
         if done:
-            env.close()
-            break
+            env.reset()
+    env.close()
     assert os.path.isdir("videos")
+    mp4_files = [file for file in os.listdir("videos") if file.endswith(".mp4")]
+    assert len(mp4_files) == 2
     shutil.rmtree("videos")
 
 
@@ -28,23 +30,26 @@ def make_env(gym_id, seed):
         env.seed(seed)
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
-        env = gym.wrappers.RecordVideo(
-            env, "videos", record_video_trigger=lambda x: x % 100 == 0
-        )
+        if seed == 1:
+            env = gym.wrappers.RecordVideo(
+                env, "videos", record_video_trigger=lambda x: x % 100 == 0
+            )
         return env
 
     return thunk
 
 
-def test_record_video_vector():
+def test_record_video_within_vector():
     envs = gym.vector.SyncVectorEnv([make_env("CartPole-v1", 1 + i) for i in range(2)])
     envs = gym.wrappers.RecordEpisodeStatistics(envs)
     envs.reset()
-    for i in range(100):
+    for i in range(199):
         _, _, _, infos = envs.step(envs.action_space.sample())
         for info in infos:
             if "episode" in info.keys():
                 print(f"i, episode_reward={info['episode']['r']}")
                 break
     assert os.path.isdir("videos")
+    mp4_files = [file for file in os.listdir("videos") if file.endswith(".mp4")]
+    assert len(mp4_files) == 2
     shutil.rmtree("videos")
