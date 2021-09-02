33ec5132de8b51515441582964c8af5ea75af781
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2021-09-02 15:18:56 +0900

    Update frozen_lake.py

--- gym/envs/toy_text/frozen_lake.py
+++ gym/envs/toy_text/frozen_lake.py
@@ -147,7 +147,7 @@ class FrozenLakeEnv(discrete.DiscreteEnv):
                         else:
                             li.append((1.0, *update_probability_matrix(row, col, a)))
 
-        super(FrozenLakeEnv, self).__init__(nS, nA, P, isd)
+        super().__init__(nS, nA, P, isd)
 
     def render(self, mode="human"):
         outfile = StringIO() if mode == "ansi" else sys.stdout
