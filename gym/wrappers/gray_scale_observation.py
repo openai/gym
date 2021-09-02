1f9ea7eeb9e22de0473037e5d42db54352c02a30
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2021-09-02 15:12:50 +0900

    Update gray_scale_observation.py

--- gym/wrappers/gray_scale_observation.py
+++ gym/wrappers/gray_scale_observation.py
@@ -7,7 +7,7 @@ class GrayScaleObservation(ObservationWrapper):
     r"""Convert the image observation from RGB to gray scale."""
 
     def __init__(self, env, keep_dim=False):
-        super(GrayScaleObservation, self).__init__(env)
+        super().__init__(env)
         self.keep_dim = keep_dim
 
         assert (
