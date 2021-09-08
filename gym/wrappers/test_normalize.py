814129f5b6c3c45e67412d9c5c999660c3b0ffda
Costa Huang <costa.huang@outlook.com>
2021-09-08 10:28:31 -0400

    quick fix

--- gym/wrappers/test_normalize.py
+++ gym/wrappers/test_normalize.py
@@ -2,7 +2,7 @@ import gym
 import numpy as np
 from numpy.testing import assert_almost_equal
 
-from gym.wrappers.normalize import Normalize, NormalizeObservation, NormalizeReturn
+from gym.wrappers.normalize import NormalizeObservation, NormalizeReturn
 
 
 class DummyRewardEnv(gym.Env):
