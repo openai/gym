d5eee8e78dab7ead43f525e02aeb7ad1e30de40d
redtachyon <ariel.j.kwiatkowski@gmail.com>
2021-09-27 23:02:46 +0200

    Fix unittest again

---
+++ gym/envs/unittest/__init__.py
@@ -0,0 +1,4 @@
+from gym.envs.unittest.cube_crash import CubeCrash
+from gym.envs.unittest.cube_crash import CubeCrashSparse
+from gym.envs.unittest.cube_crash import CubeCrashScreenBecomesBlack
+from gym.envs.unittest.memorize_digits import MemorizeDigits
