45a4d050d0d52c6e3b5c37b7681e1273d2b44e0c
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2021-10-24 02:52:40 +0200

    Stop Discrete envs from doing part of the setup (and using the randomness) in init (as opposed to reset)

--- gym/envs/toy_text/discrete.py
+++ gym/envs/toy_text/discrete.py
@@ -42,8 +42,6 @@ class DiscreteEnv(Env):
         self.action_space = spaces.Discrete(self.nA)
         self.observation_space = spaces.Discrete(self.nS)
 
-        self.s = categorical_sample(self.isd, self.np_random)
-
     def reset(self, seed: Optional[int] = None):
         super().reset(seed=seed)
         self.s = categorical_sample(self.isd, self.np_random)
