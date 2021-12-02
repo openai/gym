67234c23cbf10d60611b467430034ab302cd2589
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2021-12-02 19:09:40 +0900

    Update permutation.py

--- gym/spaces/permutation.py
+++ gym/spaces/permutation.py
@@ -4,22 +4,24 @@ from gym.spaces.space import Space
 
 class Permutation(Space):
     def __init__(self, n, k, dtype=np.int64, seed=None):
-        assert n >= k, '`n` has to be no less than `k`'
+        assert n >= k, "`n` has to be no less than `k`"
         self.n = n
         self.k = k
         self.nvec = np.array([n]*k, dtype=dtype)
         super().__init__(shape=self.nvec.shape, dtype=dtype, seed=seed)
 
     def sample(self):
-        return self.np_random.permutation(self.nvec[0])[:len(self.nvec)].astype(self.dtype)
+        return self.np_random.permutation(self.nvec[0])[: len(self.nvec)].astype(
+            self.dtype
+        )
 
     def contains(self, x):
         if isinstance(x, list):
             x = np.array(x)  # Promote list to array for contains check
         # if nvec is uint32 and space dtype is uint32, then 0 <= x < self.nvec guarantees that x
         # is within correct bounds for space dtype (even though x does not have to be unsigned)
-        is_contained = (x.shape == self.shape) and (0 <= x).all() and (x < self.nvec).all()
-        is_unique = (np.unique(x).size == len(x))
+        is_contained = x.shape == self.shape and (0 <= x).all() and (x < self.nvec).all()
+        is_unique = np.unique(x).size == len(x)
         return is_unique and is_contained
 
     def to_jsonable(self, sample_n):
@@ -29,7 +31,7 @@ class Permutation(Space):
         return np.array(sample_n)
 
     def __repr__(self):
-        return f'Permutation({self.n}, {self.k})'
+        return f"Permutation({self.n}, {self.k})"
 
     def __len__(self):
         return len(self.nvec)
