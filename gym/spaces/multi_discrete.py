18c8b988d463a95b7e51ca50b5de89415aa97488
Xuehai Pan <XuehaiPan@pku.edu.cn>
2021-12-16 13:45:37 +0800

    Type cast in `spaces` families (#2491)
    * Type cast for `spaces.Dict`
    
    * Type cast for `spaces.Tuple`
    
    * Type cast for `spaces.Discrete`
    
    * Type cast for `spaces.MultiDiscrete`
    
    * Type cast for `spaces.MultiBinary`

--- gym/spaces/multi_discrete.py
+++ gym/spaces/multi_discrete.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 import numpy as np
 from gym import logger
 from .space import Space
@@ -29,8 +30,8 @@ class MultiDiscrete(Space):
         """
         nvec: vector of counts of each categorical variable
         """
-        assert (np.array(nvec) > 0).all(), "nvec (counts) have to be positive"
-        self.nvec = np.asarray(nvec, dtype=dtype)
+        self.nvec = np.array(nvec, dtype=dtype, copy=True)
+        assert (self.nvec > 0).all(), "nvec (counts) have to be positive"
 
         super().__init__(self.nvec.shape, dtype, seed)
 
@@ -38,7 +39,7 @@ class MultiDiscrete(Space):
         return (self.np_random.random(self.nvec.shape) * self.nvec).astype(self.dtype)
 
     def contains(self, x):
-        if isinstance(x, list):
+        if isinstance(x, Sequence):
             x = np.array(x)  # Promote list to array for contains check
         # if nvec is uint32 and space dtype is uint32, then 0 <= x < self.nvec guarantees that x
         # is within correct bounds for space dtype (even though x does not have to be unsigned)
