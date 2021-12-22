102cd1bf4c7d6f9695e8c40c216df13238a165a1
Carlos Luis <carlos.luisgonc@gmail.com>
2021-12-22 19:25:36 +0100

    Remove DiscreteEnv class (#2514)

---
+++ gym/envs/toy_text/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def categorical_sample(prob_n, np_random):
+    """
+    Sample from categorical distribution
+    Each row specifies class probabilities
+    """
+    prob_n = np.asarray(prob_n)
+    csprob_n = np.cumsum(prob_n)
+    return (csprob_n > np_random.random()).argmax()
