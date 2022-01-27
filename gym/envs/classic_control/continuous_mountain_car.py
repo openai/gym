082fa27f36194903bc76bbe7e8434b98cfb1f548
trigaten <sanderman5@hotmail.com>
2022-01-27 11:45:29 -0500

    black

--- gym/envs/classic_control/continuous_mountain_car.py
+++ gym/envs/classic_control/continuous_mountain_car.py
@@ -38,25 +38,25 @@ class Continuous_MountainCarEnv(gym.Env):
     }
     ```
 
-    ## Observation Space 
-     
+    ## Observation Space
+
     The observation space is a 2-dim vector, where the 1st element represents the "car position" and the 2nd element represents the "car velocity".
 
     ## Action
-    
+
     The actual driving force is calculated by multiplying the power coef by power (0.0015)
 
     ## Reward
-    
+
     Reward of 100 is awarded if the agent reached the flag (position = 0.45)
     on top of the mountain. Reward is decrease based on amount of energy consumed each step.
 
     ## Starting State
-    
+
     The position of the car is assigned a uniform random value in [-0.6 , -0.4]. The starting velocity of the car is always assigned to 0.
 
     ## Episode Termination
-    
+
     The car position is more than 0.45. Episode length is greater than 200
 
     ## Arguments
