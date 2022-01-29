d4e156948b637acf294315462bde32bbd5f0b9c8
J K Terry <jkterry0@gmail.com>
2022-01-29 11:24:39 -0500

    fix version

--- gym/__init__.py
+++ gym/__init__.py
@@ -1,5 +1,6 @@
 from gym import error
 import urllib.request
+from gym.version import VERSION as __version__
 
 from gym.core import (
     Env,
@@ -16,15 +17,13 @@ from gym import wrappers
 
 __all__ = ["Env", "Space", "Wrapper", "make", "spec", "register"]
 
-VERSION = "0.21.0"
-
 
 try:
     with urllib.request.urlopen("https://raw.githubusercontent.com/Farama-Foundation/gym-notices/main/notices.txt") as f:
         html = f.read().decode("utf-8")
         lines = html.split("\n")
         for line in lines:
-            if line.startswith("0.22.0"):
+            if line.startswith(__version__):
                 print(line.split(' : ')[1])
 except Exception: 
     pass
