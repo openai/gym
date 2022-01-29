2daaec01c2375e882605491125447aa788afeb6e
J K Terry <jkterry0@gmail.com>
2022-01-29 11:18:13 -0500

    legacy warnings in case we screw up badly

--- gym/__init__.py
+++ gym/__init__.py
@@ -1,5 +1,5 @@
 from gym import error
-from gym.version import VERSION as __version__
+import urllib.request
 
 from gym.core import (
     Env,
@@ -15,3 +15,16 @@ from gym import vector
 from gym import wrappers
 
 __all__ = ["Env", "Space", "Wrapper", "make", "spec", "register"]
+
+VERSION = "0.21.0"
+
+
+try:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/Farama-Foundation/gym-notices/main/notices.txt") as f:
+        html = f.read().decode("utf-8")
+        lines = html.split("\n")
+        for line in lines:
+            if line.startswith("0.22.0"):
+                print(line.split(' : ')[1])
+except Exception: 
+    pass
