4dac96cf68b5ba339f69e28d42f3fdacc28e7707
Costa Huang <costa.huang@outlook.com>
2022-02-14 11:21:09 -0500

    Add wrapper for making one hot observations

---
+++ gym/wrappers/onehot_observation.py
@@ -0,0 +1,26 @@
+import gym
+import numpy as np
+
+
+def one_hot(a, size):
+    b = np.zeros((size))
+    b[a] = 1
+    return b
+
+class ProcessObsInputEnv(gym.ObservationWrapper):
+    """
+    This wrapper handles inputs from `Discrete` and `Box` observation space.
+    If the `env.observation_space` is of `Discrete` type, 
+    it returns the one-hot encoding of the state
+    """
+    def __init__(self, env):
+        super().__init__(env)
+        self.n = None
+        if isinstance(self.env.observation_space, gym.spaces.Discrete):
+            self.n = self.env.observation_space.n
+            self.observation_space = gym.spaces.Box(0, 1, (self.n,))
+
+    def observation(self, obs):
+        if self.n:
+            return one_hot(np.array(obs), self.n)
+        return obs
---
+++ tests/wrappers/test_onehot_observation.py
@@ -0,0 +1,9 @@
+import gym
+
+
+def test_onehot_observation():
+    env = gym.make('Taxi-v3')
+    env = gym.wrappers.OnehotObservation(env)
+    env.reset()
+    assert isinstance(env.observation_space, gym.spaces.Box)
+    assert env.observation_space.shape == (500,)
