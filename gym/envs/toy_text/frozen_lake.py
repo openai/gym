aeca19508a6adc3b1332360c3a18d1a60d069170
trigaten <sanderman5@hotmail.com>
2022-02-17 15:06:44 -0500

    black

--- gym/envs/toy_text/frozen_lake.py
+++ gym/envs/toy_text/frozen_lake.py
@@ -67,71 +67,71 @@ def generate_random_map(size=8, p=0.8):
 
 class FrozenLakeEnv(Env):
     """
-    Frozen lake involves crossing a frozen lake from Start(S) to goal(G) without falling into any holes(H) by walking over
-    the Frozen(F) lake. The agent may not always move in the intended direction due to the slippery nature of the frozen lake.
+     Frozen lake involves crossing a frozen lake from Start(S) to goal(G) without falling into any holes(H) by walking over
+     the Frozen(F) lake. The agent may not always move in the intended direction due to the slippery nature of the frozen lake.
 
-    The agent takes a 1-element vector for actions.
-    The action space is `(dir)`, where `dir` decides direction to move in which can be:
+     The agent takes a 1-element vector for actions.
+     The action space is `(dir)`, where `dir` decides direction to move in which can be:
 
-    - 0: LEFT
-    - 1: DOWN
-    - 2: RIGHT
-    - 3: UP
+     - 0: LEFT
+     - 1: DOWN
+     - 2: RIGHT
+     - 3: UP
 
-    The observation is a value representing the agents current position as
-        current_row * nrows + current_col (where both the row and col start at 0).
-    For example, the goal position in the 4x4 map can be calculated as follows: 3 * 4 + 3 = 15
-    The amount of possible observation spaces is dependent on the size of the map.
-    The 4x4 map thus has 16 possible observations.
+     The observation is a value representing the agents current position as
+         current_row * nrows + current_col (where both the row and col start at 0).
+     For example, the goal position in the 4x4 map can be calculated as follows: 3 * 4 + 3 = 15
+     The amount of possible observation spaces is dependent on the size of the map.
+     The 4x4 map thus has 16 possible observations.
 
-   ### Rewards
+    ### Rewards
 
-    Reward schedule:
-    - Reach goal(G): +1
-    - Reach hole(H): 0
-    - Reach frozen(F): 0
+     Reward schedule:
+     - Reach goal(G): +1
+     - Reach hole(H): 0
+     - Reach frozen(F): 0
 
-    ### Arguments
+     ### Arguments
 
-    ```
-    gym.make('FrozenLake-v1', desc=None,map_name="4x4", is_slippery=True)
-    ```
+     ```
+     gym.make('FrozenLake-v1', desc=None,map_name="4x4", is_slippery=True)
+     ```
 
-    `desc`: Used to specify custom map for frozen lake. For example,
+     `desc`: Used to specify custom map for frozen lake. For example,
 
-        desc=["SFFF", "FHFH", "FFFH", "HFFG"].
+         desc=["SFFF", "FHFH", "FFFH", "HFFG"].
 
-    `map_name`: ID to use any of the preloaded maps.
+     `map_name`: ID to use any of the preloaded maps.
 
-        "4x4":[
-            "SFFF",
-            "FHFH",
-            "FFFH",
-            "HFFG"
-            ]
+         "4x4":[
+             "SFFF",
+             "FHFH",
+             "FFFH",
+             "HFFG"
+             ]
 
-        "8x8": [
-            "SFFFFFFF",
-            "FFFFFFFF",
-            "FFFHFFFF",
-            "FFFFFHFF",
-            "FFFHFFFF",
-            "FHHFFFHF",
-            "FHFFHFHF",
-            "FFFHFFFG",
-        ]
+         "8x8": [
+             "SFFFFFFF",
+             "FFFFFFFF",
+             "FFFHFFFF",
+             "FFFFFHFF",
+             "FFFHFFFF",
+             "FHHFFFHF",
+             "FHFFHFHF",
+             "FFFHFFFG",
+         ]
 
-    `is_slippery`: True/False. If True will move in intended direction with
-    probability of 1/3 else will move in either perpendicular direction with
-    equal probability of 1/3 in both directions.
+     `is_slippery`: True/False. If True will move in intended direction with
+     probability of 1/3 else will move in either perpendicular direction with
+     equal probability of 1/3 in both directions.
 
-        For example, if action is left and is_slippery is True, then:
-        - P(move left)=1/3
-        - P(move up)=1/3
-        - P(move down)=1/3
-    ### Version History
-    * v1: Bug fixes to rewards
-    * v0: Initial versions release (1.0.0)
+         For example, if action is left and is_slippery is True, then:
+         - P(move left)=1/3
+         - P(move up)=1/3
+         - P(move down)=1/3
+     ### Version History
+     * v1: Bug fixes to rewards
+     * v0: Initial versions release (1.0.0)
     """
 
     metadata = {"render.modes": ["human", "ansi", "rgb_array"]}
