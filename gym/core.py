29a093ad5a69beb4cedcab0c6c4e74b3689b1f83
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-02-18 01:38:22 +0100

    Setter-based approach for the fix (#2626)

--- gym/core.py
+++ gym/core.py
@@ -56,6 +56,10 @@ class Env(Generic[ObsType, ActType]):
             self._np_random, seed = seeding.np_random()
         return self._np_random
 
+    @np_random.setter
+    def np_random(self, value: RandomNumberGenerator):
+        self._np_random = value
+
     @abstractmethod
     def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
         """Run one timestep of the environment's dynamics. When end of
