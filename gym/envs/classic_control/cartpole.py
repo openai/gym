0d8c070f1d551bdc6f78cfaa458c868f2341efa6
RushivArora <rushivarora@gmail.com>
2022-02-28 04:49:22 -0500

    Fixed tests and precommit

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -18,56 +18,58 @@ from gym.utils import seeding
 class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
     """
     ### Description
+
     This environment corresponds to the version of the cart-pole problem
     described by Barto, Sutton, and Anderson in ["Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem"](https://ieeexplore.ieee.org/document/6313077).
     A pole is attached by an un-actuated joint to a cart, which moves along a
-    frictionless track. The pendulum starts upright, and the goal is to prevent
-    it from falling over by increasing and reducing the cart's velocity.
+    frictionless track. The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces in the left and right direction on the cart.
 
     ### Action Space
-    The agent take a 1-element vector for actions.
-    The action space is `(action)` in `[0, 1]`, where `action` is used to push
-    the cart with a fixed amount of force:
+
+    The action is a `ndarray` with shape `(1,)` which can take values `{0, 1}` indicating the direction of the fixed force the cart is pushed with.
 
     | Num | Action                 |
     |-----|------------------------|
     | 0   | Push cart to the left  |
     | 1   | Push cart to the right |
 
-    Note: The amount the velocity is reduced or increased is not fixed as it depends on the angle the pole is pointing.
-    This is because the center of gravity of the pole increases the amount of energy needed to move the cart underneath it
+    **Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it
 
     ### Observation Space
-    The observation is a `ndarray` with shape `(4,)` where the elements correspond to the following:
+
+    The observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
 
     | Num | Observation           | Min                  | Max                |
     |-----|-----------------------|----------------------|--------------------|
-    | 0   | Cart Position         | -4.8*                 | 4.8*                |
+    | 0   | Cart Position         | -4.8                 | 4.8                |
     | 1   | Cart Velocity         | -Inf                 | Inf                |
-    | 2   | Pole Angle            | ~ -0.418 rad (-24°)** | ~ 0.418 rad (24°)** |
+    | 2   | Pole Angle            | ~ -0.418 rad (-24°)  | ~ 0.418 rad (24°)  |
     | 3   | Pole Angular Velocity | -Inf                 | Inf                |
 
-    **Note:** above denotes the ranges of possible observations for each element, but in two cases this range exceeds the
-    range of possible values in an un-terminated episode:
-    - `*`: the cart x-position can be observed between `(-4.8, 4.8)`, but an episode terminates if the cart leaves the
-    `(-2.4, 2.4)` range.
-    - `**`: Similarly, the pole angle can be observed between  `(-.418, .418)` radians or precisely **±24°**, but an episode is
-    terminated if the pole angle is outside the `(-.2095, .2095)` range or precisely **±12°**
+    **Note:** While the ranges above denote the possible values for observation space of each element, it is not reflective of the allowed values of the state space in an unterminated episode. Particularly:
+    -  The cart x-position (index 0) can be take values between `(-4.8, 4.8)`, but the episode terminates if the cart leaves the `(-2.4, 2.4)` range.
+    -  The pole angle can be observed between  `(-.418, .418)` radians (or **±24°**), but the episode terminates if the pole angle is not in the range `(-.2095, .2095)` (or **±12°**)
 
     ### Rewards
-    Reward is 1 for every step taken, including the termination step. The threshold is 475 for v1.
+
+    Since the goal is to keep the pole upright for as long as possible, a reward of `+1` for every step taken, including the termination step, is allotted. The threshold for rewards is 475 for v1.
 
     ### Starting State
-    All observations are assigned a uniform random value between (-0.05, 0.05)
+
+    All observations are assigned a uniformly random value in `(-0.05, 0.05)`
 
     ### Episode Termination
-    The episode terminates of one of the following occurs:
 
-    1. Pole Angle is more than ±12°
-    2. Cart Position is more than ±2.4 (center of the cart reaches the edge of the display)
+    The episode terminates if any one of the following occurs:
+    1. Pole Angle is greater than ±12°
+    2. Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
     3. Episode length is greater than 500 (200 for v0)
 
     ### Arguments
+
+    ```
+    gym.make('CartPole-v1')
+    ```
 
     No additional arguments are currently supported.
     """
