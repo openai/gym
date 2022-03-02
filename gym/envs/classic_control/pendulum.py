fd6f9f8d1e085266dd0025aef4ef2d0b68a8eec2
mark <mark.m.towers@gmail.com>
2022-03-02 19:32:21 +0000

    Added comment to explain why a warning is raised that can't be prevented without version update to the environment

--- gym/envs/classic_control/pendulum.py
+++ gym/envs/classic_control/pendulum.py
@@ -95,6 +95,9 @@ class PendulumEnv(gym.Env):
         self.screen_dim = 500
 
         high = np.array([1.0, 1.0, self.max_speed], dtype=np.float32)
+        # This will throw a warning in tests/envs/test_envs in utils/env_checker.py as the space is not symmetric
+        #   or normalised as max_torque == 2 by default. Ignoring the issue here as the default settings are too old
+        #   to update to follow the openai gym api
         self.action_space = spaces.Box(
             low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32
         )
@@ -187,7 +190,7 @@ class PendulumEnv(gym.Env):
             scale_img = pygame.transform.smoothscale(
                 img, (scale * np.abs(self.last_u) / 2, scale * np.abs(self.last_u) / 2)
             )
-            is_flip = self.last_u > 0
+            is_flip = bool(self.last_u > 0)
             scale_img = pygame.transform.flip(scale_img, is_flip, True)
             self.surf.blit(
                 scale_img,
