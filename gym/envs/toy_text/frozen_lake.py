15d9321b88a91ffce1ec85d2d5805544a06095cb
Omar <omar.younis98@gmail.com>
2022-03-05 18:57:33 +0100

    add clock on pygame render

--- gym/envs/toy_text/frozen_lake.py
+++ gym/envs/toy_text/frozen_lake.py
@@ -67,45 +67,39 @@ def generate_random_map(size=8, p=0.8):
 
 class FrozenLakeEnv(Env):
     """
-    Frozen lake involves crossing a frozen lake from Start(S) to goal(G) without falling into any holes(H). The agent may not always move in the intended direction due to the slippery nature of the frozen lake.
-
-    The agent take a 1-element vector for actions.
+    Frozen lake involves crossing a frozen lake from Start(S) to Goal(G) without falling into any Holes(H) by walking over
+    the Frozen(F) lake. The agent may not always move in the intended direction due to the slippery nature of the frozen lake.
+    ### Action Space
+    The agent takes a 1-element vector for actions.
     The action space is `(dir)`, where `dir` decides direction to move in which can be:
-
     - 0: LEFT
     - 1: DOWN
     - 2: RIGHT
     - 3: UP
-
-    The observation is a value representing the agents current position as
-
-        current_row * nrows + current_col
-
-    **Rewards:**
-
+    ### Observation Space
+    The observation is a value representing the agent's current position as
+    current_row * nrows + current_col (where both the row and col start at 0).
+    For example, the goal position in the 4x4 map can be calculated as follows: 3 * 4 + 3 = 15.
+    The number of possible observations is dependent on the size of the map.
+    For example, the 4x4 map has 16 possible observations.
+    ### Rewards
     Reward schedule:
     - Reach goal(G): +1
     - Reach hole(H): 0
-
+    - Reach frozen(F): 0
     ### Arguments
-
     ```
-    gym.make('FrozenLake-v0', desc=None,map_name="4x4", is_slippery=True)
+    gym.make('FrozenLake-v1', desc=None,map_name="4x4", is_slippery=True)
     ```
-
     `desc`: Used to specify custom map for frozen lake. For example,
-
         desc=["SFFF", "FHFH", "FFFH", "HFFG"].
-
     `map_name`: ID to use any of the preloaded maps.
-
         "4x4":[
             "SFFF",
             "FHFH",
             "FFFH",
             "HFFG"
             ]
-
         "8x8": [
             "SFFFFFFF",
             "FFFFFFFF",
@@ -116,20 +110,15 @@ class FrozenLakeEnv(Env):
             "FHFFHFHF",
             "FFFHFFFG",
         ]
-
-
-
-
     `is_slippery`: True/False. If True will move in intended direction with
     probability of 1/3 else will move in either perpendicular direction with
     equal probability of 1/3 in both directions.
-
         For example, if action is left and is_slippery is True, then:
         - P(move left)=1/3
         - P(move up)=1/3
         - P(move down)=1/3
     ### Version History
-
+    * v1: Bug fixes to rewards
     * v0: Initial versions release (1.0.0)
     """
 
@@ -286,7 +275,7 @@ class FrozenLakeAnsiRender(FrozenLakeEnv):
 
 
 class FrozenLakeRenderGraphics(FrozenLakeEnv):
-    metadata = {"render_modes": ["human", "rgb_array"]}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
     def __init__(self, render_mode="human", **kwargs):
         super().__init__(**kwargs)
@@ -321,6 +310,7 @@ class FrozenLakeRenderGraphics(FrozenLakeEnv):
             path.join(path.dirname(__file__), "img/elf_up.png"),
         ]
         self.elf_images = [pygame.image.load(f_name) for f_name in elfs]
+        self.clock = pygame.time.Clock()
 
     def _render(self):
         board = pygame.Surface(self.window_size, flags=SRCALPHA)
@@ -412,6 +402,7 @@ class FrozenLakeRenderGraphics(FrozenLakeEnv):
         self._render()
         if self.render_mode == "human":
             pygame.display.update()
+            self.clock.tick(self.metadata["render_fps"])
         else:
             self.render_list.append(
                 np.transpose(
