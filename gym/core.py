214a835e7654f1395510354f357f3a056c761adf
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2022-03-31 22:28:17 +0200

    Wrapper should not overwrite default render mode (#2706)

--- gym/core.py
+++ gym/core.py
@@ -282,8 +282,8 @@ class Wrapper(Env[ObsType, ActType]):
     def reset(self, **kwargs) -> Union[ObsType, tuple[ObsType, dict]]:
         return self.env.reset(**kwargs)
 
-    def render(self, mode="human", **kwargs):
-        return self.env.render(mode, **kwargs)
+    def render(self, **kwargs):
+        return self.env.render(**kwargs)
 
     def close(self):
         return self.env.close()
