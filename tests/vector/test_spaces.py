2b94d9740723e76a23efd65791603edef88b89c5
StringTheory <mark.m.towers@gmail.com>
2022-04-06 17:07:01 +0100

    Parameterized the batch_space_seed, added testing for rng_different_at_each_index and test_deterministic

--- tests/vector/test_spaces.py
+++ tests/vector/test_spaces.py
@@ -1,6 +1,10 @@
+import copy
+
 import numpy as np
 import pytest
+from numpy.testing._private.utils import assert_array_equal
 
+from gym import Space
 from gym.spaces import Box, Dict, Discrete, MultiDiscrete, Tuple
 from gym.vector.utils.spaces import batch_space, iterate
 from tests.vector.utils import CustomSpace, custom_spaces, spaces
@@ -95,34 +99,6 @@ def test_batch_space(space, expected_batch_space_4):
     assert batch_space_4 == expected_batch_space_4
 
 
-def test_batch_space_seed():
-    for space in [
-        CustomSpace(seed=123),
-        Box(0, 10, (), seed=123),
-        Tuple([Box(0, 5, (), seed=123), Box(0, 3, (), seed=123)], seed=123),
-        Dict(
-            {"space-1": Box(0, 5, (), seed=123), "space-2": Box(0, 10, (), seed=123)},
-            seed=123,
-        ),
-    ]:
-        batched_space = batch_space(space)
-        assert space.np_random == batched_space.np_random
-        for _ in range(100):
-            assert np.all(space.sample() == batched_space.sample())
-
-    space = MultiDiscrete([5, 3], seed=123)
-    batched_space = batch_space(space)
-    assert space.np_random == batched_space.np_random
-    # As type(space) == MultiDiscrete and type(batched_space) == Box
-    #   the sample functions are different, therefore cannot check sample equivalence
-
-    space = Discrete(5, seed=123)
-    batched_space = batch_space(space)
-    assert space.np_random == batched_space.np_random
-    # As type(space) == Discrete and type(batched_space) == MultiDiscrete
-    #   the sample functions are different, therefore cannot check sample equivalence
-
-
 @pytest.mark.parametrize(
     "space,expected_batch_space_4",
     list(zip(custom_spaces, expected_custom_batch_spaces_4)),
@@ -157,3 +133,66 @@ def test_iterate_custom_space(space, batch_space):
     for i, item in enumerate(iterator):
         assert item in space
     assert i == 3
+
+
+seeded_spaces = [
+    CustomSpace(seed=123),
+    Box(0, 10, (), seed=123),
+    Tuple([Box(0, 5, (), seed=123), Box(0, 3, (), seed=123)], seed=123),
+    Dict({"space-1": Box(0, 5, (), seed=123), "space-2": Box(0, 10, (), seed=123)}, seed=123,),
+    Discrete(5, seed=123),
+    MultiDiscrete([5, 3], seed=123)
+]
+
+
+@pytest.mark.parametrize(
+    "space", seeded_spaces,
+    ids=[space.__class__.__name__ for space in seeded_spaces]
+)
+def test_batch_space_seed(space):
+    batched_space = batch_space(space)  # n=1
+    assert space.np_random == batched_space.np_random
+
+
+@pytest.mark.parametrize("space", spaces, ids=[space.__class__.__name__ for space in spaces])
+@pytest.mark.parametrize("n", [4, 5], ids=[f'n={n}' for n in [3, 5]])
+@pytest.mark.parametrize("base_seed", [123, 456], ids=[f'seed={base_seed}' for base_seed in [123, 456]])
+def test_rng_different_at_each_index(space, n, base_seed):
+    space.seed(base_seed)
+
+    batched_space = batch_space(space, n)
+    assert space.np_random == batched_space.np_random
+
+    batched_sample = batched_space.sample()
+    sample = list(iterate(batched_space, batched_sample))
+    assert not all(np.all(element == sample[0]) for element in sample), sample
+
+
+@pytest.mark.parametrize("space", spaces, ids=[space.__class__.__name__ for space in spaces])
+@pytest.mark.parametrize("n", [1, 2, 5], ids=[f'n={n}' for n in [1, 2, 5]])
+@pytest.mark.parametrize("base_seed", [123, 456], ids=[f'seed={base_seed}' for base_seed in [123, 456]])
+def test_deterministic(space: Space, n: int, base_seed: int):
+    space_a = space
+    space_a.seed(base_seed)
+    space_b = copy.deepcopy(space_a)
+    assert space_a.np_random == space_b.np_random
+    assert space_a.np_random is not space_b.np_random
+
+    space_a_batched = batch_space(space_a, n)
+    space_b_batched = batch_space(space_b, n)
+    assert space_a_batched == space_b_batched
+    assert space_a_batched.np_random == space_b_batched.np_random
+    assert space_a_batched.np_random is not space_b_batched.np_random
+    assert space_a.np_random is not space_a_batched.np_random
+
+    # Check that batched space a and b random number generator are not effected by the original space
+    space_a.sample()
+    space_a_batched_sample = space_a_batched.sample()
+    space_b_batched_sample = space_b_batched.sample()
+    for a_sample, b_sample in zip(iterate(space_a_batched, space_a_batched_sample),
+                                  iterate(space_b_batched, space_b_batched_sample)):
+        if isinstance(a_sample, tuple):
+            for a_subsample, b_subsample in zip(a_sample, b_sample):
+                assert_array_equal(a_subsample, b_subsample)
+        else:
+            assert_array_equal(a_sample, b_sample)
