b7fc60fdb8a0dc77e8615ffc18dbba668e7b7a14
StringTheory <mark.m.towers@gmail.com>
2022-04-19 22:49:28 +0100

    Add sync vector determinism test for issue #2680

--- tests/vector/test_sync_vector_env.py
+++ tests/vector/test_sync_vector_env.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pytest
 
+from gym.envs.registration import EnvSpec
 from gym.spaces import Box, Discrete, MultiDiscrete, Tuple
 from gym.vector.sync_vector_env import SyncVectorEnv
+from tests.envs.spec_list import spec_list
 from tests.vector.utils import (
     CustomSpace,
     assert_rng_equal,
@@ -183,3 +185,16 @@ def test_sync_vector_env_seed():
         env_action = env.action_space.sample()
         vector_action = sync_vector_env.action_space.sample()
         assert np.all(env_action == vector_action)
+
+
+@pytest.mark.parametrize("spec", spec_list, ids=[spec.id for spec in spec_list])
+def test_sync_vector_determinism(spec: EnvSpec, seed: int = 123, n: int = 3):
+    """Check that for all environments, the sync vector envs produce the same action samples using the same seeds"""
+    env_1 = SyncVectorEnv([make_env(spec.id, seed=seed) for _ in range(n)])
+    env_2 = SyncVectorEnv([make_env(spec.id, seed=seed) for _ in range(n)])
+    assert_rng_equal(env_1.action_space.np_random, env_2.action_space.np_random)
+
+    for _ in range(100):
+        env_1_samples = env_1.action_space.sample()
+        env_2_samples = env_2.action_space.sample()
+        assert np.all(env_1_samples == env_2_samples)
