4fdd037c6531e01fe4e003eae09c357f44778187
StringTheory <mark.m.towers@gmail.com>
2022-04-19 23:13:43 +0100

    Fixed bug for https://github.com/openai/gym/pull/2727/files/462101d3846bc35ff3fad9f65979c693472a93a8#r850740825

--- gym/vector/utils/spaces.py
+++ gym/vector/utils/spaces.py
@@ -117,8 +117,12 @@ def _batch_space_dict(space, n=1):
 def _batch_space_custom(space, n=1):
     # Without deepcopy, then the space.np_random is batched_space.spaces[0].np_random
     # Which is an issue if you are sampling actions of both the original space and the batched space
-    space_copy = deepcopy(space)
-    return Tuple(tuple(space_copy for _ in range(n)), seed=deepcopy(space.np_random))
+    batched_space = Tuple(
+        tuple(deepcopy(space) for _ in range(n)), seed=deepcopy(space.np_random)
+    )
+    new_seeds = batched_space.np_random.integers(0, 1e8, n)
+    batched_space.seed(new_seeds)
+    return batched_space
 
 
 @singledispatch
