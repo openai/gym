c4bfe84fb8bb50a9baa95eee90e246ee1cbeaae7
younik <omar.younis98@gmail.com>
2022-04-22 00:01:28 +0200

    fix renderer

--- gym/utils/renderer.py
+++ gym/utils/renderer.py
@@ -4,14 +4,14 @@ NO_RETURNS_RENDER = ["human", None]
 
 
 class Renderer:
-    def __init__(self, mode: str, render: Callable[[str], Any]):
+    def __init__(self, mode: Optional[str], render: Callable[[str], Any]):
         self.mode = mode
         self.render = render
         self.render_list = []
 
     def render_step(self) -> None:
         render_return = self.render(self.mode)
-        if self.render not in NO_RETURNS_RENDER:
+        if self.mode not in NO_RETURNS_RENDER:
             self.render_list.append(render_return)
 
     def get_renders(self) -> Optional[List]:
