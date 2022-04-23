4664926440a3943ddc62bee7ff6b14ffcbc7c737
Ilya Kamen <ikamenshchikov@gmail.com>
2022-04-23 17:29:05 +0200

    Remove orphan comment
    The comment used to be due to the line that is not there anymore, like:
    ```python
    from gym.envs.mujoco.mujoco_env import MujocoEnv
    
    # ^^^^^ so that user gets the correct error
    # message if mujoco is not installed correctly
    ```
    
    Now the comment is useless.

--- gym/envs/mujoco/__init__.py
+++ gym/envs/mujoco/__init__.py
@@ -1,5 +1,3 @@
-# ^^^^^ so that user gets the correct error
-# message if mujoco is not installed correctly
 from gym.envs.mujoco.ant import AntEnv
 from gym.envs.mujoco.half_cheetah import HalfCheetahEnv
 from gym.envs.mujoco.hopper import HopperEnv
