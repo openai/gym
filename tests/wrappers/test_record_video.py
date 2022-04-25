8667dda587a67b4207a8c1bedf2d87450781a019
Gianluca De Cola <gianluca.deco@gmail.com>
2022-04-26 00:45:11 +0200

    fix test case.

--- tests/wrappers/test_record_video.py
+++ tests/wrappers/test_record_video.py
@@ -107,10 +107,7 @@ def test_record_video_within_vector_brax_info():
     envs = gym.wrappers.RecordEpisodeStatistics(envs)
     envs.reset()
     for i in range(199):
-        _, _, dones, infos = envs.step(envs.action_space.sample())
-        if all(dones):
-            print(f"episode_reward={infos['episode']['r']}")
-            break
+        envs.step(envs.action_space.sample())
     assert os.path.isdir("videos")
     mp4_files = [file for file in os.listdir("videos") if file.endswith(".mp4")]
     assert len(mp4_files) == 2
