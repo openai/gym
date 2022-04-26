ff4aff3c97b553cc2dd6389e7b2270e439c394be
younik <omar.younis98@gmail.com>
2022-04-26 23:22:20 +0200

    add comments to renderer

--- gym/utils/renderer.py
+++ gym/utils/renderer.py
@@ -1,10 +1,26 @@
 from typing import Any, Callable, List, Optional
 
+# list of modes with which render function returns None
 NO_RETURNS_RENDER = [None, "human"]
+
+# list of modes with which render returns just a single frame of the current state
 SINGLE_RENDER = ["single_rgb_array"]
 
 
 class Renderer:
+    """ This class serves to easily integrate collection of renders for environments
+    that has a function that computes a single render.
+
+    To use this function:
+    - instantiate this class with the mode and the function that computes a single frame
+    - call render_step method each time the frame should be saved in the list
+      (usually at the end of the step method)
+    - call get_renders whenever you want to retrieve renders
+      (usually in the render method)
+    - call reset to clean the render list
+      (usually in the reset method of the environment)
+    """
+
     def __init__(self, mode: Optional[str], render: Callable[[str], Any]):
         self.mode = mode
         self.render = render
