de04d7d1f3d28d5a5d47d1e461927cc9fb7199b4
Gianluca De Cola <gianluca.deco@gmail.com>
2022-05-06 01:20:33 +0200

    action check in car_racing no more needed.

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -449,33 +449,32 @@ class CarRacing(gym.Env, EzPickle):
     @validate_action_discrete
     @validate_action_continuous
     def step(self, action: Union[np.ndarray, int]):
-        if action is not None:
-            if self.continuous:
-                self.car.steer(-action[0])
-                self.car.gas(action[1])
-                self.car.brake(action[2])
-            else:
-                self.car.steer(-0.6 * (action == 1) + 0.6 * (action == 2))
-                self.car.gas(0.2 * (action == 3))
-                self.car.brake(0.8 * (action == 4))
+        if self.continuous:
+            self.car.steer(-action[0])
+            self.car.gas(action[1])
+            self.car.brake(action[2])
+        else:
+            self.car.steer(-0.6 * (action == 1) + 0.6 * (action == 2))
+            self.car.gas(0.2 * (action == 3))
+            self.car.brake(0.8 * (action == 4))
 
         self.state = self._compute_pixel_state()
 
         step_reward = 0
         done = False
-        if action is not None:  # First step without action, called from reset()
-            self.reward -= 0.1
-            # We actually don't want to count fuel spent, we want car to be faster.
-            # self.reward -=  10 * self.car.fuel_spent / ENGINE_POWER
-            self.car.fuel_spent = 0.0
-            step_reward = self.reward - self.prev_reward
-            self.prev_reward = self.reward
-            if self.tile_visited_count == len(self.track) or self.new_lap:
-                done = True
-            x, y = self.car.hull.position
-            if abs(x) > PLAYFIELD or abs(y) > PLAYFIELD:
-                done = True
-                step_reward = -100
+
+        self.reward -= 0.1
+        # We actually don't want to count fuel spent, we want car to be faster.
+        # self.reward -=  10 * self.car.fuel_spent / ENGINE_POWER
+        self.car.fuel_spent = 0.0
+        step_reward = self.reward - self.prev_reward
+        self.prev_reward = self.reward
+        if self.tile_visited_count == len(self.track) or self.new_lap:
+            done = True
+        x, y = self.car.hull.position
+        if abs(x) > PLAYFIELD or abs(y) > PLAYFIELD:
+            done = True
+            step_reward = -100
 
         return self.state, step_reward, done, {}
 
