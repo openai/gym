322ff39dcb2e2a286c82ac49418fd5a47c465ede
StringTheory <mark.m.towers@gmail.com>
2022-05-06 15:41:32 +0100

    Revert to the original try except with modified assert error message

--- gym/utils/env_checker.py
+++ gym/utils/env_checker.py
@@ -386,10 +386,12 @@ def check_env(env: gym.Env, warn: bool = True, skip_render_check: bool = True) -
     observation_space = env.observation_space
     action_space = env.action_space
 
-    with pytest.raises(AssertionError):
+    try:
         # As environments are normally wrapped by OrderEnforcing
         # "Cannot call env.render() before calling env.reset()" is raised
         env.step(env.action_space.sample())
+    except AssertionError as e:
+        assert "Cannot call env.step()" in str(e)
 
     # Warn the user if needed.
     # A warning means that the environment may run but not work properly with popular RL libraries.
