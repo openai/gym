02c7957d4b2851c99fb06c7afbfee055eccdf09c
mark <mark.m.towers@gmail.com>
2022-05-09 20:33:18 +0100

    fix the `has_wrapper` type hinting

--- tests/wrappers/utils.py
+++ tests/wrappers/utils.py
@@ -1,9 +1,8 @@
-from typing import Union
 
 import gym
 
 
-def has_wrapper(wrapped_env: Union[gym.Wrapper, gym.Env], wrapper_type: type):
+def has_wrapper(wrapped_env: gym.Env, wrapper_type: type) -> bool:
     while isinstance(wrapped_env, gym.Wrapper):
         if isinstance(wrapped_env, wrapper_type):
             return True
