2004e2736fbc3fb3c95f6832f2ddbeafd3f1665a
StringTheory <mark.m.towers@gmail.com>
2022-05-11 15:17:08 +0100

    Updated wrapper top docstring

--- gym/wrappers/__init__.py
+++ gym/wrappers/__init__.py
@@ -1,4 +1,4 @@
-"""Module of wrapper functions."""
+"""Module of wrapper classes."""
 from gym.wrappers.atari_preprocessing import AtariPreprocessing
 from gym.wrappers.autoreset import AutoResetWrapper
 from gym.wrappers.clip_action import ClipAction
