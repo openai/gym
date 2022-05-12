f4da436ba5c4de0b10d8b838330ee0326fab9c9d
arjun_kg <kg.arjun@gmail.com>
2022-05-13 00:47:43 +0530

    update test env

--- tests/envs/register_during_make_env.py
+++ tests/envs/register_during_make_env.py
@@ -5,15 +5,14 @@ class RegisterDuringMakeEnv(gym.Env):
     """Used in `test_registration.py` to check if `env.make` can import and register an env"""
 
     def __init__(self):
-        super().__init__()
-        self.act_space = gym.spaces.Discrete(2)
-        self.obs_space = gym.spaces.Discrete(2)
+        self.action_space = gym.spaces.Discrete(1)
+        self.observation_space = gym.spaces.Discrete(1)
 
-    def reset(self, return_info=True):
-        return self.obs_space.sample(), {}
+    def reset(self, *, seed=None, return_info=True, options=None):
+        return 0, {}
 
     def step(self, action):
-        return self.obs_space.sample(), 0, False, {}
+        return 0, 0, False, {}
 
 
 gym.register(
