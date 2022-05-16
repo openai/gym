64b4b31d8245f6972b3d37270faf69b74908a67d
Andrew Tan <andrew.jinshen@gmail.com>
2022-05-17 00:37:43 +0800

    Fix edge case for out of bounds (#2822)
    * Fix edge case for out of bounds
    
    This deals with the edge case where an object's corner may not be in the screen, but the object still overlaps and should be drawn.
    
    * Calculate maximum diagonal for out of bounds calculation

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -38,6 +38,10 @@ TRACK_TURN_RATE = 0.31
 TRACK_WIDTH = 40 / SCALE
 BORDER = 8 / SCALE
 BORDER_MIN_COUNT = 4
+GRASS_DIM = PLAYFIELD / 20.0
+MAX_SHAPE_DIM = (
+    max(GRASS_DIM, TRACK_WIDTH, TRACK_DETAIL_STEP) * math.sqrt(2) * ZOOM * SCALE
+)
 
 
 class FrictionDetector(contactListener):
@@ -557,16 +561,15 @@ class CarRacing(gym.Env, EzPickle):
         )
 
         # draw grass patches
-        k = bounds / (20.0)
         grass = []
         for x in range(0, 40, 2):
             for y in range(0, 40, 2):
                 grass.append(
                     [
-                        (k * x + k, k * y + 0),
-                        (k * x + 0, k * y + 0),
-                        (k * x + 0, k * y + k),
-                        (k * x + k, k * y + k),
+                        (GRASS_DIM * x + GRASS_DIM, GRASS_DIM * y + 0),
+                        (GRASS_DIM * x + 0, GRASS_DIM * y + 0),
+                        (GRASS_DIM * x + 0, GRASS_DIM * y + GRASS_DIM),
+                        (GRASS_DIM * x + GRASS_DIM, GRASS_DIM * y + GRASS_DIM),
                     ]
                 )
         for poly in grass:
@@ -661,15 +664,17 @@ class CarRacing(gym.Env, EzPickle):
             (c[0] * zoom + translation[0], c[1] * zoom + translation[1]) for c in poly
         ]
         # This checks if the polygon is out of bounds of the screen, and we skip drawing if so.
-        if not clip:
+        # Instead of calculating exactly if the polygon and screen overlap,
+        # we simply check if the polygon is in a larger bounding box whose dimension
+        # is greater than the screen by MAX_SHAPE_DIM, which is the maximum
+        # diagonal length of an environment object
+        if not clip or any(
+            (-MAX_SHAPE_DIM <= coord[0] <= WINDOW_W + MAX_SHAPE_DIM)
+            and (-MAX_SHAPE_DIM <= coord[1] <= WINDOW_H + MAX_SHAPE_DIM)
+            for coord in poly
+        ):
             gfxdraw.aapolygon(self.surf, poly, color)
             gfxdraw.filled_polygon(self.surf, poly, color)
-            return
-        for coord in poly:
-            if (0 < coord[0] < WINDOW_W) and (0 < coord[1] < WINDOW_H):
-                gfxdraw.aapolygon(self.surf, poly, color)
-                gfxdraw.filled_polygon(self.surf, poly, color)
-                return
 
     def _create_image_array(self, screen, size):
         import pygame
