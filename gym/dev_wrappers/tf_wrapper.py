b7dd2ee96bb97df5911e962efcaef7782def9fe8
Justin Deutsch <djustin8@vt.edu>
2022-05-30 16:31:02 -0400

    add initial tensorflow wrapper

---
+++ gym/dev_wrappers/tf_wrapper.py
@@ -0,0 +1,49 @@
+from typing import Union
+
+import jax.dlpack
+import tensorflow as tf
+
+from gym import Env, Wrapper
+
+class TensorFlowWrapper(Wrapper):
+    def __init__(self,
+        env: Union[Env, Wrapper]):
+
+        super().__init__(env)
+        self.is_vector_env = getattr(env, "is_vector_env", False)
+
+    def step(self, action: tf.Tensor):
+        jax_action = self._tf_to_jax(action)
+        obs, reward, done, info = super().step(jax_action)
+
+        if self.is_vector_env:
+            obs = [self._jax_to_tf(o) for o in obs]
+        else:
+            obs = self._jax_to_tf(obs)
+
+        return obs, reward, done, info
+
+    def reset(self, **kwargs) -> Union[tf.Tensor, tuple[tf.Tensor, dict]]:
+        return_info = kwargs.get("return_info", False)
+        if return_info:
+            obs, info = super().reset(**kwargs)
+        else:
+            obs = super().reset(**kwargs)
+
+        # TODO: Verify this captures vector envs and converts everything from jax to torch
+        if self.is_vector_env:
+            obs = [self._jax_to_tf(o) for o in obs]
+        else:
+            obs = self._jax_to_tf(obs)
+
+        if return_info:
+            return obs, info
+        else:
+            return obs
+
+    def _tf_to_jax(self, arr: tf.Tensor):
+        return jax.dlpack.from_dlpack(tf.experimental.dlpack.to_dlpack(arr))
+
+    def _jax_to_tf(self, arr):
+        return tf.experimental.dlpack.from_dlpack(jax.dlpack.to_dlpack(arr))
+
--- tests/dev_wrappers/test_tf_wrapper.py
+++ tests/dev_wrappers/test_tf_wrapper.py
@@ -1,8 +1,82 @@
+from typing import Optional
+
+import jax.numpy as jnp
+import numpy as np
+import pytest
+import tensorflow as tf
+
 import gym
-#from gym.dev_wrappers.tf_wrapper import T
+from gym.dev_wrappers.tf_wrapper import TensorFlowWrapper
 
-def test_torch_wrap_observation():
-    pass
+class DummyJaxEnv(gym.Env):
+    metadata = {}
 
-def test_torch_wrapper_reset_info():
-    pass
+    def __init__(self, return_reward_idx=0):
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Box(
+            low=np.array([-1.0]), high=np.array([1.0]), dtype=np.float64
+        )
+        self.returned_rewards = [0, 1, 2, 3, 4]
+        self.return_reward_idx = return_reward_idx
+        self.t = self.return_reward_idx
+
+    def step(self, action):
+        self.t += 1
+        return jnp.array([self.t]), self.t, self.t == len(self.returned_rewards), {}
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        return_info: Optional[bool] = False,
+        options: Optional[dict] = None
+    ):
+        super().reset(seed=seed)
+        self.t = self.return_reward_idx
+        if not return_info:
+            return jnp.array([self.t])
+        else:
+            return jnp.array([self.t]), {}
+
+
+def make_env(return_reward_idx):
+    def thunk():
+        env = DummyJaxEnv(return_reward_idx)
+        return env
+
+    return thunk
+
+
+def test_tf_wrapper_reset_info():
+    env = DummyJaxEnv(return_reward_idx=0)
+    env = TensorFlowWrapper(env)
+    obs = env.reset()
+    assert isinstance(obs, tf.Tensor)
+    del obs
+
+    obs = env.reset(return_info=False)
+    assert isinstance(obs, tf.Tensor)
+    del obs
+
+    obs, info = env.reset(return_info=True)
+    assert isinstance(obs, tf.Tensor)
+    assert isinstance(info, dict)
+
+
+def test_tf_wrapper_step():
+    env = DummyJaxEnv(return_reward_idx=0)
+    env = TensorFlowWrapper(env)
+
+    (obs, *_) = env.step(tf.convert_to_tensor(env.action_space.sample()))
+    assert isinstance(obs, tf.Tensor)
+    assert env.observation_space.contains(obs.numpy())
+
+
+@pytest.mark.xfail(reason="VectorEnv for brax not yet implemented")
+def test_tf_wrapper_vector_env():
+    env_fns = [make_env(0), make_env(1)]
+    envs = gym.vector.SyncVectorEnv(env_fns)
+    envs.reset()
+
+    (obs, *_) = envs.step(tf.convert_to_tensor(envs.action_space.sample()))
+    assert all(isinstance(o, tf.Tensor) for o in obs)
