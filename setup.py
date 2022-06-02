676ed18c83fa7679f8818b52324b009e453c7928
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2022-06-02 20:30:41 +0100

    Updated setup to check the version is valid, added testing and all dependency groups and collects the requirements from requirements.txt to keep everything standardized.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 """Setups the project."""
 import itertools
-import os.path
-import sys
+import re
 
 from setuptools import find_packages, setup
 
-# Don't import gym module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "gym"))
-from version import VERSION  # noqa:E402
+with open('gym/version.py') as file:
+    full_version = file.read()
+    assert re.match(r'VERSION = "\d\.\d+\.\d+"\n', full_version).group(0) == full_version, \
+        f'Unexpected version re'
+    VERSION = re.search(r'\d\.\d+\.\d+', full_version).group(0)
 
 # Environment-specific dependencies.
 extras = {
@@ -17,30 +18,29 @@ extras = {
     "classic_control": ["pygame==2.1.0"],
     "mujoco_py": ["mujoco_py<2.2,>=2.1"],
     "mujoco": ["mujoco==2.2.0", "imageio>=2.14.1"],
-    "toy_text": ["pygame==2.1.0", "scipy>=1.4.1"],
+    "toy_text": ["pygame==2.1.0"],
     "other": ["lz4>=3.1.0", "opencv-python>=3.0", "matplotlib>=3.0"],
 }
 
-# Meta dependency groups.
-nomujoco_blacklist = {"mujoco_py", "mujoco", "accept-rom-license", "atari"}
-nomujoco_groups = set(extras.keys()) - nomujoco_blacklist
+# Testing dependency groups.
+testing_group = set(extras.keys()) - {"accept-rom-license", "atari"}
+extras["testing"] = list(set(
+    itertools.chain.from_iterable(map(lambda group: extras[group], testing_group))
+)) + ["pytest", "mock"]
 
-extras["nomujoco"] = list(
-    itertools.chain.from_iterable(map(lambda group: extras[group], nomujoco_groups))
-)
-
-noatari_blacklist = {"accept-rom-license", "atari"}
-noatari_groups = set(extras.keys()) - noatari_blacklist
-extras["noatari"] = list(
-    itertools.chain.from_iterable(map(lambda group: extras[group], noatari_groups))
-)
-
-all_blacklist = {"accept-rom-license"}
-all_groups = set(extras.keys()) - all_blacklist
-
-extras["all"] = list(
+# All dependency groups
+all_groups = set(extras.keys()) - {"accept-rom-license"}
+extras["all"] = list(set(
     itertools.chain.from_iterable(map(lambda group: extras[group], all_groups))
-)
+))
+
+# Gets the requirements from "requirements.txt"
+with open('requirements.txt') as file:
+    install_requirements = list(map(lambda line: line.strip(), file.readlines()))
+
+# Updates the test_requirements.txt based on `extras["testing"]`
+with open('test_requirements.txt', 'w') as file:
+    file.writelines(list(map(lambda line: f'{line}\n', extras["testing"])))
 
 setup(
     name="gym",
@@ -52,14 +52,9 @@ setup(
     license="MIT",
     packages=[package for package in find_packages() if package.startswith("gym")],
     zip_safe=False,
-    install_requires=[
-        "numpy>=1.18.0",
-        "cloudpickle>=1.2.0",
-        "importlib_metadata>=4.8.0; python_version < '3.10'",
-        "gym_notices>=0.0.4",
-        "dataclasses==0.8; python_version == '3.6'",
-    ],
+    install_requires=install_requirements,
     extras_require=extras,
+    tests_require=extras["testing"],
     package_data={
         "gym": [
             "envs/mujoco/assets/*.xml",
@@ -69,7 +64,6 @@ setup(
             "py.typed",
         ]
     },
-    tests_require=["pytest", "mock"],
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
