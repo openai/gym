c5bcecda43202e374fe4da16c33b8b3ec20ef483
NomsterDude <85312283+NomsterDude@users.noreply.github.com>
2022-06-09 21:22:08 -0700

    Update __init__.py
    Commented out an unused import.

--- gym/__init__.py
+++ gym/__init__.py
@@ -1,7 +1,7 @@
 """Root __init__ of the gym module setting the __all__ of gym modules."""
 # isort: skip_file
 
-from gym import error
+# from gym import error
 from gym.version import VERSION as __version__
 
 from gym.core import (
