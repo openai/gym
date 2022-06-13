2287267fdfd9084b5584e1f40a5a76e504f1cc5c
StringTheory <mark.m.towers@gmail.com>
2022-06-13 22:21:20 +0100

    Fixed check environment

--- tests/envs/test_envs.py
+++ tests/envs/test_envs.py
@@ -3,6 +3,7 @@ from typing import List
 import numpy as np
 import pytest
 
+import gym
 from gym import envs
 from gym.spaces import Box
 from gym.utils.env_checker import check_env
@@ -12,64 +13,28 @@ from tests.envs.spec_list import spec_list, spec_list_no_mujoco_py
 # This runs a smoketest on each official registered env. We may want
 # to try also running environments which are not officially registered
 # envs.
-@pytest.mark.filterwarnings(
-    "ignore:.*We recommend you to use a symmetric and normalized Box action space.*"
-)
+IGNORE_WARNINGS = [
+    "Agent's minimum observation space value is -infinity. This is probably too low.",
+    "Agent's maximum observation space value is infinity. This is probably too high.",
+    "We recommend you to use a symmetric and normalized Box action space (range=[-1, 1]) https://stable-baselines3.readthedocs.io/en/master/guide/rl_tips.html"
+]
+IGNORE_WARNINGS = [f'\x1b[33mWARN: {message}\x1b[0m' for message in IGNORE_WARNINGS]
+
+
 @pytest.mark.parametrize(
-    "spec", spec_list_no_mujoco_py, ids=[spec.id for spec in spec_list_no_mujoco_py]
+    "spec", spec_list, ids=[spec.id for spec in spec_list]
 )
 def test_env(spec):
     # Capture warnings
-    with pytest.warns(None) as warnings:
-        env = spec.make()
+    env = spec.make(disable_env_checker=True)
 
     # Test if env adheres to Gym API
-    check_env(env, skip_render_check=True)
+    with pytest.warns(None) as warnings:
+        check_env(env)
 
-    # Check that dtype is explicitly declared for gym.Box spaces
-    for warning_msg in warnings:
-        assert "autodetected dtype" not in str(warning_msg.message)
-
-    ob_space = env.observation_space
-    act_space = env.action_space
-    ob = env.reset()
-    assert ob_space.contains(ob), f"Reset observation: {ob!r} not in space"
-    if isinstance(ob_space, Box):
-        # Only checking dtypes for Box spaces to avoid iterating through tuple entries
-        assert (
-            ob.dtype == ob_space.dtype
-        ), f"Reset observation dtype: {ob.dtype}, expected: {ob_space.dtype}"
-
-    a = act_space.sample()
-    observation, reward, done, _info = env.step(a)
-    assert ob_space.contains(
-        observation
-    ), f"Step observation: {observation!r} not in space"
-    assert np.isscalar(reward), f"{reward} is not a scalar for {env}"
-    assert isinstance(done, bool), f"Expected {done} to be a boolean"
-    if isinstance(ob_space, Box):
-        assert (
-            observation.dtype == ob_space.dtype
-        ), f"Step observation dtype: {ob.dtype}, expected: {ob_space.dtype}"
-
-    env.close()
-
-
-@pytest.mark.parametrize("spec", spec_list, ids=[spec.id for spec in spec_list])
-def test_reset_info(spec):
-
-    with pytest.warns(None):
-        env = spec.make()
-
-    ob_space = env.observation_space
-    obs = env.reset()
-    assert ob_space.contains(obs)
-    obs = env.reset(return_info=False)
-    assert ob_space.contains(obs)
-    obs, info = env.reset(return_info=True)
-    assert ob_space.contains(obs)
-    assert isinstance(info, dict)
-    env.close()
+    for warning in warnings.list:
+        if warning.message.args[0] not in IGNORE_WARNINGS:
+            raise gym.error.Error(f"Unexpected warning: {warning.message}")
 
 
 @pytest.mark.parametrize(
