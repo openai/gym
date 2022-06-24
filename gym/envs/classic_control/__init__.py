5c8449d2ab2de1f8e34151afb9031dfe8c7c18e0
Jianfeng Lin <62167162+OliverLin28@users.noreply.github.com>
2022-06-24 15:45:25 +0200

    Update __init__.py

--- gym/envs/classic_control/__init__.py
+++ gym/envs/classic_control/__init__.py
@@ -3,3 +3,4 @@ from gym.envs.classic_control.cartpole import CartPoleEnv
 from gym.envs.classic_control.continuous_mountain_car import Continuous_MountainCarEnv
 from gym.envs.classic_control.mountain_car import MountainCarEnv
 from gym.envs.classic_control.pendulum import PendulumEnv
+from gym.envs.classic_control.BoatControl_v0 import BoatControl
