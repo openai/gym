61a39f41bced1f4efefc0f68641ed222c110e4dc
Rodrigo de Lazcano <r.l.p.v96@gmail.com>
2022-06-30 10:59:59 -0400

    Initialize observation spaces and pytest (#2929)
    * Remove step initialization for mujoco obs spaces
    
    	* remove step initialization for mujoco obs space
    
    	* pre-commit
    
    pytest obs space mujoco

--- gym/envs/mujoco/hopper_v3.py
+++ gym/envs/mujoco/hopper_v3.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from gym import utils
 from gym.envs.mujoco import mujoco_env
+from gym.spaces import Box
 
 DEFAULT_CAMERA_CONFIG = {
     "trackbodyid": 2,
@@ -58,8 +59,22 @@ class HopperEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             exclude_current_positions_from_observation
         )
 
+        if exclude_current_positions_from_observation:
+            observation_space = Box(
+                low=-np.inf, high=np.inf, shape=(11,), dtype=np.float64
+            )
+        else:
+            observation_space = Box(
+                low=-np.inf, high=np.inf, shape=(12,), dtype=np.float64
+            )
+
         mujoco_env.MujocoEnv.__init__(
-            self, xml_file, 4, mujoco_bindings="mujoco_py", **kwargs
+            self,
+            xml_file,
+            4,
+            mujoco_bindings="mujoco_py",
+            observation_space=observation_space,
+            **kwargs
         )
 
     @property
