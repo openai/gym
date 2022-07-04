6663c511f12365b03a8228832fff58ad99817753
StringTheory <mark.m.towers@gmail.com>
2022-07-04 22:53:50 +0100

    pre-commit

--- tests/utils/test_env_checker.py
+++ tests/utils/test_env_checker.py
@@ -67,7 +67,9 @@ def _reset_default_seed(
     self: GenericTestEnv, seed="Error", return_info=False, options=None
 ):
     super(GenericTestEnv, self).reset(seed=seed)
-    self.observation_space._np_random = self.np_random  # pyright: ignore [reportPrivateUsage]
+    self.observation_space._np_random = (
+        self.np_random
+    )  # pyright: ignore [reportPrivateUsage]
     return self.observation_space.sample()
 
 
