b13ee57eb278f53da5daf3f18c09acab9879c99b
Gianluca De Cola <gianluca.deco@gmail.com>
2022-07-21 23:00:12 +0200

    remove unused for lambda reward.

--- gym/dev_wrappers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Root __init__ of the gym dev_wrappers."""
-from typing import Dict, Sequence, TypeVar, Union
-
-ArgType = TypeVar("ArgType")
-FuncArgType = Union[ArgType, Dict[str, "FuncArgType"], Sequence["FuncArgType"]]
