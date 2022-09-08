abefea70d412bc721593d5ad570e02712b05048d
Vincent Moens <vincentmoens@gmail.com>
2022-09-08 15:59:15 +0200

    [BugFix] Raise exception in PixelObservationWrapper if env.render_mode is not specified (#3076)
    * init
    
    * amend

--- gym/wrappers/pixel_observation.py
+++ gym/wrappers/pixel_observation.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, List, Optional, Tuple
 import numpy as np
 
 import gym
-from gym import logger, spaces
+from gym import spaces
 
 STATE_KEY = "state"
 
@@ -95,8 +95,7 @@ class PixelObservationWrapper(gym.ObservationWrapper):
 
         default_render_kwargs = {}
         if not env.render_mode:
-            default_render_kwargs = {"mode": "rgb_array_list"}
-            logger.warn(
+            raise AttributeError(
                 "env.render_mode must be specified to use PixelObservationWrapper:"
                 "`gym.make(env_name, render_mode='rgb_array')`."
             )
