2ac7b1c7eab3b47e4a7111c0681a3d268f85f127
Eric Kalosa-Kenyon <ekalosak@users.noreply.github.com>
2022-10-04 09:12:08 -0700

    clarified and fixed typo in GraphInstance docstring (#3100)

--- gym/spaces/graph.py
+++ gym/spaces/graph.py
@@ -14,8 +14,8 @@ class GraphInstance(NamedTuple):
     """A Graph space instance.
 
     * nodes (np.ndarray): an (n x ...) sized array representing the features for n nodes, (...) must adhere to the shape of the node space.
-    * edges (Optional[np.ndarray]): an (m x ...) sized array representing the features for m nodes, (...) must adhere to the shape of the edge space.
-    * edge_links (Optional[np.ndarray]): an (m x 2) sized array of ints representing the two nodes that each edge connects.
+    * edges (Optional[np.ndarray]): an (m x ...) sized array representing the features for m edges, (...) must adhere to the shape of the edge space.
+    * edge_links (Optional[np.ndarray]): an (m x 2) sized array of ints representing the indices of the two nodes that each edge connects.
     """
 
     nodes: np.ndarray
