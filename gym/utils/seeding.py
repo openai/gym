ea90698d93ec1a505b435de343cd457370ce7fcd
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2022-10-15 11:22:44 +0100

    Change seeding error message to report seed type

--- gym/utils/seeding.py
+++ gym/utils/seeding.py
@@ -19,7 +19,7 @@ def np_random(seed: Optional[int] = None) -> Tuple[np.random.Generator, Any]:
         Error: Seed must be a non-negative integer or omitted
     """
     if seed is not None and not (isinstance(seed, int) and 0 <= seed):
-        raise error.Error(f"Seed must be a non-negative integer or omitted, not {seed}")
+        raise error.Error(f"Seed must be a non-negative integer or omitted, not {type(seed)}")
 
     seed_seq = np.random.SeedSequence(seed)
     np_seed = seed_seq.entropy
