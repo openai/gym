9c4f181e1296de9f72f4fbd1d8aa90def361903e
Kartik Srinivas <88765934+kartiksrinivas007@users.noreply.github.com>
2024-11-17 15:43:44 -0500

    Correct spelling core.py L90
    Removed "either" -- typo or grammatical error

--- gym/core.py
+++ gym/core.py
@@ -87,7 +87,7 @@ class Env(Generic[ObsType, ActType]):
         """Run one timestep of the environment's dynamics.
 
         When end of episode is reached, you are responsible for calling :meth:`reset` to reset this environment's state.
-        Accepts an action and returns either a tuple `(observation, reward, terminated, truncated, info)`.
+        Accepts an action and returns a tuple `(observation, reward, terminated, truncated, info)`.
 
         Args:
             action (ActType): an action provided by the agent
